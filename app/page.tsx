38e7dad698021ffdc332719f4859c030d0f940c8
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-08 09:00:50 +0530

    Add LiveBlock to the home Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import LiveBlock from "@/components/LiveBlock";
+const text =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto iure quos placeat quibusdam laudantium sunt ullam voluptatem eius atque id dolore itaque inventore ex mollitia, illo tempore vel, nam quis natus commodi ut repudiandae. Soluta saepe beatae iusto voluptatem ipsa nam facilis eum iure eligendi deleniti? Officiis ut voluptatum vero?";
+const buttonsArray = ["Explain", "Example", "Story", "Analogy", "History"];
+
 export default function Home() {
-  return <button className="bg-green-600">Falcon One</button>;
+  return (
+    <div>
+      <LiveBlock generatedText={text} buttonsArray={buttonsArray} />
+    </div>
+  );
 }
