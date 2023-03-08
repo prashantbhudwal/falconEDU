80a56669e0b2a995e7a67a13cfca96248836f842
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-08 18:38:05 +0530

    Add function that returns a prompt for a button

--- app/utils/index.tsx
+++ app/utils/index.tsx
@@ -20,3 +20,26 @@ export const getEmoji = function (blockType: string) {
       return "☣️";
   }
 };
+
+export const getPrompt = function (promptType: string) {
+  switch (promptType) {
+    case "story":
+      return "Tell me a story about";
+    case "example":
+      return " Give me an example of";
+    case "analogy":
+      return "Give me an analogy for";
+    case "history":
+      return "Tell me about the history of";
+    case "application":
+      return "Give me an application of";
+    case "antiExample":
+      return "Give me an anti-example of";
+    case "contrast":
+      return "Contrast";
+    case "explain":
+      return "Explain";
+    default:
+      return "Explain";
+  }
+};
