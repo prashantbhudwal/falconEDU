d48a17fad30ed8300c09d53f0e90e6e8c9e12981
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-08 12:11:26 +0530

    Fix max width for big screens

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
     );
   });
   return (
-    <div className="flex flex-col gap-4 items-center  max-w-2xl">
+    <div className="flex flex-col gap-4 items-center max-w-xl">
       {chatBlocks}
       <LiveBlock blockData={liveBlock} buttonsArray={buttonsArray} />
     </div>
