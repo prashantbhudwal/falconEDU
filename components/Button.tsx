3fb02175876a1edd103a5ed8528d3f3a25d00fb5
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-08 09:17:26 +0530

    Style the Button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,8 +8,10 @@ export default function Button({
 }) {
   return (
     <button
-      onClick={() => onClick()}
-      className={"bg-green-600 text-white rounded-md px-4 py-2"}
+      onClick={onClick}
+      className={
+        "bg-emerald-500 ring-1 ring-slate-700 text-slate-700 rounded-md px-4 py-2 text-sm font-medium"
+      }
     >
       {children}
     </button>
