dfb8b73c3caa17dce5c3bfc8aa5f6cd741e117d6
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-08 12:04:06 +0530

    Extract data to a schema file

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,6 @@
 import Block from "@/components/Block";
 import LiveBlock from "@/components/LiveBlock";
-const buttonsArray = [
-  "Explain",
-  "Example",
-  "Story",
-  "Analogy",
-  "History",
-  "Application",
-  "Anti-Example",
-  "Contrast",
-];
-const emojis = {
-  define: "📖",
-  explain: "📝",
-  example: "🧩",
-  story: "🏜",
-  analogy: "🪢",
-  history: "📜",
-  application: "🎯",
-  antiExample: "☣️",
-  contrast: "🔀",
-};
-
-const chatArray = [
-  {
-    message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto iure quos placeat quibusdam laudantium sunt ullam voluptatem eius atque id dolore itaque inventore ex mollitia, illo tempore vel, nam quis natus commodi ut repudiandae. Soluta saepe beatae iusto voluptatem ipsa nam facilis eum iure eligendi deleniti? Officiis ut voluptatum vero?",
-    id: "1",
-    blockType: "story",
-    blockEmoji: "🏜",
-  },
-  {
-    message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto iure quos placeat quibusdam laudantium sunt ullam voluptatem eius atque id dolore itaque inventore ex mollitia, illo tempore vel, nam quis natus commodi ut repudiandae. Soluta saepe beatae iusto voluptatem ipsa nam facilis eum iure eligendi deleniti? Officiis ut voluptatum vero?",
-    id: "2",
-    blockType: "example",
-    blockEmoji: "🧩",
-  },
-  {
-    message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto iure quos placeat quibusdam laudantium sunt ullam voluptatem eius atque id dolore itaque inventore ex mollitia, illo tempore vel, nam quis natus commodi ut repudiandae. Soluta saepe beatae iusto voluptatem ipsa nam facilis eum iure eligendi deleniti? Officiis ut voluptatum vero?",
-    id: "3",
-    blockType: "analogy",
-    blockEmoji: "🪢",
-  },
-];
+import { chatArray, buttonsArray } from "@/app/schema";
 
 const liveBlock = chatArray[0];
 
