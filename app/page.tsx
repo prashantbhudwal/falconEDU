1a40fbe0c26d6875931b6a60401d984411de70b1
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-08 17:07:15 +0530

    Add input box to get the topic

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
+"use client";
 import { buttonsArray } from "@/app/schema";
 import Chat from "./chat";
+import { useState } from "react";
+import Button from "@/components/Button";
 
 const chatResponse =
-  "This is a simulation of chat response from the API route.";
+  "This is a simulation of chat response from the API route. It is a string. It is a very long string";
 
 export default function Home() {
+  const [topic, setTopic] = useState<string>("");
+  const [input, setInput] = useState<string>("");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+  if (topic === "") {
+    return (
+      <div className="flex flex-col gap-2 items-center m-4">
+        <input
+          required
+          type="text"
+          onChange={handleChange}
+          value={input}
+          className="border-slate-700 rounded-md bg-slate-300 text-black p-3  w-96"
+        />
+        <Button onClick={() => setTopic(input)}>Start</Button>
+      </div>
+    );
+  }
+
   return <Chat buttonsArray={buttonsArray} chatResponse={chatResponse} />;
 }
