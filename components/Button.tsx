5b0119df6e3431e7b8fe098ab3c820941ca29e1c
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-08 07:35:57 +0530

    Add skeleton for Button component

---
+++ components/Button.tsx
@@ -0,0 +1,16 @@
+export default function Button({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick: () => void;
+}) {
+  return (
+    <button
+      onClick={() => onClick}
+      className={"bg-green-600 text-white rounded-md px-4 py-2"}
+    >
+      {children}
+    </button>
+  );
+}
