db8dafb3e0b257c07d9ea8e33d4b0edd5eee6c2d
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-08 14:04:47 +0530

    Add OpenAI configuration

--- app/api/falcon/route.ts
+++ app/api/falcon/route.ts
@@ -1,5 +1,14 @@
 import { NextResponse, NextRequest } from "next/server";
+import { OpenAIApi, Configuration } from "openai";
 
-export async function POST() {
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  const openai = new OpenAIApi(configuration);
+
   return NextResponse.json({ message: "Hello World" });
 }
