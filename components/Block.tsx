d0bd7b14551d03275e2ccd0d0aabe22adc788d41
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-08 11:59:23 +0530

    Increase the size of the emoji

--- components/Block.tsx
+++ components/Block.tsx
@@ -11,7 +11,7 @@ export default function Block({
     <div className="bg-slate-800 text-slate-300 px-5 py-3 rounded-lg ring-1 ring-slate-500">
       <header className="text-xs font-medium text-slate-500 text-right flex justify-between items-baseline border-b border-solid border-slate-700 pb-2 ">
         <p>{blockType.toUpperCase()}</p>
-        <p>{blockEmoji}</p>
+        <p className="text-base">{blockEmoji}</p>
       </header>
       <p className="leading-6 text-base pt-2">{displayText}</p>
     </div>
