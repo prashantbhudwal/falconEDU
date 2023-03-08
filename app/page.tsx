8fce4dfef80e194081aa8aa21064d488dd2be484
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-08 14:47:14 +0530

    Add chatResponse prop

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 import { chatArray, buttonsArray } from "@/app/schema";
 import Chat from "./chat";
 
+const chatResponse =
+  "This is a simulation of chat response from the API route.";
+
 export default function Home() {
-  return <Chat chatArray={chatArray} buttonsArray={buttonsArray} />;
+  return (
+    <Chat
+      chatArray={chatArray}
+      buttonsArray={buttonsArray}
+      chatResponse={chatResponse}
+    />
+  );
 }
