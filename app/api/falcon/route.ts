b5745b703914b7f087ff05a45eca6cc0d9d08e22
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-08 18:17:14 +0530

    Add API route for fetching data from openai

--- app/api/falcon/route.ts
+++ app/api/falcon/route.ts
@@ -7,8 +7,22 @@ const configuration = new Configuration({
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-
+  const prompt = body.prompt;
   const openai = new OpenAIApi(configuration);
+  async function fetchCompletion() {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `I am a student in grade 6, give me an example of: ${prompt}`,
+        },
+      ],
+    });
+    return completion.data.choices[0].message;
+  }
 
-  return NextResponse.json({ message: "Hello World" });
+  const response = await fetchCompletion();
+
+  return NextResponse.json({ response });
 }
