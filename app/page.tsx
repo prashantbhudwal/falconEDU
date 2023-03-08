2f21501891416e0addd716e77dfb5a8a69504b9e
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-08 11:02:58 +0530

    Add more ways to explain

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,16 @@ import Block from "@/components/Block";
 import LiveBlock from "@/components/LiveBlock";
 const text =
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto iure quos placeat quibusdam laudantium sunt ullam voluptatem eius atque id dolore itaque inventore ex mollitia, illo tempore vel, nam quis natus commodi ut repudiandae. Soluta saepe beatae iusto voluptatem ipsa nam facilis eum iure eligendi deleniti? Officiis ut voluptatum vero?";
-const buttonsArray = ["Explain", "Example", "Story", "Analogy", "History"];
+const buttonsArray = [
+  "Explain",
+  "Example",
+  "Story",
+  "Analogy",
+  "History",
+  "Application",
+  "Anti-Example",
+  "Contrast",
+];
 const chatArray = [
   {
     message:
