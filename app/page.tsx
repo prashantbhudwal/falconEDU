0ad031845698d33e6bc130f27f3889cce7a3b471
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-09 18:45:21 +0530

    Minor style fixes

--- app/page.tsx
+++ app/page.tsx
@@ -11,15 +11,24 @@ export default function Home() {
   };
   if (topic === "") {
     return (
-      <div className="flex flex-col gap-2 items-center m-4">
+      <div className="flex flex-col gap-4 items-center m-4">
+        <h1 className=" text-slate-600 p-4 text-xl">
+          What would you like to learn today?
+        </h1>
         <input
+          placeholder="Enter a topic"
           required
           type="text"
           onChange={handleChange}
           value={input}
-          className="border-slate-700 rounded-md bg-slate-300 text-black p-3  w-96"
+          className="border-slate-700 rounded-md bg-slate-300 text-black p-4  w-96"
         />
-        <Button onClick={() => setTopic(input)}>Start</Button>
+        <button
+          className="bg-emerald-500 ring-1 ring-slate-700 text-slate-700 rounded-md px-8 py-2 text-lg font-medium capitalize"
+          onClick={() => setTopic(input)}
+        >
+          Start
+        </button>
       </div>
     );
   }
