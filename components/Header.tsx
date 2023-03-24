c05088d53a905cd8e86cae7d1a255777f3952453
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-24 18:30:04 +0530

    Display the grade and board in the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,20 @@
+"use client";
+import { useAppState } from "@/app/context/app-context";
+
 export default function Header() {
+  const { grade } = useAppState();
+
   return (
-    <header className="border-b border-solid border-slate-700 text-slate-200 p-4">
-      <h1 className="text-2xl">Falcon One</h1>
-      <p className="text-sm text-slate-600"> Your Teaching Assistant</p>
+    <header className="border-b border-solid border-slate-700 text-slate-200 p-4 pr-6">
+      <div className="flex items-center justify-between">
+        <div>
+          <h1 className="text-2xl">Falcon One</h1>
+          <p className="text-sm  text-slate-600">Your Teaching CoPilot</p>
+        </div>
+        {grade && (
+          <span className="text-base ml-2 font-semibold">{`NCERT, Grade ${grade}`}</span>
+        )}
+      </div>
     </header>
   );
 }
