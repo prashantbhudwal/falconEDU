ba8955e528e8366475134cf7ba89300c9877d84a
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-28 21:54:56 +0530

    Add basic preferences

--- app/preferences/page.tsx
+++ app/preferences/page.tsx
@@ -1,7 +1,79 @@
-export default function Preferences() {
+"use client";
+import { useState } from "react";
+import grades from "../data/grades.json";
+import { useAppState } from "../context/app-context";
+
+export default function Home() {
+  const { topic, subtopic, grade, setTopic, setSubtopic, setGrade } =
+    useAppState();
+  const [started, setStarted] = useState(false);
+
+  const handleGradeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setGrade(e.target.value);
+  };
+
+  const handleChapterChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setTopic(e.target.value);
+  };
+
+  const handleTopicChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSubtopic(e.target.value);
+  };
+
+  const handleClick = () => {
+    setStarted(true);
+  };
+
   return (
-    <div className="bg-slate-800 text-slate-300 px-5 py-3 rounded-lg ring-1 ring-slate-500">
-      <p className="leading-6 text-base pt-2">Preferences</p>
+    <div className="flex flex-col gap-4 items-center m-4">
+      <select
+        onChange={handleGradeChange}
+        value={grade}
+        className="border-slate-700 rounded-md bg-slate-300 text-black p-4 w-96"
+      >
+        <option value="">Select a grade</option>
+        {grades.map((g) => (
+          <option key={g.id} value={g.grade}>
+            Grade {g.grade}
+          </option>
+        ))}
+      </select>
+      <select
+        onChange={handleChapterChange}
+        value={topic}
+        className="border-slate-700 rounded-md bg-slate-300 text-black p-4 w-96"
+      >
+        <option value="">Select a chapter</option>
+        {grades
+          .find((g) => g.grade === grade)
+          ?.chapters.map((c) => (
+            <option key={c.id} value={c.name}>
+              {c.name}
+            </option>
+          ))}
+      </select>
+      <select
+        onChange={handleTopicChange}
+        value={subtopic}
+        className="border-slate-700 rounded-md bg-slate-300 text-black p-4 w-96"
+      >
+        <option value="">Select a topic</option>
+        {grades
+          .find((g) => g.grade === grade)
+          ?.chapters.find((c) => c.name === topic)
+          ?.topics.map((t) => (
+            <option key={t.id} value={t.name}>
+              {t.name}
+            </option>
+          ))}
+      </select>
+      <button
+        className="bg-emerald-500 ring-1 ring-slate-700 text-slate-700 rounded-md px-8 py-2 text-lg font-medium capitalize"
+        onClick={handleClick}
+        disabled={!topic || !subtopic || !grade}
+      >
+        Start
+      </button>
     </div>
   );
 }
