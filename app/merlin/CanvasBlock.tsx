364f40b132b40bf44a5e04486d1d4e24a6f88cc2
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-29 22:22:37 +0530

    Style the component

--- app/merlin/CanvasBlock.tsx
+++ app/merlin/CanvasBlock.tsx
@@ -1,7 +1,7 @@
 export default function CanvasBlock({ generatedData }: { generatedData: any }) {
   return (
-    <div className="bg-slate-800 text-slate-300 px-5 py-3 rounded-lg ring-1 ring-slate-500">
-      <p className="leading-6 text-base pt-2">{JSON.stringify(generatedData)}</p>
+    <div className="bg-slate-800 text-slate-300 px-5 py-3 rounded-lg  shadow-sm shadow-emerald-600 max-w-4xl">
+      <p className="leading-7 text-xl pt-2">{generatedData.text}</p>
     </div>
   );
 }
