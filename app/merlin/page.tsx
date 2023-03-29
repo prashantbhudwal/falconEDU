2cc6898efb97329e982ad6ba937f30c52a2ae715
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-29 22:23:27 +0530

    Update layout

--- app/merlin/page.tsx
+++ app/merlin/page.tsx
@@ -14,19 +14,27 @@ export default function Merlin() {
   };
 
   const generatedData = {
-    text: "This is a generated text",
+    text: "This is a generated text. This is a generated text. This is a generated text. This is a generated text. This is a generated text. This is a generated text. This is a generated text. This is a generated text. This is a generated text. This is a generated text. This is a generated text. This is a generated text.",
   };
   return (
-    <div className="grid grid-cols-3 md:grid-cols-3 gap-4">
-      <TopBar className="col-span-3" />
-      <Sidebar className="col-span-1">
+    <div className="grid grid-cols-12 gap-4 w-full">
+      <TopBar className="col-start-1 col-span-full h-16 max-h-16" />
+      <Sidebar
+        className="col-start-1 col-span-2 row-start-2"
+        heading={"Blocks"}
+      >
+        <Chip promptData={data} />
+        <Chip promptData={data} />
         <Chip promptData={data} />
       </Sidebar>
-      <Canvas className="col-span-1">
+      <Canvas className="col-start-3 col-span-8 h-screen">
+        <CanvasBlock generatedData={generatedData} />
+        <CanvasBlock generatedData={generatedData} />
         <CanvasBlock generatedData={generatedData} />
       </Canvas>
-      <Sidebar className="col-span-1">
-        <Chip promptData={data} />
+      <Sidebar className="col-start-11 col-span-2" heading={"Outline"}>
+        1. This is the text <br /> 2. More text <br />
+        1. This is the text <br /> 2. More text <br />
       </Sidebar>
     </div>
   );
