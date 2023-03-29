88d46ee68ff036571548ecfeb780b40019069afa
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-29 22:29:13 +0530

    Update chip typography & spacing

--- app/merlin/Chip.tsx
+++ app/merlin/Chip.tsx
@@ -1,8 +1,8 @@
 export default function Chip({ promptData }: { promptData: any }) {
   const displayText = promptData.name;
   return (
-    <div className="bg-slate-800 text-slate-300 px-5 py-3 rounded-lg ring-1 ring-slate-500">
-      <p className="leading-6 text-base pt-2">{displayText}</p>
+    <div className="bg-slate-800 text-slate-300 px-4 py-4 rounded-sm ring-1 ring-slate-500">
+      <p className="leading-6 text-lg uppercase text-center">{displayText}</p>
     </div>
   );
 }
