845a60725f48517bf78acdcf4b28e36caa34e419
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-31 20:54:58 +0530

    Add fetching boilerplate

--- app/merlin/Canvas.tsx
+++ app/merlin/Canvas.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { useDrop, DropTargetMonitor } from "react-dnd";
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import CanvasBlock from "./CanvasBlock";
 import { blockContentArray } from "../utils";
+import useFalconStream from "@/hooks/useOpenAIStream";
 
 export default function Canvas({ className }: { className?: string }) {
+  const [messages, setMessages] = useState<string[]>([]);
   const [blockType, setBlockType] = useState<string>("");
   const specObject = {
     accept: "Box",
@@ -14,22 +16,35 @@ export default function Canvas({ className }: { className?: string }) {
       canDrop: monitor.canDrop(),
     }),
   };
-  const [{ canDrop, isOver }, drop] = useDrop(() => specObject);
+  const [{ isOver }, drop] = useDrop(() => specObject);
+
+  const handleNewMessage = useCallback((message: string) => {
+    setMessages((prevMessages) => [...prevMessages, message]);
+  }, []);
+
+  const { isLoading, error } = useFalconStream(
+    "Can you write a note on JayZ",
+    handleNewMessage
+  );
+
+  useEffect(() => {
+    if (blockType) {
+      setMessages([]);
+    }
+  }, [blockType]);
 
   return (
     <div
       ref={drop}
       role={"Board"}
-      className={`${className} flex flex-col items-center gap-4   text-slate-300 px-5 py-3 rounded-lg ring-2 ring-emerald-500 shadow-emerald-500 shadow-md ${
-        isOver
-          ? "shadow-inner backdrop-blur-xl bg-emerald-900"
-          : "shadow-md bg-slate-900"
+      className={`${className} flex flex-col items-center gap-4   text-slate-300 px-5 py-3 rounded-lg ring-2 ring-emerald-500 shadow-emerald-500 ${
+        isOver ? "shadow-inner bg-emerald-900" : "shadow-md bg-slate-900"
       }`}
     >
       <header className="font-medium text-emerald-500 text-center border-b border-solid border-emerald-700 pb-2">
         <p className="uppercase">Canvas</p>
       </header>
-      {JSON.stringify(blockType)}
+      <CanvasBlock text={messages} emoji={"🥸"} type={blockType} key={"test"} />
       {blockContentArray.map((block: any) => {
         return <CanvasBlock {...block} key={block.id} />;
       })}
