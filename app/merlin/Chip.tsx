13931cbd667e370ed8c840d0f9746ac042a5d23e
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-31 19:35:30 +0530

    Make chip draggable

--- app/merlin/Chip.tsx
+++ app/merlin/Chip.tsx
@@ -1,8 +1,31 @@
-export default function Chip({ promptData }: { promptData: any }) {
-  const displayText = promptData.name;
+"use client";
+import { useDrag, DragSourceMonitor } from "react-dnd";
+
+export default function Chip({ text }: { text: string }) {
+  const specObject = {
+    type: "Box",
+    item: { text },
+    collect: (monitor: DragSourceMonitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  };
+  const [{ isDragging }, drag, dragPreview] = useDrag(() => specObject);
   return (
-    <div className="bg-slate-800 text-slate-300 px-4 py-4 rounded-sm ring-1 ring-slate-500">
-      <p className="leading-6 text-lg uppercase text-center">{displayText}</p>
+    <div
+      ref={dragPreview}
+      className={` text-slate-300 px-3 py-3 rounded-xl ${
+        isDragging
+          ? "opacity-100 ring-emerald-500 ring-4 bg-emerald-800 "
+          : "opacity-100 ring-slate-500 ring-1 bg-slate-800"
+      }`}
+    >
+      <p
+        role={"Handle"}
+        ref={drag}
+        className={`leading-6 text-lg uppercase text-center `}
+      >
+        {text}
+      </p>
     </div>
   );
 }
