1ce7433be4a0cb9ff766d5bb9096bf31f4154bd3
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-31 12:01:07 +0530

    API route to handle stream:true from OpenAI

--- app/api/falconStream/route.ts
+++ app/api/falconStream/route.ts
@@ -1,25 +1,79 @@
-import { getPrompt } from "@/app/utils";
-import { NextRequest, NextResponse } from "next/server";
-import getCompletion from "@/app/api/openAI";
-import { ChatCompletionRequestMessage } from "openai";
+// app/api/route.ts
+import { Configuration, OpenAIApi } from "openai";
+import { NextResponse, NextRequest } from "next/server";
+
+export const runtime = "nodejs";
+// This is required to enable streaming
+export const dynamic = "force-dynamic";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  const { topic, subtopic, grade, promptType } = body;
+  const prompt = body.prompt;
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
 
-  const prompt = getPrompt(promptType);
+  let responseStream = new TransformStream();
+  const writer = responseStream.writable.getWriter();
+  const encoder = new TextEncoder();
 
-  const messages: ChatCompletionRequestMessage[] = [
-    {
-      role: "system",
-      content: `You are a knowledgeable teaching assistant. I am a teacher in India teaching grade ${grade} science at a school that follows NCERT textbooks. I am teaching the chapter "${subtopic}" from the chapter "${topic}". My students' primary language is not English. Help me brainstorm ideas and strategies for effectively teaching this topic in my classroom. Keep your answers concise.`,
+  // writer.write(encoder.encode("streaming")); // Send "streaming" message to the client
+
+  try {
+    const openaiRes = await openai.createChatCompletion(
+      {
+        model: "gpt-3.5-turbo",
+        stream: true,
+        max_tokens: 100,
+        messages: [
+          {
+            role: "user",
+            content: prompt,
+          },
+        ],
+      },
+      { responseType: "stream" }
+    );
+
+    // @ts-ignore
+    openaiRes.data.on("data", async (data: Buffer) => {
+      const lines = data
+        .toString()
+        .split("\n")
+        .filter((line: string) => line.trim() !== "");
+      for (const line of lines) {
+        const message = line.replace(/^data: /, "");
+        if (message === "[DONE]") {
+          console.log("Stream completed");
+          writer.close();
+          return;
+        }
+        try {
+          const parsed = JSON.parse(message);
+          console.log(parsed);
+          console.log(parsed.choices[0].delta);
+          const filteredContent = parsed.choices[0].delta.content
+            ? parsed.choices[0].delta?.content
+            : "";
+
+          await writer.write(encoder.encode(`${filteredContent}`));
+        } catch (error) {
+          console.error("Could not JSON parse stream message", message, error);
+        }
+      }
+    });
+  } catch (error) {
+    console.error("An error occurred during OpenAI request", error);
+    writer.write(encoder.encode("An error occurred during OpenAI request"));
+    writer.close();
+  }
+
+  return new Response(responseStream.readable, {
+    headers: {
+      "Content-Type": "text/event-stream",
+      Connection: "keep-alive",
+      "Cache-Control": "no-cache, no-transform",
     },
-    {
-      role: "user",
-      content: `${prompt}: ${subtopic} from the chapter ${topic} of grade ${grade} NCERT science textbook.`,
-    },
-  ];
-  const openAIresponse = await getCompletion(messages);
-  const response = { ...openAIresponse, topic, promptType };
-  return NextResponse.json({ response });
+  });
 }
