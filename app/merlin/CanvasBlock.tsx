0abc5eb218f082b366fb7656bea5dc22cd82f2ce
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-31 19:37:12 +0530

    Adjust styles for better readability

--- app/merlin/CanvasBlock.tsx
+++ app/merlin/CanvasBlock.tsx
@@ -1,7 +1,7 @@
 export default function CanvasBlock({ generatedData }: { generatedData: any }) {
   return (
-    <div className="bg-slate-800 text-slate-300 px-5 py-3 rounded-lg  shadow-sm shadow-emerald-600 max-w-4xl">
-      <p className="leading-7 text-xl pt-2">{generatedData.text}</p>
+    <div className="bg-slate-800 text-neutral-300 px-5 py-3 rounded-lg  shadow-sm shadow-emerald-600 max-w-4xl">
+      <p className="leading-7 text-lg pt-2">{generatedData.text}</p>
     </div>
   );
 }
