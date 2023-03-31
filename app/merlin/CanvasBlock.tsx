53f38be5c93bc3a174b878830a392bf50e97b3e7
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-31 19:55:59 +0530

    Add blockType, blockEmoji and displayText

--- app/merlin/CanvasBlock.tsx
+++ app/merlin/CanvasBlock.tsx
@@ -1,7 +1,19 @@
-export default function CanvasBlock({ generatedData }: { generatedData: any }) {
+export default function CanvasBlock({
+  text: displayText,
+  emoji: blockEmoji,
+  type: blockType,
+}: {
+  text: string;
+  emoji: string;
+  type: string;
+}) {
   return (
-    <div className="bg-slate-800 text-neutral-300 px-5 py-3 rounded-lg  shadow-sm shadow-emerald-600 max-w-4xl">
-      <p className="leading-7 text-lg pt-2">{generatedData.text}</p>
+    <div className="bg-slate-800 text-neutral-300 px-5 py-3 rounded-lg  shadow-sm shadow-emerald-600 max-w-4xl w-full">
+      <header className="text-xs font-medium text-slate-500 text-right flex justify-between items-baseline border-b border-solid border-slate-700 pb-2 ">
+        <p className="uppercase">{blockType}</p>
+        <p className="text-base">{blockEmoji}</p>
+      </header>
+      <p className="leading-7 text-lg pt-2">{displayText}</p>
     </div>
   );
 }
