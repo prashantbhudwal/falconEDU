505cc4c75f05663390b9389c1b7d54faddee55b7
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-31 19:36:21 +0530

    Add react dnd context

--- app/merlin/page.tsx
+++ app/merlin/page.tsx
@@ -1,9 +1,13 @@
+"use client";
 import Canvas from "./Canvas";
 import Chip from "./Chip";
 import Sidebar from "./Sidebar";
 import TopBar from "./TopBar";
 import CanvasBlock from "./CanvasBlock";
 import OutlineBlock from "./OutlineBlock";
+import { buttonsArray as promptsArray } from "../schema";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd";
 
 export default function Merlin() {
   const data = {
@@ -18,27 +22,32 @@ export default function Merlin() {
     text: "This is a generated text. This is a generated text. This is a generated text. This is a generated text. This is a generated text. This is a generated text. This is a generated text. This is a generated text. This is a generated text. This is a generated text. This is a generated text. This is a generated text.",
   };
   return (
-    <div className="grid grid-cols-12 gap-4 w-full">
-      <TopBar className="col-start-1 col-span-full h-16 max-h-16" />
-      <Sidebar
-        className="col-start-1 col-span-2 row-start-2"
-        heading={"Blocks"}
-      >
-        <Chip promptData={data} />
-        <Chip promptData={data} />
-        <Chip promptData={data} />
-      </Sidebar>
-      <Canvas className="col-start-3 col-span-8 h-screen">
-        <CanvasBlock generatedData={generatedData} />
-        <CanvasBlock generatedData={generatedData} />
-        <CanvasBlock generatedData={generatedData} />
-      </Canvas>
-      <Sidebar className="col-start-11 col-span-2" heading={"Outline"}>
-        <OutlineBlock>
-          1. This is the text <br /> 2. More text <br />
-          1. This is the text <br /> 2. More text <br />
-        </OutlineBlock>
-      </Sidebar>
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className="grid grid-cols-12 gap-4 w-full">
+        {/* <TopBar className="col-start-1 col-span-full h-16 max-h-16" /> */}
+        <Sidebar
+          className="col-start-1 col-span-2 row-start-1"
+          heading={"Blocks"}
+        >
+          {promptsArray.map((buttonText: string) => (
+            <Chip key={buttonText} text={buttonText} />
+          ))}
+        </Sidebar>
+        <Canvas className="col-start-3 col-span-8 h-screen overflow-y-auto custom-scrollbar">
+          <CanvasBlock generatedData={generatedData} />
+          <CanvasBlock generatedData={generatedData} />
+          <CanvasBlock generatedData={generatedData} />
+          <CanvasBlock generatedData={generatedData} />
+          <CanvasBlock generatedData={generatedData} />
+          <CanvasBlock generatedData={generatedData} />
+        </Canvas>
+        <Sidebar className="col-start-11 col-span-2" heading={"Outline"}>
+          <OutlineBlock>
+            1. This is the text <br /> 2. More text <br />
+            1. This is the text <br /> 2. More text <br />
+          </OutlineBlock>
+        </Sidebar>
+      </div>
+    </DndProvider>
   );
 }
