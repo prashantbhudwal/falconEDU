d8b2c788d333a95e55c34d6630d91069a85d8de0
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-05 21:45:59 +0530

    Fix typography

--- components/DesktopOnly.tsx
+++ components/DesktopOnly.tsx
@@ -1,8 +1,10 @@
 export default function DesktopOnly() {
   return (
     <>
-      <p className="text-2xl text-emerald-600 font-semibold mb-4">Oops! 🙈</p>
-      <p className="text-lg text-gray-700">
+      <p className="text-2xl text-emerald-600 font-semibold mb-4 text-center">
+        Oops! 🙈
+      </p>
+      <p className="text-lg text-gray-700 text-center">
         This app is currently optimized for desktop usage only.
       </p>
     </>
