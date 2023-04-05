5b12b929a64fe6c62dc2a567a029e9f28478a16e
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-05 20:41:56 +0530

    Add placeholder component for mobile screens

---
+++ components/DesktopOnly.tsx
@@ -0,0 +1,10 @@
+export default function DesktopOnly() {
+  return (
+    <>
+      <p className="text-2xl text-emerald-600 font-semibold mb-4">Oops! 🙈</p>
+      <p className="text-lg text-gray-700">
+        This app is currently optimized for desktop usage only.
+      </p>
+    </>
+  );
+}
