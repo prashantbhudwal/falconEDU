7b048d81d964fad301bc702f9bab978267186f19
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-05 21:43:11 +0530

    Add new preferences route

--- app/preferences/page.tsx
+++ app/preferences/page.tsx
@@ -1,13 +1,12 @@
 "use client";
-import { useState } from "react";
 import grades from "../data/grades.json";
 import { useAppState } from "../context/app-context";
-import Select from "@/components/Select";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   const { topic, subtopic, grade, setTopic, setSubtopic, setGrade } =
     useAppState();
-  const [started, setStarted] = useState(false);
 
   const handleGradeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setGrade(e.target.value);
@@ -21,51 +20,65 @@ export default function Home() {
     setSubtopic(e.target.value);
   };
 
-  const handleClick = () => {
-    setStarted(true);
-  };
-
-  const gradeOptions = grades.map((g) => ({
-    id: g.id,
-    name: `Grade ${g.grade}`,
-  }));
-  const chapterOptions =
-    grades
-      .find((g) => g.grade.toString() === grade)
-      ?.chapters.map((c) => ({ id: c.id, name: c.name })) || [];
-  const topicOptions =
-    grades
-      .find((g) => g.grade.toString() === grade)
-      ?.chapters.find((c) => c.name === topic)
-      ?.topics.map((t) => ({ id: t.id, name: t.name })) || [];
-
   return (
     <div className="flex flex-col gap-4 items-center m-4">
-      <Select
-        options={gradeOptions}
-        value={grade}
+      <select
         onChange={handleGradeChange}
-        label="a grade"
-      />
-      <Select
-        options={chapterOptions}
-        value={topic}
-        onChange={handleChapterChange}
-        label="a chapter"
-      />
-      <Select
-        options={topicOptions}
-        value={subtopic}
-        onChange={handleTopicChange}
-        label="a topic"
-      />
-      <button
-        className="bg-fuchsia-500 ring-1 ring-slate-700 text-slate-800 rounded-md px-8 py-2 text-lg font-medium capitalize"
-        onClick={handleClick}
-        disabled={!topic || !subtopic || !grade}
+        value={grade}
+        className="border-slate-700 rounded-md bg-slate-300 text-black p-4 w-96"
       >
-        Save
-      </button>
+        <option value="">Select a grade</option>
+        {grades.map((g) => (
+          <option key={g.id} value={g.grade}>
+            Grade {g.grade}
+          </option>
+        ))}
+      </select>
+      <select
+        onChange={handleChapterChange}
+        value={topic}
+        className="border-slate-700 rounded-md bg-slate-300 text-black p-4 w-96"
+      >
+        <option value="">Select a chapter</option>
+        {grades
+          .find((g) => g.grade === grade)
+          ?.chapters.map((c) => (
+            <option key={c.id} value={c.name}>
+              {c.name}
+            </option>
+          ))}
+      </select>
+      <select
+        onChange={handleTopicChange}
+        value={subtopic}
+        className="border-slate-700 rounded-md bg-slate-300 text-black p-4 w-96"
+      >
+        <option value="">Select a topic</option>
+        {grades
+          .find((g) => g.grade === grade)
+          ?.chapters.find((c) => c.name === topic)
+          ?.topics.map((t) => (
+            <option key={t.id} value={t.name}>
+              {t.name}
+            </option>
+          ))}
+      </select>
+      <div className="flex flex-row gap-4">
+        {/* <button
+          className="bg-emerald-500 ring-1 ring-slate-700 text-slate-700 rounded-md px-8 py-2 text-lg font-medium capitalize"
+          onClick={() => router.push("/chat")}
+          disabled={!topic || !subtopic || !grade}
+        >
+          Mobile
+        </button> */}
+        <button
+          className="bg-emerald-400 ring-1 ring-slate-700 text-slate-700 rounded-md px-8 py-2 text-lg font-medium capitalize disabled:opacity-50"
+          onClick={() => router.push("/merlin")}
+          disabled={!topic || !subtopic || !grade}
+        >
+          New Lesson
+        </button>
+      </div>
     </div>
   );
 }
