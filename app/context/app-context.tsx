7197cafd17878b9a8aa78797f171e938b00b0382
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-06 09:42:25 +0530

    Add currentLesson state to context

--- app/context/app-context.tsx
+++ app/context/app-context.tsx
@@ -1,23 +1,32 @@
 "use client";
 
 import React, { createContext, useContext, useState } from "react";
-
+interface BlockContent {
+  text: string | string[];
+  id: string;
+  type: string;
+  emoji: string;
+}
 interface AppState {
   grade: string;
   topic: string;
   subtopic: string;
+  currentLesson: BlockContent[];
   setGrade: React.Dispatch<React.SetStateAction<string>>;
   setTopic: React.Dispatch<React.SetStateAction<string>>;
   setSubtopic: React.Dispatch<React.SetStateAction<string>>;
+  setCurrentLesson: React.Dispatch<React.SetStateAction<BlockContent[]>>;
 }
 
 const AppContext = createContext<AppState>({
   grade: "",
   topic: "",
   subtopic: "",
+  currentLesson: [],
   setGrade: () => {},
   setTopic: () => {},
   setSubtopic: () => {},
+  setCurrentLesson: () => {},
 });
 
 export const useAppState = () => useContext(AppContext);
@@ -28,6 +37,7 @@ export const AppProvider: React.FC<React.PropsWithChildren<{}>> = ({
   const [grade, setGrade] = useState<string>("");
   const [topic, setTopic] = useState<string>("");
   const [subtopic, setSubtopic] = useState<string>("");
+  const [currentLesson, setCurrentLesson] = useState<BlockContent[]>([]);
 
   return (
     <AppContext.Provider
@@ -35,9 +45,11 @@ export const AppProvider: React.FC<React.PropsWithChildren<{}>> = ({
         grade,
         topic,
         subtopic,
+        currentLesson,
         setGrade,
         setTopic,
         setSubtopic,
+        setCurrentLesson,
       }}
     >
       {children}
