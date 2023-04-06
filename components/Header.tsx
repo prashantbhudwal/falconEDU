f56bc839cc06785b58700bb1503e207469df204b
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-06 10:02:28 +0530

    Display the generate lesson button when the app is started

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import { useAppState } from "@/app/context/app-context";
 import Link from "next/link";
 
 export default function Header() {
-  const { grade } = useAppState();
+  const { grade, started } = useAppState();
 
   return (
     <header className="border-b border-solid border-slate-700 text-slate-200 py-3 pl-4 pr-6">
@@ -17,6 +17,14 @@ export default function Header() {
         <div className="flex items-center gap-6">
           {grade && (
             <span className="text-base ml-2 font-semibold">{`NCERT, Grade ${grade}`}</span>
+          )}
+          {started && (
+            <Link
+              href={"/lesson"}
+              className="bg-emerald-500 hover:bg-emerald-600 text-white font-medium py-2 px-4 rounded"
+            >
+              Generate Lesson
+            </Link>
           )}
         </div>
       </div>
