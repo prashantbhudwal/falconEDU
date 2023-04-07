2c18e539c173b85ec46628fe1f2ed6585c76ce30
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-07 20:00:38 +0530

    Remove redundant component

--- app/merlin/TopBar.tsx
+++
@@ -1,7 +0,0 @@
-export default function TopBar({ className }: { className?: string }) {
-  return (
-    <div className={className}>
-      <h1 className="text-2xl font-bold text-slate-300">Merlin</h1>
-    </div>
-  );
-}
