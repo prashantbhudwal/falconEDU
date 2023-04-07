a95fd7601f321f996813f06ba0b19026638cc298
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-07 18:50:30 +0530

    Fix typography

--- app/merlin/OutlineBlock.tsx
+++ app/merlin/OutlineBlock.tsx
@@ -1,3 +1,7 @@
 export default function OutlineBlock({ children }: any) {
-  return <div className="text-xl text-slate-400 leading-8">{children}</div>;
+  return (
+    <div className="text-lg text-slate-400 leading-7 capitalize pl-4">
+      {children}
+    </div>
+  );
 }
