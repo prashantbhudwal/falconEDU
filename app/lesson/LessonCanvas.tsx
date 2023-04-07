a2738fde634bcf336f7f97caf47c24d853b2a9c5
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-07 20:52:18 +0530

    Add a lesson canvas component

--- app/lesson/LessonCanvas.tsx
+++ app/lesson/LessonCanvas.tsx
@@ -1,11 +1,13 @@
 "use client";
+import { useDrop, DropTargetMonitor } from "react-dnd";
 import { useState, useEffect, useCallback } from "react";
-import CanvasBlock from "@/app/merlin/CanvasBlock";
+import CanvasBlock from "../merlin/CanvasBlock";
 import useFalconStream from "@/hooks/useFalconStream";
 import { useAppState } from "../context/app-context";
 import { getEmoji } from "../utils";
 import { v4 as uuid } from "uuid";
-import LiveBlock from "@/app/merlin/LiveBlock";
+import LiveBlock from "../merlin/LiveBlock";
+import useLessonStream from "@/hooks/useLessonStream";
 
 interface BlockContent {
   text: string | string[];
@@ -15,8 +17,70 @@ interface BlockContent {
 }
 
 export default function Canvas({ className }: { className?: string }) {
-  const { topic, subtopic, currentLesson } = useAppState();
-  console.log(currentLesson);
+  const [fetchNow, setFetchNow] = useState(false);
+  const [messages, setMessages] = useState<string[]>([]);
+  const [streamCompleted, setStreamCompleted] = useState<boolean>();
+  const [currentBlockId, setCurrentBlockId] = useState<string>("");
+  const [lastBlockId, setLastBlockId] = useState<string>("");
+  const [lessonPlanBlockContent, setLessonPlanBlockContent] = useState<
+    BlockContent[]
+  >([]);
 
-  return <></>;
+  useEffect(() => {
+    setMessages([]);
+    setStreamCompleted(false);
+    setFetchNow(true);
+  }, []);
+
+  const { topic, subtopic, currentLesson: blockContent } = useAppState();
+
+  const handleNewMessage = useCallback((message: string) => {
+    setMessages((prevMessages) => [...prevMessages, message]);
+  }, []);
+
+  const { isLoading, error } = useLessonStream(
+    handleNewMessage,
+    fetchNow,
+    () => setFetchNow(false),
+    () => setStreamCompleted(true),
+    () => setCurrentBlockId(uuid())
+  );
+
+  useEffect(() => {
+    if (
+      streamCompleted === true &&
+      messages.length > 0 &&
+      currentBlockId != lastBlockId
+    ) {
+      const randomId = uuid();
+      const emoji = "📝";
+      setLessonPlanBlockContent((prevBlockContent) => [
+        ...prevBlockContent,
+        {
+          text: messages,
+          id: randomId,
+          type: "lessonPlan",
+          emoji: emoji,
+        },
+      ]);
+      setLastBlockId(currentBlockId);
+    }
+  }, [streamCompleted]);
+
+  return (
+    <div
+      className={`${className} flex flex-col items-center gap-4   text-slate-300 px-5 py-3 rounded-lg ring-2 ring-emerald-500 shadow-emerald-500 ${"shadow-md bg-slate-900"}`}
+    >
+      <header className="font-medium text-emerald-500 text-center border-b border-solid border-emerald-700 pb-2">
+        <h1 className="text-xl">{subtopic}</h1>
+        <p className=" text-base  text-slate-600">Chapter: {topic}</p>
+      </header>
+      <LiveBlock
+        text={messages}
+        emoji={"📝"}
+        type={"application"}
+        key={"test"}
+      />
+    </div>
+  );
 }
