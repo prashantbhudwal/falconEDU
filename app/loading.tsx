d238fee5e40e87cd7fbefb1e2eca4c32f4fbdbe7
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-07 18:31:09 +0530

    Add loading

---
+++ app/loading.tsx
@@ -0,0 +1,24 @@
+export default function Loading() {
+  return (
+    <div className="bg-slate-900 text-slate-900 px-5 py-3 rounded-lg ring-2 ring-emerald-500 shadow-emerald-500 w-screen">
+      <div className="animate-pulse flex flex-col items-center gap-4">
+        <div className="w-full h-6 bg-slate-900 rounded-full animate-pulse"></div>
+        <div className="w-full h-48 bg-slate-900 rounded-lg animate-pulse">
+          <div className="w-1/3 h-4 my-2 mx-2 bg-slate-800 rounded-full"></div>
+          <div className="w-1/2 h-4 my-2 mx-2 bg-slate-800 rounded-full"></div>
+          <div className="w-1/4 h-4 my-2 mx-2 bg-slate-800 rounded-full"></div>
+        </div>
+        <div className="w-full h-48 bg-slate-900 rounded-lg animate-pulse">
+          <div className="w-1/2 h-4 my-2 mx-2 bg-slate-800 rounded-full"></div>
+          <div className="w-1/4 h-4 my-2 mx-2 bg-slate-800 rounded-full"></div>
+          <div className="w-1/3 h-4 my-2 mx-2 bg-slate-800 rounded-full"></div>
+        </div>
+        <div className="w-full h-48 bg-slate-900 rounded-lg animate-pulse">
+          <div className="w-1/4 h-4 my-2 mx-2 bg-slate-800 rounded-full"></div>
+          <div className="w-1/3 h-4 my-2 mx-2 bg-slate-800 rounded-full"></div>
+          <div className="w-1/2 h-4 my-2 mx-2 bg-slate-800 rounded-full"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
