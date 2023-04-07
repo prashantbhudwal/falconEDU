3a833a43bc92c00a9e81bb26a4a545de7778ddbb
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-07 20:32:46 +0530

    Create api route for lesson generation

---
+++ app/api/lessonGenerator/route.ts
@@ -0,0 +1,48 @@
+// app/api/route.ts
+import { NextRequest } from "next/server";
+import { getLessonCompletionStream, handleGPT3TurboStreamData } from "../openAI";
+import { ChatCompletionRequestMessage } from "openai";
+import { getPrompt } from "@/app/utils";
+
+export const runtime = "nodejs";
+// This is required to enable streaming
+export const dynamic = "force-dynamic";
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const { topic, subtopic, grade, promptType, ideaArray } = body;
+  // const prompt = getPrompt(promptType);
+
+  const messages: ChatCompletionRequestMessage[] = [
+    {
+      role: "system",
+      content: `You are a knowledgeable teaching assistant. I am a teacher in India teaching grade ${grade} science at a school that follows NCERT textbooks. I am teaching the chapter "${subtopic}" from the chapter "${topic}". My students' primary language is not English.`,
+    },
+    {
+      role: "user",
+      content: `Give me a lesson plan, that uses the following ideas that I have come up with ${ideaArray}`,
+    },
+  ];
+  let responseStream = new TransformStream();
+  const writer = responseStream.writable.getWriter();
+  try {
+    const openaiRes = await getLessonCompletionStream(messages);
+    // console.log(openaiRes.headers["x-request-id"]);
+    // @ts-ignore
+    openaiRes.data.on("data", (data: Buffer) =>
+      handleGPT3TurboStreamData(data, writer)
+    );
+  } catch (error) {
+    console.error("An error occurred during OpenAI request", error);
+    const encoder = new TextEncoder();
+    writer.write(encoder.encode("An error occurred during OpenAI request"));
+    writer.close();
+  }
+  return new Response(responseStream.readable, {
+    headers: {
+      "Content-Type": "text/event-stream",
+      Connection: "keep-alive",
+      "Cache-Control": "no-cache, no-transform",
+    },
+  });
+}
