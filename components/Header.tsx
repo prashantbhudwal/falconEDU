83c7bf05dbd6d998ace5b5ea85c7980deef12fdd
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-07 19:59:18 +0530

    Hide Generate button if !currentLesson

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import { useAppState } from "@/app/context/app-context";
 import Link from "next/link";
 
 export default function Header() {
-  const { grade, started } = useAppState();
+  const { grade, started, currentLesson } = useAppState();
 
   return (
     <header className="border-b border-solid border-slate-700 text-slate-200 py-3 pl-4 pr-6">
@@ -18,7 +18,7 @@ export default function Header() {
           {grade && (
             <span className="text-base ml-2 font-semibold">{`NCERT, Grade ${grade}`}</span>
           )}
-          {started && (
+          {started && currentLesson.length !== 0 && (
             <Link
               href={"/lesson"}
               className="bg-emerald-500 hover:bg-emerald-600 text-white font-medium py-2 px-4 rounded"
