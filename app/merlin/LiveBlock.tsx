ac2a05809c9ba98c481f9b48f23a46575ce8617d
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-07 16:20:08 +0530

    Change shadow of live block

--- app/merlin/LiveBlock.tsx
+++ app/merlin/LiveBlock.tsx
@@ -11,9 +11,7 @@ export default function LiveBlock({
 }) {
   return (
     <div
-      className={`bg-slate-800 text-neutral-300 px-5 py-3 rounded-lg shadow-sm ${getBlockShadow(
-        blockType
-      )} max-w-4xl w-full`}
+      className={`bg-slate-800 text-neutral-300 px-5 py-3 rounded-lg shadow-lg shadow-green-600 max-w-4xl w-full`}
     >
       <header className="text-xs font-medium text-slate-500 gap-3 text-right flex items-baseline border-b border-solid border-slate-700 pb-2 ">
         <p className="text-base">{blockEmoji}</p>
