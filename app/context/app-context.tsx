2c6e0f090b59903a4a78c95c0975c5282a4d21f9
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-07 22:14:06 +0530

    Add subject to context

--- app/context/app-context.tsx
+++ app/context/app-context.tsx
@@ -11,12 +11,14 @@ interface AppState {
   started: boolean;
   grade: string;
   board: string;
+  subject: string; // Add the subject to the AppState interface
   topic: string;
   subtopic: string;
   currentLesson: BlockContent[];
   setStarted: React.Dispatch<React.SetStateAction<boolean>>;
   setGrade: React.Dispatch<React.SetStateAction<string>>;
   setBoard: React.Dispatch<React.SetStateAction<string>>;
+  setSubject: React.Dispatch<React.SetStateAction<string>>; // Add the setSubject to the AppState interface
   setTopic: React.Dispatch<React.SetStateAction<string>>;
   setSubtopic: React.Dispatch<React.SetStateAction<string>>;
   setCurrentLesson: React.Dispatch<React.SetStateAction<BlockContent[]>>;
@@ -26,12 +28,14 @@ const AppContext = createContext<AppState>({
   started: false,
   grade: "",
   board: "",
+  subject: "", // Add the subject to the AppContext initial state
   topic: "",
   subtopic: "",
   currentLesson: [],
   setStarted: () => {},
   setGrade: () => {},
   setBoard: () => {},
+  setSubject: () => {}, // Add the setSubject to the AppContext initial state
   setTopic: () => {},
   setSubtopic: () => {},
   setCurrentLesson: () => {},
@@ -44,6 +48,7 @@ export const AppProvider: React.FC<React.PropsWithChildren<{}>> = ({
 }) => {
   const [grade, setGrade] = useState<string>("");
   const [board, setBoard] = useState<string>("");
+  const [subject, setSubject] = useState<string>(""); // Create the subject state
   const [topic, setTopic] = useState<string>("");
   const [subtopic, setSubtopic] = useState<string>("");
   const [currentLesson, setCurrentLesson] = useState<BlockContent[]>([]);
@@ -55,12 +60,14 @@ export const AppProvider: React.FC<React.PropsWithChildren<{}>> = ({
         started,
         grade,
         board,
+        subject, // Add the subject to the AppContext.Provider value
         topic,
         subtopic,
         currentLesson,
         setStarted,
         setGrade,
         setBoard,
+        setSubject, // Add the setSubject to the AppContext.Provider value
         setTopic,
         setSubtopic,
         setCurrentLesson,
