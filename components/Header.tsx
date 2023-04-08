43d07b512a3ad14d135eda0e2fd8761f39923e09
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-08 18:56:26 +0530

    New Lesson Button

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,11 @@
 import { useAppState } from "@/app/context/app-context";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 export default function Header() {
   const { grade, started, board, currentLesson } = useAppState();
   const pathname = usePathname();
+  const router = useRouter();
 
   return (
     <header className="border-b border-solid border-slate-700 text-slate-200 py-3 pl-4 pr-6">
@@ -26,6 +28,15 @@ export default function Header() {
               className="bg-emerald-500 hover:bg-emerald-600 text-white font-medium py-2 px-4 rounded"
             >
               Generate Lesson
+            </Link>
+          )}
+          {started && currentLesson.length !== 0 && pathname === "/lesson" && (
+            <Link
+              href={"/preferences"}
+              key={pathname} //rerenders the component when the path changes
+              className="bg-emerald-500 hover:bg-emerald-600 text-white font-medium py-2 px-4 rounded"
+            >
+              New Lesson
             </Link>
           )}
         </div>
