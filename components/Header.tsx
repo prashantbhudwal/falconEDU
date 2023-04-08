f8fae4e1684c50a857ee1fc5ef916fe5c87c8b73
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-08 18:20:59 +0530

    Display generate button only on merlin

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { useAppState } from "@/app/context/app-context";
 import Link from "next/link";
-
+import { usePathname } from "next/navigation";
 export default function Header() {
   const { grade, started, currentLesson } = useAppState();
+  const pathname = usePathname();
 
   return (
     <header className="border-b border-solid border-slate-700 text-slate-200 py-3 pl-4 pr-6">
@@ -18,9 +19,10 @@ export default function Header() {
           {grade && (
             <span className="text-base ml-2 font-semibold">{`NCERT, Grade ${grade}`}</span>
           )}
-          {started && currentLesson.length !== 0 && (
+          {started && currentLesson.length !== 0 && pathname === "/merlin" && (
             <Link
               href={"/lesson"}
+              key={pathname} //rerenders the component when the path changes
               className="bg-emerald-500 hover:bg-emerald-600 text-white font-medium py-2 px-4 rounded"
             >
               Generate Lesson
