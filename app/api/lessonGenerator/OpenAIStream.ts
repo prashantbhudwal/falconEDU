9cfb103c5ed432ed81d1df50b942dc0affaa7648
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-10 09:33:10 +0530

    Remove logs

--- app/api/lessonGenerator/OpenAIStream.ts
+++ app/api/lessonGenerator/OpenAIStream.ts
@@ -33,7 +33,6 @@ export async function OpenAIStream(messages: any) {
       function onParse(event: ParsedEvent | ReconnectInterval) {
         if (event.type === "event") {
           const data = event.data;
-          console.log(data);
           if (data === "[DONE]") {
             controller.close();
             return;
