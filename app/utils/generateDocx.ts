8f6bac49976dc172d6334503993db19f40828936
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-10 21:25:30 +0530

    Format doc

--- app/utils/generateDocx.ts
+++ app/utils/generateDocx.ts
@@ -1,32 +1,100 @@
 // generateDocx.ts
-import { Document, Packer, Paragraph, HeadingLevel } from "docx";
+import {
+  Document,
+  Packer,
+  Paragraph,
+  HeadingLevel,
+  TextRun,
+  AlignmentType,
+  BorderStyle,
+  WidthType,
+  Header,
+  Footer,
+} from "docx";
 import { saveAs } from "file-saver";
 
 interface DocData {
   topic: string;
   subtopic: string;
-  messages: string[];
+  lessonToDownload: string[];
 }
 
 export async function generateDocx(data: DocData): Promise<void> {
-  const text = data.messages.join("");
+  const text = data.lessonToDownload.join("");
   const paragraphs = text.split("\n").map((paragraphContent) => {
     return new Paragraph({
-      text: paragraphContent.trim(),
+      children: [
+        new TextRun({
+          text: paragraphContent.trim(),
+          size: 24, // equivalent to text-lg in Tailwind CSS
+        }),
+      ],
+      spacing: {
+        line: 336, // equivalent to leading-7 in Tailwind CSS
+      },
     });
+  });
+
+  const headerParagraph = new Paragraph({
+    children: [
+      new TextRun({
+        text: `Chapter: ${data.topic}`,
+        size: 24, // equivalent to text-lg in Tailwind CSS
+        color: "6B7280", // equivalent to text-slate-600 in Tailwind CSS
+        font: "Helvetica",
+      }),
+    ],
+    alignment: AlignmentType.CENTER,
+  });
+
+  const header = new Header({
+    children: [headerParagraph],
+  });
+
+  const footerParagraph = new Paragraph({
+    children: [
+      new TextRun({
+        text: "Lesson Generated with Falcon AI",
+        size: 24,
+        font: "Helvetica",
+      }),
+    ],
+    alignment: AlignmentType.CENTER,
+  });
+
+  const footer = new Footer({
+    children: [footerParagraph],
   });
 
   const doc = new Document({
     sections: [
       {
+        headers: {
+          default: header,
+        },
+        footers: {
+          default: footer,
+        },
         children: [
           new Paragraph({
-            text: data.topic,
+            text: data.subtopic,
             heading: HeadingLevel.HEADING_1,
+            alignment: AlignmentType.CENTER,
           }),
           new Paragraph({
-            text: data.subtopic,
-            heading: HeadingLevel.HEADING_2,
+            border: {
+              bottom: {
+                color: "4B5563", // equivalent to border-slate-700 in Tailwind CSS
+                size: 2,
+                space: 2,
+                style: BorderStyle.SINGLE,
+              },
+            },
+          }),
+          new Paragraph({
+            spacing: {
+              after: 200, // Add spacing after the border
+            },
           }),
           ...paragraphs,
         ],
