6352133eabaec40f995a3aece2ff0a005b2dc536
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-10 09:17:07 +0530

    Add new lesson generator

--- app/api/lessonGenerator/route.ts
+++ app/api/lessonGenerator/route.ts
@@ -5,9 +5,12 @@ import {
   handleGPT3TurboStreamData,
 } from "../openAI";
 import { ChatCompletionRequestMessage } from "openai";
-import { getPrompt } from "@/app/utils";
+import { OpenAIStream } from "./OpenAIStream";
+import { lessonOptions } from "../openAI/options";
 
-export const runtime = "nodejs";
+export const config = {
+  runtime: "edge",
+};
 // This is required to enable streaming
 export const dynamic = "force-dynamic";
 
@@ -40,6 +43,7 @@ export async function POST(request: NextRequest) {
     text: obj.text.join(" "),
   }));
   const ideas = generateGptPrompt(newArray);
+
   const messages: ChatCompletionRequestMessage[] = [
     {
       role: "system",
@@ -50,26 +54,32 @@ export async function POST(request: NextRequest) {
       content: `Give me a lesson plan. Make SURE that you use these ${ideas}. And start with Objectives, no need to give the subject and topic.`,
     },
   ];
-  let responseStream = new TransformStream();
-  const writer = responseStream.writable.getWriter();
-  try {
-    const openaiRes = await getLessonCompletionStream(messages);
-    // console.log(openaiRes.headers["x-request-id"]);
-    // @ts-ignore
-    openaiRes.data.on("data", (data: Buffer) =>
-      handleGPT3TurboStreamData(data, writer)
-    );
-  } catch (error) {
-    console.error("An error occurred during OpenAI request", error);
-    const encoder = new TextEncoder();
-    writer.write(encoder.encode("An error occurred during OpenAI request"));
-    writer.close();
-  }
-  return new Response(responseStream.readable, {
-    headers: {
-      "Content-Type": "text/event-stream",
-      Connection: "keep-alive",
-      "Cache-Control": "no-cache, no-transform",
-    },
-  });
+
+  const stream = await OpenAIStream(messages);
+  return new Response(stream);
 }
+
+//   let responseStream = new TransformStream();
+//   const writer = responseStream.writable.getWriter();
+//   try {
+//     const openaiRes = await getLessonCompletionStream(messages);
+//     // console.log(openaiRes.headers["x-request-id"]);
+//     // @ts-ignore
+//     openaiRes.data.on("data", (data: Buffer) =>
+//       handleGPT3TurboStreamData(data, writer)
+//     );
+//   } catch (error) {
+//     console.error("An error occurred during OpenAI request", error);
+//     const encoder = new TextEncoder();
+//     writer.write(encoder.encode("An error occurred during OpenAI request"));
+//     writer.close();
+//   }
+
+//   return new Response(responseStream.readable, {
+//     headers: {
+//       "Content-Type": "text/event-stream",
+//       Connection: "keep-alive",
+//       "Cache-Control": "no-cache, no-transform",
+//     },
+//   });
+// }
