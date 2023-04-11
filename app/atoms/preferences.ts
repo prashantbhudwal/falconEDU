be1984e13d7e8dcce62c856627213ce834301ce4
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-11 22:14:26 +0530

    Add preferences atoms

---
+++ app/atoms/preferences.ts
@@ -0,0 +1,8 @@
+import { atom } from "jotai";
+
+export const gradeAtom = atom("");
+export const boardAtom = atom("");
+export const subjectAtom = atom("");
+export const topicAtom = atom("");
+export const subtopicAtom = atom("");
+
