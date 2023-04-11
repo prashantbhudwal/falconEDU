4006110881f6728e00aee177ef668a654b53c6af
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-11 23:30:05 +0530

    Remove scrollbar

--- app/lesson/page.tsx
+++ app/lesson/page.tsx
@@ -3,7 +3,7 @@ import LessonCanvas from "./LessonCanvas";
 export default function Lesson() {
   return (
     <div className="grid grid-cols-12 gap-4 w-full">
-      <LessonCanvas className="col-start-3 col-span-8 h-screen overflow-y-auto custom-scrollbar" />
+      <LessonCanvas className="col-start-3 col-span-8 min-h-screen" />
     </div>
   );
 }
