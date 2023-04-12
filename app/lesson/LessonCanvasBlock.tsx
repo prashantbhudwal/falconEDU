62cf7a7994e516bef9e6cac2bf71dad5d054d033
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-12 13:59:48 +0530

    Add subject, board and grade

--- app/lesson/LessonCanvasBlock.tsx
+++ app/lesson/LessonCanvasBlock.tsx
@@ -2,7 +2,13 @@
 import { useState } from "react";
 import Issue from "./Issue";
 import { useAtom } from "jotai";
-import { topicAtom, subtopicAtom } from "../atoms/preferences";
+import {
+  topicAtom,
+  subtopicAtom,
+  boardAtom,
+  gradeAtom,
+  subjectAtom,
+} from "../atoms/preferences";
 import { lessonStreamCompletedAtom } from "../atoms/lesson";
 
 export default function LessonCanvasBlock({
@@ -13,14 +19,21 @@ export default function LessonCanvasBlock({
   const [showNote, setShowNote] = useState(false);
   const [topic] = useAtom(topicAtom);
   const [subtopic] = useAtom(subtopicAtom);
+  const [board] = useAtom(boardAtom);
+  const [grade] = useAtom(gradeAtom);
+  const [subject] = useAtom(subjectAtom);
   const [lessonStreamCompleted] = useAtom(lessonStreamCompletedAtom);
   return (
     <div
-      className={`bg-slate-100 text-slate-900 px-5 py-5 rounded-lg shadow-sm shadow-slate-200 max-w-4xl w-full`}
+      className={`bg-slate-100 text-slate-900 px-8 py-5 rounded-lg shadow-sm shadow-slate-200 max-w-4xl w-full`}
     >
-      <header className="font-medium text-slate-900 text-center border-b border-solid border-slate-700 pb-2">
-        <h1 className="text-3xl">{subtopic}</h1>
-        <p className=" text-lg  text-slate-600">Chapter: {topic}</p>
+      <header className="font-medium text-slate-900 text-center border-b border-solid border-slate-700 pb-2 flex flex-row justify-between items-baseline">
+        <div className=" text-slate-500">{`Grade ${grade}, ${subject}`}</div>
+        <div>
+          <h1 className="text-3xl">{subtopic}</h1>
+          <p className=" text-lg  text-slate-600">{topic}</p>
+        </div>
+        <div className=" text-slate-500">{board}</div>
       </header>
       <p className="leading-7 text-lg pt-8 py-5 whitespace-pre-wrap">
         {displayText}
