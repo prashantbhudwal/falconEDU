7c53c0ce2d1be5e2356e974b61668f0c81c5d2c2
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-14 00:20:29 +0530

    Display live block conditionally

--- app/lesson/LessonCanvas.tsx
+++ app/lesson/LessonCanvas.tsx
@@ -76,7 +76,11 @@ export default function Canvas({ className }: { className?: string }) {
     <div
       className={`${className} flex flex-col items-center gap-4   text-slate-300 px-5 py-3 rounded-lg ring-2 ring-emerald-500 shadow-emerald-500 ${"shadow-md bg-slate-900"}`}
     >
-      <LessonCanvasBlock text={messages} />
+      {lessonStreamCompleted ? (
+        <LessonCanvasBlock text={lessonToDownload} />
+      ) : (
+        <LessonCanvasBlock text={messages} />
+      )}
     </div>
   );
 }
