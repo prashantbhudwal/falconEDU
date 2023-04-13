e2cc1f612bde71bc3f12d03a3a8181a6dce21903
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-14 00:49:04 +0530

    Convert AidBlock into a styled component

---
+++ app/magic/AidCanvas.tsx
@@ -0,0 +1,15 @@
+export default function AidCanvas({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div
+      className={`${className} flex flex-col items-center gap-4 text-slate-300 px-5 py-3 rounded-lg ring-2 ring-emerald-500 shadow-emerald-500 ${"shadow-md bg-slate-900"}`}
+    >
+      {children}
+    </div>
+  );
+}
