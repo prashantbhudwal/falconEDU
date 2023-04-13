b65adb01d5677410de79d7e36e5b88bf8ecb37fd
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-13 23:04:04 +0530

    Add teaching aids atoms

--- app/atoms/lesson.ts
+++ app/atoms/lesson.ts
@@ -1,6 +1,25 @@
 import { atom } from "jotai";
 import { BlockContent } from "../../types";
+type teachingAids = {
+  name: string;
+  content: string[];
+};
+
+type Aid = {
+  id: string;
+  name:
+    | "lessonOutline"
+    | "lessonPlan"
+    | "slides"
+    | "story"
+    | "assessment"
+    | "activity";
+  content: string | string[];
+};
 
 export const currentLessonAtom = atom<BlockContent[]>([]);
 export const lessonStreamCompletedAtom = atom(false);
 export const lessonToDownloadAtom = atom<string[]>([]);
+export const contentStreamAtom = atom<string[]>([]);
+export const teachingAidsAtom = atom<teachingAids[]>([]);
+export const visibleAidAtom = atom<string>("");
