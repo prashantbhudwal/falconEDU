92756472d1c608bb5dd2d7799b4aa95ad6f326f3
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-14 00:49:29 +0530

    Generate content in main page

--- app/magic/page.tsx
+++ app/magic/page.tsx
@@ -1,10 +1,30 @@
 "use client";
-import Aid from "./Aid";
+import AidCanvas from "./AidCanvas";
 import Sidebar from "@/app/components/Sidebar";
 import AidChip from "./AidChip";
 import { teachingAids } from "./teachingAidsGenerator";
+import { topicAtom, subtopicAtom } from "@/app/atoms/preferences";
+import { useAtom } from "jotai";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Aid from "./Aid";
+import { lessonToDownloadAtom } from "../atoms/lesson";
+import { useContentStream } from "./useContentStream";
 
 export default function Lesson() {
+  const [lessonToDownload, setLessonToDownload] = useAtom(lessonToDownloadAtom);
+
+  const router = useRouter();
+  const [topic] = useAtom(topicAtom);
+  const [subtopic] = useAtom(subtopicAtom);
+  const { contentStream, lessonStreamCompleted } = useContentStream();
+
+  useEffect(() => {
+    if (topic === "" || subtopic === "") {
+      router.push("/preferences");
+    }
+  }, [topic, subtopic, router]);
+
   return (
     <div className="grid grid-cols-12 gap-4 w-full">
       <Sidebar
@@ -15,7 +35,13 @@ export default function Lesson() {
           <AidChip key={aid} text={aid} />
         ))}
       </Sidebar>
-      <Aid className="col-start-3 col-span-8 min-h-screen" />
+      <AidCanvas className="col-start-3 col-span-8 min-h-screen">
+        {lessonStreamCompleted ? (
+          <Aid content={lessonToDownload} />
+        ) : (
+          <Aid content={contentStream} />
+        )}
+      </AidCanvas>
     </div>
   );
 }
