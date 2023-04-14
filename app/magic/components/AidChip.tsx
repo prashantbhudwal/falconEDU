c9de900c479400f9d7daf9c90688213c9c2d56e6
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-14 21:56:17 +0530

    Disable links while streaming

--- app/magic/components/AidChip.tsx
+++ app/magic/components/AidChip.tsx
@@ -1,14 +1,27 @@
+"use client";
 import { getEmoji } from "../../utils";
 import Link from "next/link";
+import { useAtom } from "jotai";
+import { contentStreamCompletedAtom } from "@/app/atoms/lesson";
 
 export default function AidChip({ aid }: { aid: string }) {
-  return (
-    <Link href={`/magic/${aid}`}>
-      <div
-        className={` text-slate-300 px-3 py-2 rounded-md opacity-100 hover:bg-slate-800 hover:cursor-pointer`}
-      >
-        <p className={`text-lg capitalize`}>{`${getEmoji(aid)} ${aid}`}</p>
-      </div>
-    </Link>
+  const [contentStreamCompleted] = useAtom(contentStreamCompletedAtom);
+
+  const linkContent = (
+    <div
+      className={`text-slate-300 px-3 py-2 rounded-md ${
+        contentStreamCompleted
+          ? "opacity-100 hover:bg-slate-800 hover:cursor-pointer"
+          : "opacity-50 cursor-not-allowed"
+      }`}
+    >
+      <p className={`text-lg capitalize`}>{`${getEmoji(aid)} ${aid}`}</p>
+    </div>
+  );
+
+  return contentStreamCompleted ? (
+    <Link href={`/magic/${aid}`}>{linkContent}</Link>
+  ) : (
+    <>{linkContent}</>
   );
 }
