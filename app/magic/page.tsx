e17c032cd3636069f26662b1edc431d45aed1334
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-14 19:22:23 +0530

    Refactor component to use content from useAid

--- app/magic/page.tsx
+++ app/magic/page.tsx
@@ -1,62 +1,10 @@
 "use client";
-type Aid = {
-  id: string;
-  isHandout: boolean;
-  name:
-    | "lessonOutline"
-    | "lessonPlan"
-    | "slides"
-    | "story"
-    | "assessment"
-    | "activity";
-  content: string | string[];
-};
+import { useAid } from "@/app/hooks/useAid";
 import AidHeader from "./components/AidHeader";
-import { useContentStream } from "../hooks/useContentStream";
-import { lessonToDownloadAtom, lessonIdeasAtom } from "../atoms/lesson";
-import { useAtom } from "jotai";
-import { visibleAidAtom } from "../atoms/lesson";
-import { useState, useEffect } from "react";
-import { BlockContent } from "@/types";
-import { StreamPayload } from "@/types";
-import { topicAtom, subtopicAtom, gradeAtom } from "@/app/atoms/preferences";
 
 export default function Aid() {
-  const [topic] = useAtom(topicAtom);
-  const [subtopic] = useAtom(subtopicAtom);
-  const [grade] = useAtom(gradeAtom);
-
-  const [lessonIdeas] = useAtom(lessonIdeasAtom);
-  const [fetchNow, setFetchNow] = useState(false);
-  const [lessonToDownload, setLessonToDownload] = useAtom(lessonToDownloadAtom);
-  const [visibleAid] = useAtom(visibleAidAtom);
-  const [payload, setPayload] = useState<StreamPayload>({
-    topic: "",
-    subtopic: "",
-    grade: "",
-    data: [],
-    payloadType: "lesson",
-  });
-
-  useEffect(() => {
-    if (visibleAid === "lesson") {
-      setFetchNow(true);
-      setPayload({
-        topic,
-        subtopic,
-        grade,
-        data: lessonIdeas,
-        payloadType: "lesson",
-      });
-    } else {
-      setFetchNow(false);
-    }
-  }, [visibleAid, lessonIdeas, grade, topic, subtopic]);
-
-  const { contentStream, contentStreamCompleted } = useContentStream(
-    fetchNow,
-    payload
-  );
+  const aidType = "lesson"; // Change this to "outline" for the outline aid
+  const { content } = useAid(aidType);
 
   return (
     <div
@@ -64,7 +12,7 @@ export default function Aid() {
     >
       <AidHeader />
       <p className="leading-7 text-lg pt-8 py-5 whitespace-pre-wrap">
-        {contentStreamCompleted ? lessonToDownload : contentStream}
+        {content}
       </p>
     </div>
   );
