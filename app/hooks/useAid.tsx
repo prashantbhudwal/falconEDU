07d1b6c72190e02e96e8035bf9a5a279fe4bef58
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-14 19:19:09 +0530

    Add new custom hook

---
+++ app/hooks/useAid.tsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import { useAtom } from "jotai";
+import { useContentStream } from "./useContentStream";
+import { lessonToDownloadAtom, lessonIdeasAtom } from "../atoms/lesson";
+import { visibleAidAtom } from "../atoms/lesson";
+import { topicAtom, subtopicAtom, gradeAtom } from "@/app/atoms/preferences";
+import { StreamPayload } from "@/types";
+
+export function useAid(aidType: "lesson" | "outline") {
+  const [topic] = useAtom(topicAtom);
+  const [subtopic] = useAtom(subtopicAtom);
+  const [grade] = useAtom(gradeAtom);
+
+  const [lessonIdeas] = useAtom(lessonIdeasAtom);
+  const [fetchNow, setFetchNow] = useState(false);
+  const [lessonToDownload, setLessonToDownload] = useAtom(lessonToDownloadAtom);
+  const [visibleAid] = useAtom(visibleAidAtom);
+  const [payload, setPayload] = useState<StreamPayload>({
+    topic: "",
+    subtopic: "",
+    grade: "",
+    data: [],
+    payloadType: aidType,
+  });
+
+  useEffect(() => {
+    if (visibleAid === aidType) {
+      setFetchNow(true);
+      setPayload({
+        topic,
+        subtopic,
+        grade,
+        data: lessonIdeas,
+        payloadType: aidType,
+      });
+    } else {
+      setFetchNow(false);
+    }
+  }, [visibleAid, lessonIdeas, grade, topic, subtopic, aidType]);
+
+  const { contentStream, contentStreamCompleted } = useContentStream(
+    fetchNow,
+    payload
+  );
+
+  return {
+    content: contentStreamCompleted ? lessonToDownload : contentStream,
+  };
+}
