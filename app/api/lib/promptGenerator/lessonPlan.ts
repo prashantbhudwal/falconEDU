ebc9701117725de80b6e747efbc3929c656d5aaf
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-14 18:56:23 +0530

    Add lesson plan prompt generator

--- app/api/lib/promptGenerator/lessonPlan.ts
+++ app/api/lib/promptGenerator/lessonPlan.ts
@@ -1,5 +1,7 @@
 import { StreamPayload } from "@/types";
 import { ChatCompletionRequestMessage } from "openai";
+import { processStreamText } from "../utils";
+
 interface Idea {
   ideaType: string;
   text: string;
@@ -12,7 +14,8 @@ function generateGptPrompt(ideas: Idea[]): string {
     }
     // Add the idea type as a heading
     acc += `## ${idea.ideaType}\n\n`;
-    // Add the idea text as a paragraph
+
+    // Add the cleaned idea text as a paragraph
     acc += `${idea.text}\n`;
     return acc;
   }, "");
@@ -20,23 +23,23 @@ function generateGptPrompt(ideas: Idea[]): string {
   return `ideas:\n\n${promptText}`;
 }
 
-export default function getLessonPlanMessages(payload: StreamPayload){
-    const { topic, subtopic, grade, data, payloadType } = payload;
-    const newArray = data.map((obj: any) => ({
-        ideaType: obj.type,
-        text: obj.text.join(" "),
-    }));
-    const ideas = generateGptPrompt(newArray);
-    
-    const messages: ChatCompletionRequestMessage[] = [
-        {
-        role: "system",
-        content: `You are a knowledgeable teaching expert. I am a teacher in India teaching grade ${grade} science at a school that follows NCERT textbooks. I am teaching the chapter "${subtopic}" from the chapter "${topic}". My students' primary language is not English.`,
-        },
-        {
-        role: "user",
-        content: `Give me a lesson plan. Make SURE that you use these ${ideas}. And start with Objectives, no need to give the subject and topic.`,
-        },
-    ];
-    return messages;
+export default function getLessonPlanMessages(payload: StreamPayload) {
+  const { topic, subtopic, grade, data } = payload;
+  const newArray = data.map((obj: any) => ({
+    ideaType: obj.type,
+    text: processStreamText(obj.text),
+  }));
+  const ideas = generateGptPrompt(newArray);
+
+  const messages: ChatCompletionRequestMessage[] = [
+    {
+      role: "system",
+      content: `You are a knowledgeable teaching expert. I am a teacher in India teaching grade ${grade} science at a school that follows NCERT textbooks. I am teaching the chapter "${subtopic}" from the chapter "${topic}". My students' primary language is not English.`,
+    },
+    {
+      role: "user",
+      content: `Give me a lesson plan. Make SURE that you use these ${ideas}. And start with Objectives, no need to give the subject and topic.`,
+    },
+  ];
+  return messages;
 }
