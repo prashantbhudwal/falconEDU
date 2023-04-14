d38873849b5090a775bead0767b6d8ea2443a41c
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-14 23:32:12 +0530

    Change prompt

--- app/api/lib/promptGenerator/outline.ts
+++ app/api/lib/promptGenerator/outline.ts
@@ -5,16 +5,15 @@ import { processStreamText } from "../utils";
 export default function getOutlineMessages(payload: StreamPayload) {
   const { data, grade, topic, subtopic } = payload;
   const lesson = processStreamText(data);
-  console.log(lesson);
 
   const messages: ChatCompletionRequestMessage[] = [
     {
       role: "system",
-      content: `You are a knowledgeable teaching expert. I am a teacher in India teaching grade ${grade} science at a school that follows NCERT textbooks. I am teaching the chapter "${subtopic}" from the chapter "${topic}". My students' primary language is not English.`,
+      content: `You are a teaching assistant. Give me an outline of this lesson plan in a list. Be concise.`,
     },
     {
       role: "user",
-      content: `Give me an outline of this lesson plan. ${lesson}`,
+      content: `${lesson}`,
     },
   ];
   return messages;
