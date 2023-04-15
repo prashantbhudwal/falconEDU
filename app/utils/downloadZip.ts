99e9c48cf04d2d52c268556dd7f4da690c79aaa3
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-16 01:42:40 +0530

    Update downloaded file name

--- app/utils/downloadZip.ts
+++ app/utils/downloadZip.ts
@@ -5,9 +5,9 @@ export async function downloadZip(docxArray: Blob[]): Promise<void> {
   const zip = new JSZip();
 
   docxArray.forEach((docx, index) => {
-    zip.file(`document_${index + 1}.docx`, docx);
+    zip.file(`falconLesson_${index + 1}.docx`, docx);
   });
 
   const zipBlob = await zip.generateAsync({ type: "blob" });
-  saveAs(zipBlob, "documents.zip");
+  saveAs(zipBlob, "falconLesson.zip");
 }
