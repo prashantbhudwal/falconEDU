3c11a19b1e14e6000b80b899ded34792dd4de179
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-15 21:46:02 +0530

    Custom hook to find the latest aid that was generated

---
+++ app/hooks/useLatestAid.tsx
@@ -0,0 +1,28 @@
+import { useAtom } from "jotai";
+import { teachingAidsAtom } from "../atoms/lesson";
+
+interface Content {
+  content: string[];
+  id: string;
+  name: string;
+}
+
+function findLatestContent(arr: Content[], name: string): Content | null {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i].name === name) {
+      return arr[i];
+    }
+  }
+  return null; // If no content with the given name is found
+}
+
+export default function useLatestAid(aidType: string): string[] | null {
+  const [teachingAids] = useAtom(teachingAidsAtom);
+  const latestAid = findLatestContent(teachingAids, aidType);
+
+  if (latestAid) {
+    return latestAid.content;
+  } else {
+    return null;
+  }
+}
