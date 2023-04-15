182e27a53a320daa54a75e9be0d20ebfea6821a3
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-15 21:46:33 +0530

    Display latest aid instead of current content

--- app/magic/aid/[aid]/page.tsx
+++ app/magic/aid/[aid]/page.tsx
@@ -5,6 +5,7 @@ import Issue from "@/app/components/Issue";
 import { contentStreamCompletedAtom } from "@/app/atoms/lesson";
 import { useAtom } from "jotai";
 import { useState } from "react";
+import useLatestAid from "@/app/hooks/useLatestAid";
 
 export default function Page({
   params,
@@ -14,14 +15,14 @@ export default function Page({
   const [showNote, setShowNote] = useState(false);
   const [contentStreamCompleted] = useAtom(contentStreamCompletedAtom);
   const { content } = useAid(params.aid);
-
+  const latestAid = useLatestAid(params.aid);
   return (
     <div
       className={`bg-slate-100 text-slate-900 px-8 py-5 rounded-lg shadow-sm shadow-slate-200 max-w-4xl w-full`}
     >
       <AidHeader />
       <p className="leading-7 text-lg pt-8 py-5 whitespace-pre-wrap">
-        {content}
+        {contentStreamCompleted ? latestAid : content}
       </p>
       {contentStreamCompleted && (
         <p
