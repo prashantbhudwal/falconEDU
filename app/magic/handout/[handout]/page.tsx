7da675257c99d854dac2bea275b2e992c4ebe0a7
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-15 14:04:09 +0530

    Reorganize routes

---
+++ app/magic/handout/[handout]/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+import AidHeader from "../../components/AidHeader";
+import HandoutHeader from "../../components/HandoutHeader";
+import Issue from "@/app/lesson/Issue";
+import { useAtom } from "jotai";
+import { useState } from "react";
+import { BlockContent } from "@/types";
+import { lessonIdeasAtom } from "@/app/atoms/lesson";
+type Aid = {
+  id: string;
+  aid: string;
+  content: string | string[];
+};
+const ideasToAids = (ideas: BlockContent[]): Aid[] => {
+  return ideas.map(({ id, type, text }) => ({
+    id,
+    aid: type,
+    content: text,
+  }));
+};
+export default function Page({
+  params,
+}: {
+  params: { handout: "story" | "activity" | "quiz" };
+}) {
+  const [showNote, setShowNote] = useState(false);
+  const [lessonIdeas] = useAtom(lessonIdeasAtom);
+  const aids = ideasToAids(lessonIdeas);
+  const newAids = aids.filter((aid) => aid.aid === params.handout);
+
+  return (
+    <div>
+      <HandoutHeader />
+      {newAids.map((aid) => (
+        <div
+          key={aid.id}
+          className={`bg-slate-100 text-slate-900 px-8 py-5 rounded-lg shadow-sm shadow-slate-200 max-w-4xl w-full`}
+        >
+          <h1 className="text-3xl capitalize text-center">{params.handout}</h1>
+          <p className="leading-7 text-lg pt-8 py-5 whitespace-pre-wrap">
+            {aid.content}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
