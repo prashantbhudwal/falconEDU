d99923c1f0a023f7fef05adddb034eba118626e2
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-17 18:56:06 +0530

    Delete archived files

--- archive/lesson/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import LessonCanvas from "./LessonCanvas";
-export default function Lesson() {
-  return (
-    <div className="grid grid-cols-12 gap-4 w-full">
-      <LessonCanvas className="col-start-3 col-span-8 min-h-screen" />
-    </div>
-  );
-}
