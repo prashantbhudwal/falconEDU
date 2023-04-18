9be65dbe522c298d60d08343eb478c474edda9a7
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-18 17:36:35 +0530

    Improve the prompts

--- app/api/lib/ideaChatGenerator/index.ts
+++ app/api/lib/ideaChatGenerator/index.ts
@@ -6,7 +6,7 @@ function getSystemMessage(
   topic: string,
   subtopic: string
 ): string {
-  return `You are a teaching assistant. I am a teacher in India teaching teaching the topic "${subtopic}" from the chapter "${topic}" from the Grade ${grade}, NCERT Science Textbook. My students' primary language is not English. Help me brainstorm ideas and strategies for effectively teaching this topic in my classroom. Make sure you adhere to the Grade ${grade}, NCERT Science Textbook.`;
+  return `You are a teaching assistant that helps me with lesson planning by providing ideas. I am a teacher in India teaching teaching the topic "${subtopic}" from the chapter "${topic}" from the Grade ${grade}, NCERT Science Textbook. My students' primary language is not English. Make sure you adhere to the Grade ${grade}, NCERT Science Textbook.`;
 }
 
 export function getChatMessages(
@@ -33,7 +33,7 @@ export function getChatMessages(
         },
         {
           role: "user",
-          content: `Provide one engaging example that explains the concept of: ${subtopic}, helping students to better visualize the idea. Keep your answers concise, as short as possible. Start with, "Here is an example..."`,
+          content: `Provide an example that clarifies the topic of ${subtopic} for students. Keep your answers concise, as short as possible. Start with, "Here is an example..."`,
         },
       ];
     case "analogy":
@@ -44,7 +44,7 @@ export function getChatMessages(
         },
         {
           role: "user",
-          content: `Present one everyday analogy that simplifies the concept of: ${subtopic}, using familiar situations to make the idea more relatable. Keep your answers concise, as short as possible.`,
+          content: `Present one everyday analogy that simplifies the topic of: ${subtopic}, using familiar situations to make the idea more relatable. Keep your answers concise, as short as possible.`,
         },
       ];
     case "history":
@@ -55,7 +55,7 @@ export function getChatMessages(
         },
         {
           role: "user",
-          content: `Discuss the historical context of the concept of: ${subtopic}. Keep your answers concise, as short as possible.`,
+          content: `Discuss the historical context of the topic of: ${subtopic}. Keep your answers concise, as short as possible.`,
         },
       ];
     case "application":
@@ -66,7 +66,7 @@ export function getChatMessages(
         },
         {
           role: "user",
-          content: `Describe a real-world application of this concept that students can relate to: ${subtopic}. Keep your answers concise, as short as possible.`,
+          content: `Describe a real-world application of the topic "${subtopic}", that students can relate to: . Keep your answers concise, as short as possible.`,
         },
       ];
     case "antiExample":
@@ -88,7 +88,7 @@ export function getChatMessages(
         },
         {
           role: "user",
-          content: `Compare and contrast this concept with other closely related topics to help students distinguish between them: ${subtopic}. Keep your answers concise, as short as possible.`,
+          content: `Compare and contrast this topic with other closely related topics to help students distinguish between them: ${subtopic}. Keep your answers concise, as short as possible.`,
         },
       ];
     case "definition":
@@ -99,7 +99,7 @@ export function getChatMessages(
         },
         {
           role: "user",
-          content: `Define the concept in a clear and concise manner for students: ${subtopic}. Keep your answers concise, as short as possible.`,
+          content: `Define the topic in a clear and concise manner for students: ${subtopic}. Keep your answers concise, as short as possible.`,
         },
       ];
     case "activity":
@@ -132,7 +132,7 @@ export function getChatMessages(
         },
         {
           role: "user",
-          content: `Provide an example that clarifies the concept for students: ${subtopic}. Keep your answers concise, as short as possible. Keep your answers concise, as short as possible.`,
+          content: `Provide an example that clarifies the topic for students: ${subtopic}. Keep your answers concise, as short as possible. Keep your answers concise, as short as possible.`,
         },
       ];
   }
