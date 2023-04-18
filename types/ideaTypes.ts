1ca2d8eb11a267d7a7a6548abe88732879e79648
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-18 12:53:41 +0530

    Add types

---
+++ types/ideaTypes.ts
@@ -0,0 +1,15 @@
+export type ideaType =
+  | "story"
+  | "example"
+  | "analogy"
+  | "history"
+  | "application"
+  | "antiExample"
+  | "contrast"
+  | "definition"
+  | "activity"
+  | "quiz"
+  | "";
+
+export type aidType = "lesson" | "outline" | "slides" | "";
+export type handoutType = "story" | "activity" | "quiz" | "";
