9ed4bb2bc430dc284644e9806566130e8d216889
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-19 15:24:47 +0530

    Archive old streaming functions

---
+++ app/api/lib/openAI/archived.ts
@@ -0,0 +1,84 @@
+import { OpenAIApi, Configuration } from "openai";
+import { ChatCompletionRequestMessage } from "openai";
+import { ideaOptions, lessonOptions } from "./options";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const model = new OpenAIApi(configuration);
+
+export default async function getCompletion(
+  messages: ChatCompletionRequestMessage[]
+) {
+  const { MODEL, TEMPERATURE, MAX_TOKENS } = ideaOptions;
+  const completion = await model.createChatCompletion({
+    model: MODEL,
+    temperature: TEMPERATURE,
+    max_tokens: MAX_TOKENS,
+    messages: messages,
+  });
+  return completion.data;
+}
+
+export async function getCompletionStream(
+  messages: ChatCompletionRequestMessage[]
+) {
+  const { MODEL, TEMPERATURE, MAX_TOKENS, STREAM } = ideaOptions;
+  const completion = await model.createChatCompletion(
+    {
+      model: MODEL,
+      temperature: TEMPERATURE,
+      max_tokens: MAX_TOKENS,
+      messages: messages,
+      stream: STREAM,
+    },
+    { responseType: "stream" }
+  );
+  return completion;
+}
+
+export async function getLessonCompletionStream(
+  messages: ChatCompletionRequestMessage[]
+) {
+  const { MODEL, TEMPERATURE, MAX_TOKENS, STREAM } = lessonOptions;
+  const completion = await model.createChatCompletion(
+    {
+      model: MODEL,
+      temperature: TEMPERATURE,
+      max_tokens: MAX_TOKENS,
+      messages: messages,
+      stream: STREAM,
+    },
+    { responseType: "stream" }
+  );
+  return completion;
+}
+
+export async function handleGPT3TurboStreamData(
+  data: Buffer,
+  writer: WritableStreamDefaultWriter<Uint8Array>
+) {
+  const encoder = new TextEncoder();
+  const lines = data
+    .toString()
+    .split("\n")
+    .filter((line) => line.trim() !== "");
+
+  for (const line of lines) {
+    const message = line.replace(/^data: /, "");
+    if (message === "[DONE]") {
+      console.log("Stream completed");
+      writer.close();
+      return;
+    }
+
+    try {
+      const parsed = JSON.parse(message);
+      const filteredContent = parsed.choices[0]?.delta?.content || "";
+
+      await writer.write(encoder.encode(filteredContent));
+    } catch (error) {
+      console.error("Could not JSON parse stream message", message, error);
+    }
+  }
+}
