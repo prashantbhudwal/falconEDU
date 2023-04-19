5332c9d2d2938ab716c0c9214f7bba65a534240b
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-19 15:11:59 +0530

    Use new streaming function to generate stream

--- app/api/contentStreamOnEdge/route.ts
+++ app/api/contentStreamOnEdge/route.ts
@@ -1,20 +1,40 @@
 import { NextRequest } from "next/server";
 import { StreamPayload } from "@/types";
 import { ChatCompletionRequestMessage } from "openai";
-import { OpenAIStream } from "./OpenAIStream";
+import { streamFromOpenAI } from "./OpenAIStream";
 import getChatCompletionRequestMessages from "../lib/chatGenerator";
-
+import { lessonOptions } from "../lib/openAI/options";
 export const config = {
   runtime: "edge",
 };
 // This is required to enable streaming
 export const dynamic = "force-dynamic";
 
+const getAidRequestPayload = (messages: any) => {
+  const { MODEL, TEMPERATURE, MAX_TOKENS, STREAM } = lessonOptions;
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    },
+    responseType: "stream",
+    body: JSON.stringify({
+      model: MODEL,
+      temperature: TEMPERATURE,
+      // max_tokens: MAX_TOKENS, //TODO Change to variable
+      messages: messages,
+      stream: STREAM,
+    }),
+  };
+  return requestOptions;
+};
+
 export async function POST(request: NextRequest) {
   const body: StreamPayload = await request.json();
   const messages: ChatCompletionRequestMessage[] =
     getChatCompletionRequestMessages(body);
-
-  const stream = await OpenAIStream(messages);
+  const aidPayload = getAidRequestPayload(messages);
+  const stream = await streamFromOpenAI(aidPayload);
   return new Response(stream);
 }
