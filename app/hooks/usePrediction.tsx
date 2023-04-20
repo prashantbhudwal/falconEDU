1df95f870adef1a53fead8ec53fa74e67fb0959c
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-20 22:58:26 +0530

    Add custom hook for prediction

---
+++ app/hooks/usePrediction.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { useAtom } from "jotai";
+import { useContentStream } from "./useContentStream";
+import { v4 as uuid } from "uuid";
+import { contentStreamCompletedAtom } from "../atoms/lesson";
+import { gradeAtom } from "@/app/atoms/preferences";
+import { PredictionPayload } from "@/types";
+import { shouldRegenerateAtom } from "../atoms/lesson";
+import { boardAtom } from "@/app/atoms/preferences";
+
+export function usePrediction(node: string) {
+  const [grade] = useAtom(gradeAtom);
+  const [board] = useAtom(boardAtom);
+  const [contentStreamCompleted, setContentStreamCompleted] = useAtom(
+    contentStreamCompletedAtom
+  );
+
+  const { contentStream, startGeneration, currentStreamId, prevStreamId } =
+    useContentStream();
+  const [shouldRegenerate, setShouldRegenerate] = useAtom(shouldRegenerateAtom);
+
+  const startStreaming = () => {
+    if (node == "") return;
+    const payload: PredictionPayload = {
+      topic: node,
+      grade,
+      board,
+    };
+    startGeneration(payload);
+    setShouldRegenerate(false);
+  };
+  return {
+    content: contentStream,
+    startStreaming,
+  };
+}
