d87f358a015d7d9e6fe17e9eea0a6fee40d55ac1
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-21 21:03:46 +0530

    Move Grid to its own component

---
+++ app/preferences/PredictionGrid.tsx
@@ -0,0 +1,49 @@
+type PredictionGridProps = {
+  content: string[];
+  selectedOption: string;
+  handleChange: (event: any) => void;
+};
+
+export default function PredictionGrid({
+  content,
+  selectedOption,
+  handleChange,
+}: PredictionGridProps) {
+  const contentString = content.join("");
+  console.log("topics", contentString);
+  const contentArray = contentString
+    .replace(/\n/g, "")
+    .replace(/\$\$(?:(?!\$\$|\n).)*\$\$/g, (match) => match.slice(2, -2))
+    .split("_");
+  console.log("topicArray", contentArray);
+
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <div className="text-xl text-slate-400">Or just select one...</div>
+      <div className="flex flex-row gap-2 flex-wrap w-4/5 justify-center">
+        {contentArray.map((topic, index) => (
+          <label
+            key={index}
+            htmlFor={`topic-${index}`}
+            className={`p-3 w-96 border rounded cursor-pointer ${
+              selectedOption === topic
+                ? "bg-emerald-500 text-slate-900"
+                : "bg-slate-300 text-slate-900"
+            }`}
+          >
+            <input
+              type="radio"
+              id={`topic-${index}`}
+              name="topics"
+              value={topic}
+              checked={selectedOption === topic}
+              onChange={handleChange}
+              className="hidden" // This hides the default radio button
+            />
+            {topic}
+          </label>
+        ))}
+      </div>
+    </div>
+  );
+}
