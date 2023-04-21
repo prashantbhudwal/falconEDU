caf086fb26232d39b34df3f8794da8c2aa0f2262
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-21 11:54:28 +0530

    Update prompts

--- app/api/predictionOnEdge/route.ts
+++ app/api/predictionOnEdge/route.ts
@@ -35,15 +35,15 @@ export async function POST(request: NextRequest) {
   const messages: ChatCompletionRequestMessage[] = [
     {
       role: "system",
-      content: `I want to teach "Properties of Matter" to grade 6 students in India.  Give me a list of topics to focus on, and make sure that you reply with just the CSV of the topics, each topic starts and ends with a backtick, and nothing else, no comments, no conclusions, nothing. No adding comments like, here is a list. The response should just have a list and that's it. I am putting this output directly in code, so don't mess it up. No spaces after commas.`,
+      content: `I want to teach "Properties of Matter" to grade 6 students in India.  Give me a list of topics to focus on, and make sure that you reply with just the underscore separated values of the topics, each topic starts with a $$ and ends with a $$, and nothing else, no comments, no conclusions, nothing. No adding comments like, here is a list. The response should just have a list and that's it. I am putting this output directly in code, so don't mess it up. No spaces after commas.`,
     },
     {
       role: "assistant",
-      content: `[States of matter],[Physical and chemical properties of matter],[Density and buoyancy],[Solids, liquids, gases and plasma],[Changes in states of matter (melting, freezing, boiling)],[Mixtures and solutions]`,
+      content: `$$States of matter$$_$$Physical and chemical properties of matter$$_$$Density and buoyancy$$_$$Solids, liquids, gases and plasma$$_$$Changes in states of matter (melting, freezing, boiling)$$_$$Mixtures and solutions$$`,
     },
     {
       role: "user",
-      content: `Okay, perfect format. I am a ${board} teacher teaching "${topic}" to "Grade ${grade}" students. Give me a list of topics to focus on, in the same format.`,
+      content: `Okay, perfect format. I am a ${board} teacher teaching "${topic}" from Science to "Grade ${grade}" students. Give me a list of topics to focus on, in the same format.`,
     },
   ];
   const aidPayload = getAidRequestPayload(messages);
