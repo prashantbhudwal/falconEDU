6eeff84fa80d0c180f2142895070327f046b2505
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-21 14:46:07 +0530

    Display suggestions

--- app/predict/topic/[topic]/page.tsx
+++ app/predict/topic/[topic]/page.tsx
@@ -1,46 +1,71 @@
 "use client";
+import { PropagateLoader } from "react-spinners";
 import { usePrediction } from "@/app/hooks/usePrediction";
 import { contentStreamCompletedAtom } from "@/app/atoms/lesson";
 import { useAtom } from "jotai";
 import { useEffect, useState } from "react";
 import { shouldRegenerateAtom } from "@/app/atoms/lesson";
+const TestString =
+  "$$States of matter$$_$$Physical and chemical properties of matter$$_$$Density and buoyancy$$_$$Solids, liquids, gases and plasma$$_$$Changes in states of matter (melting, freezing, boiling)$$_$$Mixtures and solutions$$_$$Physical and chemical properties of matter$$_$$Density and buoyancy$$_$$Physical and chemical properties of matter$$_$$Density and buoyancy$$";
 
-const getOptions = function (topics: string) {
+const getRadioButtons = function (
+  topics: string,
+  selectedTopic: string,
+  handleTopicChange: (event: any) => void
+) {
   const topicArray = topics
     .replace(/\$\$(?:(?!\$\$).)*\$\$/g, (match) => match.slice(2, -2))
     .split("_");
 
-  const topicOptions = topicArray.map((topic) => (
-    <option key={topic} value={topic}>
+  const topicRadioButtons = topicArray.map((topic, index) => (
+    <label
+      key={index}
+      htmlFor={`topic-${index}`}
+      className={`p-3 w-96 border rounded cursor-pointer ${
+        selectedTopic === topic
+          ? "bg-emerald-500 text-slate-900"
+          : "bg-slate-300 text-slate-900"
+      }`}
+    >
+      <input
+        type="radio"
+        id={`topic-${index}`}
+        name="topics"
+        value={topic}
+        checked={selectedTopic === topic}
+        onChange={handleTopicChange}
+        className="hidden" // This hides the default radio button
+      />
       {topic}
-    </option>
+    </label>
   ));
 
-   
-  return topicOptions;
+  const optionsGrid = (
+    <div className="flex flex-col items-center gap-4">
+      <div className="text-xl text-slate-400">Or just select one...</div>
+      <div className="flex flex-row gap-2 flex-wrap w-4/5 justify-center">
+        {topicRadioButtons}
+      </div>
+    </div>
+  );
+  return optionsGrid;
 };
 
 export default function Page({ params }: { params: { topic: string } }) {
   const [contentStreamCompleted] = useAtom(contentStreamCompletedAtom);
   const topic = decodeURIComponent(params.topic.replace(/%20/g, " "));
   const { content, startStreaming } = usePrediction(topic);
-  const [shouldRegenerate, setShouldRegenerate] = useAtom(shouldRegenerateAtom);
   const [selectedTopic, setSelectedTopic] = useState("");
   const [topics, setTopics] = useState("");
-  // console.log(content);
 
-  const handleTopicChange = (event) => {
+  const handleTopicChange = (event: any) => {
     setSelectedTopic(event.target.value);
   };
 
   useEffect(() => {
     startStreaming();
   }, []);
-  useEffect(() => {
-    if (shouldRegenerate) {
-      startStreaming();
-    }
-  }, [shouldRegenerate]);
+
   useEffect(() => {
     if (contentStreamCompleted) {
       const concatenatedString = content.join("");
@@ -49,26 +74,34 @@ export default function Page({ params }: { params: { topic: string } }) {
   }, [contentStreamCompleted]);
 
   return (
-    <div
-      className={`bg-slate-100 text-slate-900 px-8 py-5 rounded-lg shadow-sm shadow-slate-200 max-w-4xl w-full`}
-    >
-      <p className="leading-7 text-lg pt-8 py-5 whitespace-pre-wrap">
-        {/* {content} */}
-        {!contentStreamCompleted && "Thinking"}
-      </p>
-      {contentStreamCompleted && (
-        <select value={selectedTopic} onChange={handleTopicChange}>
-          <option value="">Select a topic</option>
-          {topics && getOptions(topics)}
-        </select>
-      )}
+    <div className="flex flex-col gap-6 items-center m-4">
+      <div className="mt-4 flex gap-2">
+        <input
+          type="text"
+          id="inputText"
+          value={selectedTopic}
+          onChange={handleTopicChange}
+          className="border-slate-700 rounded-md bg-slate-300 text-black p-4 w-96"
+          placeholder="Enter a topic"
+        />
+        <button
+          className="bg-emerald-400 ring-1 ring-slate-700 text-slate-700 rounded-md px-8 py-2 text-lg font-medium capitalize disabled:opacity-50"
+          // onClick={}
+          disabled={!selectedTopic}
+        >
+          Next
+        </button>
+      </div>
 
-      {/* <button
-        onClick={() => setShouldRegenerate(true)}
-        className={`text-green-500`}
-      >
-        Regenerate
-      </button> */}
+      {!contentStreamCompleted ? (
+        <div className="flex flex-col items-center justify-center gap-2 h-12">
+          <PropagateLoader color="#10B981" />
+        </div>
+      ) : (
+        contentStreamCompleted &&
+        topics &&
+        getRadioButtons(topics, selectedTopic, handleTopicChange)
+      )}
     </div>
   );
 }
