f4673bd93d99449d542e617b6326b3e0ef1e48e3
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-21 21:34:53 +0530

    Remove App start code

--- app/preferences/topic/page.tsx
+++ app/preferences/topic/page.tsx
@@ -12,24 +12,16 @@ import { lessonIdeasAtom } from "@/app/atoms/lesson";
 import { useRouter } from "next/navigation";
 import { subjectAtom } from "../../atoms/preferences";
 
-export default function Page({ params }: { params: { slug: string } }) {
+export default function Page() {
   const [contentStreamCompleted] = useAtom(contentStreamCompletedAtom);
   const [subject] = useAtom(subjectAtom);
   const { content, startStreaming } = usePrediction(subject);
-  const [selectedTopic, setSelectedTopic] = useState("");
   const [allContent, setAllContent] = useState([""]);
   const [topic, setTopic] = useAtom(topicAtom);
-  const [started, setStarted] = useAtom(startedAtom);
-  const [lessonIdeas, setLessonIdeas] = useAtom(lessonIdeasAtom);
   const router = useRouter();
 
-  const handleStart = () => {
-    router.push("/merlin");
-    setStarted(true);
-    setLessonIdeas([]);
-  };
   const handleTopicChange = (event: any) => {
-    setSelectedTopic(event.target.value);
+    setTopic(event.target.value);
   };
 
   useEffect(() => {
@@ -48,28 +40,18 @@ export default function Page({ params }: { params: { slug: string } }) {
         <input
           type="text"
           id="inputText"
-          value={selectedTopic}
+          value={topic}
           onChange={handleTopicChange}
           className="border-slate-700 rounded-md bg-slate-300 text-black p-4 w-96"
           placeholder="Enter a topic"
         />
-        {topic ? (
-          <button
-            className="bg-emerald-400 ring-1 ring-slate-700 text-slate-700 rounded-md px-8 py-2 text-lg font-medium capitalize disabled:opacity-50"
-            onClick={handleStart}
-            disabled={!topic}
-          >
-            New Lesson
-          </button>
-        ) : (
-          <button
-            className="bg-emerald-500 ring-1 ring-slate-700 text-slate-700 rounded-md px-8 py-2 text-lg font-medium capitalize disabled:opacity-50"
-            // onClick={}
-            disabled={!selectedTopic}
-          >
-            Next
-          </button>
-        )}
+        <button
+          className="bg-emerald-500 ring-1 ring-slate-700 text-slate-700 rounded-md px-8 py-2 text-lg font-medium capitalize disabled:opacity-50"
+          onClick={() => router.push("/preferences/subtopic")}
+          disabled={!topic}
+        >
+          Next
+        </button>
       </div>
 
       {!contentStreamCompleted ? (
@@ -81,7 +63,7 @@ export default function Page({ params }: { params: { slug: string } }) {
         allContent && (
           <PredictionGrid
             content={allContent}
-            selectedOption={selectedTopic}
+            selectedOption={topic}
             handleChange={handleTopicChange}
           />
         )
