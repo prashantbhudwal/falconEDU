47d8e09841092aee53e1181ed27a559de75ff132
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-22 23:56:51 +0530

    Add Title text to the layout page

--- app/preferences/layout.tsx
+++ app/preferences/layout.tsx
@@ -1,7 +1,30 @@
+"use client";
+import { usePathname } from "next/navigation";
+
+const getTitle = (pathname: string) => {
+  switch (pathname) {
+    case "/preferences":
+      return "What are you teaching today?";
+    case "/preferences/topic":
+      return "What chapter are you teaching?";
+    case "/preferences/subtopic":
+      return "What topic are you teaching?";
+    default:
+      return "What are you teaching today?";
+  }
+};
+
 export default function PreferencesLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  const pathname = usePathname();
+
+  return (
+    <div className="flex flex-col gap-4 items-center mt-7">
+      <div className="text-4xl text-slate-400">{getTitle(pathname)}</div>
+      {children}
+    </div>
+  );
 }
