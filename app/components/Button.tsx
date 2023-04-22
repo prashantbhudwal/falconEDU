78247402235bd555423cab779b8d80c7d7b0a4de
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-23 00:21:25 +0530

    Fix UI of the button component

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,19 +1,19 @@
-"use Client";
-export default function Button({
-  children,
-  onClick,
-}: {
+"use client";
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
-  onClick: () => void;
-}) {
+}
+
+const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
   return (
     <button
-      onClick={onClick}
-      className={
-        "bg-emerald-500 ring-1 ring-slate-700 text-slate-700 rounded-md px-2 py-1 text-sm font-medium capitalize"
-      }
+      {...props}
+      className={`bg-emerald-500 ring-1 ring-slate-700 text-slate-700 rounded-md px-2 py-1 font-medium capitalize disabled:opacity-50 px-8 py-2  ${
+        props.className ? props.className : ""
+      }`}
     >
       {children}
     </button>
   );
-}
+};
+
+export default Button;
