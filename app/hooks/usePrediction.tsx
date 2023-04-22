148762c56d3da8991b6eb61d937f8a279bd47161
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-22 16:57:26 +0530

    Generate payload based on action

--- app/hooks/usePrediction.tsx
+++ app/hooks/usePrediction.tsx
@@ -1,16 +1,54 @@
-import { useState, useEffect } from "react";
 import { useAtom } from "jotai";
 import { useContentStream } from "./useContentStream";
-import { v4 as uuid } from "uuid";
 import { contentStreamCompletedAtom } from "../atoms/lesson";
 import { gradeAtom } from "@/app/atoms/preferences";
 import { PredictionPayload, APIRoute } from "@/types";
 import { shouldRegenerateAtom } from "../atoms/lesson";
 import { boardAtom } from "@/app/atoms/preferences";
-const ROUTE: APIRoute = "/api/predictionOnEdge";
 import { subjectAtom } from "@/app/atoms/preferences";
+import { PredictionAction } from "@/types";
 
-export function usePrediction(node: string) {
+const getPayload = function (
+  data: string,
+  action: PredictionAction,
+  grade: string,
+  board: string,
+  subject: string
+) {
+  switch (action) {
+    case "predictChapters":
+      return {
+        action: action,
+        data: {
+          subject: data,
+          grade: grade,
+          board: board,
+        },
+      };
+    case "predictSubtopics":
+      return {
+        action: action,
+        data: {
+          subject: subject,
+          grade: grade,
+          board: board,
+          topic: data,
+        },
+      };
+    default:
+      return {
+        action: action,
+        data: {
+          subject: data,
+          grade: grade,
+          board: board,
+        },
+      };
+  }
+};
+
+export function usePrediction(data: string, action: PredictionAction) {
+  const ROUTE: APIRoute = "/api/predictionOnEdge";
   const [grade] = useAtom(gradeAtom);
   const [board] = useAtom(boardAtom);
   const [subject] = useAtom(subjectAtom);
@@ -24,16 +62,14 @@ export function usePrediction(node: string) {
   const [shouldRegenerate, setShouldRegenerate] = useAtom(shouldRegenerateAtom);
 
   const startStreaming = () => {
-    if (node == "") return;
-    const payload: PredictionPayload = {
-      action: "predictChapters",
-      data: {
-        subject: subject,
-        grade: grade,
-        board: board,
-      },
-    };
-    console.log(payload);
+    if (data == "") return;
+    const payload: PredictionPayload = getPayload(
+      data,
+      action,
+      grade,
+      board,
+      subject
+    );
     startGeneration(payload);
     setShouldRegenerate(false);
   };
