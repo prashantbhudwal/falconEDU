113958ab3e632162085d10e1c6f12ddbd102432c
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-24 22:34:12 +0530

    Animate button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,18 +1,24 @@
 "use client";
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+import { motion, MotionProps } from "framer-motion";
+type CombinedProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  MotionProps;
+
+interface ButtonProps extends CombinedProps {
   children: React.ReactNode;
 }
 
 const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
   return (
-    <button
+    <motion.button
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
       {...props}
       className={`bg-emerald-500 ring-1 ring-slate-700 text-slate-700 rounded-md px-2 py-1 font-medium capitalize disabled:opacity-50 px-8 py-2  ${
         props.className ? props.className : ""
       }`}
     >
       {children}
-    </button>
+    </motion.button>
   );
 };
 
