2aad2565caae6986b6c22fe197e5174f51d91307
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-27 19:42:57 +0530

    Fix footer styles

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,19 +1,16 @@
 import Link from "next/link";
 export default function Footer() {
   return (
-    <footer className="flex justify-evenly items-center py-4 text-slate-700">
+    <footer className="flex flex-col text-slate-700 w-screen items-center gap-4 py-6 border-t border-slate-700">
       <Link
         href="https://notionforms.io/forms/work-with-us-5"
-        className="hover:text-slate-500 underline underline-offset-4"
+        className="hover:text-slate-500 underline underline-offset-4 block"
       >
         Work with us
       </Link>
-      <div className="flex-none mx-2">
-        <p>© 2023 FalconAI</p>
-      </div>
       <Link
         href="https://notionforms.io/forms/invest-in-us"
-        className="hover:text-slate-500 underline underline-offset-4"
+        className="hover:text-slate-500 underline underline-offset-4 block"
       >
         Invest in us
       </Link>
