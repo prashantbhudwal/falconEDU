e152cf89a4b373ca0b146da4b01083c8cf205fdd
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-27 18:21:02 +0530

    Add links in the footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
 export default function Footer() {
   return (
-    <footer className="text-center py-4 text-slate-700">
-      <p>© 2023 FalconOne</p>
+    <footer className="flex justify-evenly items-center py-4 text-slate-700">
+      <Link
+        href="https://notionforms.io/forms/work-with-us-5"
+        className="hover:text-slate-500 underline underline-offset-4"
+      >
+        Work with us
+      </Link>
+      <div className="flex-none mx-2">
+        <p>© 2023 FalconAI</p>
+      </div>
+      <Link
+        href="https://notionforms.io/forms/invest-in-us"
+        className="hover:text-slate-500 underline underline-offset-4"
+      >
+        Invest in us
+      </Link>
     </footer>
   );
 }
