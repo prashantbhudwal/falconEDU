eca2788edce703ad61e36daa83c7d4fdba1a8893
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-28 17:04:53 +0530

    Remove horizontal scrollbar

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 export default function Footer() {
   return (
-    <footer className="flex flex-col text-slate-700 w-screen items-center gap-4 py-6 border-t border-slate-700">
+    <footer className="flex flex-col text-slate-700 items-center gap-4 py-6 border-t border-slate-700">
       <Link
         href="https://notionforms.io/forms/work-with-us-5"
         className="hover:text-slate-500 underline underline-offset-4 block"
