9ae97355062c35d893a194d44cc78ea2b94240ad
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-30 16:11:03 +0530

    Ignore quiz while generating video script

--- app/api/lib/aidChatGenerator/shortVideoScript.ts
+++ app/api/lib/aidChatGenerator/shortVideoScript.ts
@@ -1,6 +1,6 @@
 import { StreamPayload } from "@/types";
 import { ChatCompletionRequestMessage } from "openai";
-import { processIdeas, generateMarkdown } from "@/app/api/lib/utils";
+import { processIdeas, generateMarkdownWithoutQuiz } from "@/app/api/lib/utils";
 
 interface Idea {
   ideaType: string;
@@ -10,15 +10,15 @@ interface Idea {
 export default function getShortVideoScriptMessages(payload: StreamPayload) {
   const { topic, subtopic, grade, data, board, subject } = payload;
   const newArray = processIdeas(data);
-  const ideas = generateMarkdown(newArray);
+  const ideas = generateMarkdownWithoutQuiz(newArray);
   const messages: ChatCompletionRequestMessage[] = [
     {
       role: "system",
-      content: `You are a video script writer. I am a teacher in India teaching grade ${grade} ${subject} at a school that follows "${board}" textbooks. I am teaching the chapter "${subtopic}" from the chapter "${topic}". My students' primary language is not English. I will give you some ideas and you give me a video script.`,
+      content: `You are a video script writer. I am a teacher in India teaching grade ${grade} ${subject} at a school that follows "${board}" textbooks. I am teaching the chapter "${subtopic}" from the chapter "${topic}". My students' primary language is not English. I will give you some ideas and you give me a video script. Start your response with "Short Video Script:".`,
     },
     {
       role: "user",
-      content: `Give script for a 3 minute video for my children. Make it fun and engaging. Also remember that I will be recording this video on my phone'e selfie camera. So, no mention of any graphics or animations. Also, the whole video will be in one shot, no editors will be used in post. Make SURE that you use some or all of these ${ideas}.`,
+      content: `Give script for a 3 minute video for my children in the style of Kurzgesagt. Make it fun and engaging. Also remember that I will be recording this video on my phone'e selfie camera. So, no use of any graphics or animations. The whole video will be in one shot, so no cuts because no editors will be used in post. Make SURE that you use some or all of these ${ideas}.`,
     },
   ];
   return messages;
--- app/api/lib/utils/index.ts
+++ app/api/lib/utils/index.ts
@@ -26,3 +26,21 @@ export function generateMarkdown(ideas: any) {
 
   return `ideas:\n\n${markdown}`;
 }
+
+export function generateMarkdownWithoutQuiz(ideas: any) {
+  const markdown = ideas.reduce((acc: any, idea: any) => {
+    // Add a separator between ideas
+    if (acc.length > 0) {
+      acc += "\n\n";
+    }
+    // Add the idea type as a heading, only if it's not a quiz
+    if (idea.ideaType !== "quiz") {
+      acc += `## ${idea.ideaType}\n\n`;
+      // Add the cleaned idea text as a paragraph
+      acc += `${idea.text}\n`;
+    }
+    return acc;
+  }, "");
+
+  return `ideas:\n\n${markdown}`;
+}
