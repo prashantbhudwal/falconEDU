c6f1ce4cf3c5a584b4162e9bdd151b3c1dfc8232
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-30 15:26:56 +0530

    Update prompts

--- app/api/lib/aidChatGenerator/blackboard.ts
+++ app/api/lib/aidChatGenerator/blackboard.ts
@@ -9,11 +9,11 @@ export default function getBlackboardMessages(payload: StreamPayload) {
   const messages: ChatCompletionRequestMessage[] = [
     {
       role: "system",
-      content: `You are a teaching assistant. Give me an outline of this lesson plan in a list. Be concise. Don't mention objectives or materials needed.`,
+      content: `You are a teaching assistant. Don't give any introduction to your responses like, "As a teaching assistant.." or "As a language model". Start your response with "Here are best ways to use a blackboard during this lesson."`,
     },
     {
       role: "user",
-      content: `${lesson}`,
+      content: `I am a teacher. I have no computer in my classroom. I use blackboard and I need your help. How do I best use the blackboard in the class during this lesson plan - ${lesson}`,
     },
   ];
   return messages;
--- app/api/lib/aidChatGenerator/shortVideoScript.ts
+++ app/api/lib/aidChatGenerator/shortVideoScript.ts
@@ -14,11 +14,11 @@ export default function getShortVideoScriptMessages(payload: StreamPayload) {
   const messages: ChatCompletionRequestMessage[] = [
     {
       role: "system",
-      content: `You are a video script writer. I am a teacher in India teaching grade ${grade} ${subject} at a school that follows "${board}" textbooks. I am teaching the chapter "${subtopic}" from the chapter "${topic}". My students' primary language is not English.`,
+      content: `You are a video script writer. I am a teacher in India teaching grade ${grade} ${subject} at a school that follows "${board}" textbooks. I am teaching the chapter "${subtopic}" from the chapter "${topic}". My students' primary language is not English. I will give you some ideas and you give me a video script.`,
     },
     {
       role: "user",
-      content: `Give script for a 3 minute video for my children. Make SURE that you use some or all of these ${ideas}. Make it fun and engaging.`,
+      content: `Give script for a 3 minute video for my children. Make it fun and engaging. Also remember that I will be recording this video on my phone'e selfie camera. So, no mention of any graphics or animations. Also, the whole video will be in one shot, no editors will be used in post. Make SURE that you use some or all of these ${ideas}.`,
     },
   ];
   return messages;
