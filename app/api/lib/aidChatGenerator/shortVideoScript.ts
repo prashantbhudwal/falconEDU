975897de50a6ea66a9e9a8f3ef163d400aace047
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-30 14:45:25 +0530

    Add prompt for video generation

--- app/api/lib/aidChatGenerator/shortVideoScript.ts
+++ app/api/lib/aidChatGenerator/shortVideoScript.ts
@@ -14,11 +14,11 @@ export default function getShortVideoScriptMessages(payload: StreamPayload) {
   const messages: ChatCompletionRequestMessage[] = [
     {
       role: "system",
-      content: `You are a knowledgeable teaching expert. I am a teacher in India teaching grade ${grade} ${subject} at a school that follows "${board}" textbooks. I am teaching the chapter "${subtopic}" from the chapter "${topic}". My students' primary language is not English.`,
+      content: `You are a video script writer. I am a teacher in India teaching grade ${grade} ${subject} at a school that follows "${board}" textbooks. I am teaching the chapter "${subtopic}" from the chapter "${topic}". My students' primary language is not English.`,
     },
     {
       role: "user",
-      content: `Give me a lesson plan. Make SURE that you use these ${ideas}. And start with Objectives, no need to give the subject and topic.`,
+      content: `Give script for a 3 minute video for my children. Make SURE that you use some or all of these ${ideas}. Make it fun and engaging.`,
     },
   ];
   return messages;
