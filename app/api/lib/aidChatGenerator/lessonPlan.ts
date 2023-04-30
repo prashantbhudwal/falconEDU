a0533413af28dc4b4adf37379aea2bfb87b74f85
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-30 11:19:42 +0530

    Use new utils

--- app/api/lib/aidChatGenerator/lessonPlan.ts
+++ app/api/lib/aidChatGenerator/lessonPlan.ts
@@ -1,33 +1,15 @@
 import { StreamPayload } from "@/types";
 import { ChatCompletionRequestMessage } from "openai";
-import { processStreamText } from "@/app/api/lib/utils";
+import { processIdeas, generateMarkdown } from "@/app/api/lib/utils";
 
 interface Idea {
   ideaType: string;
   text: string;
 }
-function generateMarkdown(ideas: Idea[]): string {
-  const markdown = ideas.reduce((acc, idea) => {
-    // Add a separator between ideas
-    if (acc.length > 0) {
-      acc += "\n\n";
-    }
-    // Add the idea type as a heading
-    acc += `## ${idea.ideaType}\n\n`;
-    // Add the cleaned idea text as a paragraph
-    acc += `${idea.text}\n`;
-    return acc;
-  }, "");
-
-  return `ideas:\n\n${markdown}`;
-}
 
 export default function getLessonPlanMessages(payload: StreamPayload) {
   const { topic, subtopic, grade, data } = payload;
-  const newArray = data.map((obj: any) => ({
-    ideaType: obj.type,
-    text: processStreamText(obj.text),
-  }));
+  const newArray = processIdeas(data);
   const ideas = generateMarkdown(newArray);
   const messages: ChatCompletionRequestMessage[] = [
     {
