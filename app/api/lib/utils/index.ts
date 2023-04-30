7fb15096fbcc1672cde5e67d179d75140d896591
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-04-30 11:19:56 +0530

    Add new utils

--- app/api/lib/utils/index.ts
+++ app/api/lib/utils/index.ts
@@ -2,3 +2,27 @@ export function processStreamText(text: any) {
   let processStreamText = text.join(" ").replace(/\s+/g, " ");
   return processStreamText;
 }
+
+export function processIdeas(ideas: any) {
+  const newArray = ideas.map((obj: any) => ({
+    ideaType: obj.type,
+    text: processStreamText(obj.text),
+  }));
+  return newArray;
+}
+
+export function generateMarkdown(ideas: any) {
+  const markdown = ideas.reduce((acc: any, idea: any) => {
+    // Add a separator between ideas
+    if (acc.length > 0) {
+      acc += "\n\n";
+    }
+    // Add the idea type as a heading
+    acc += `## ${idea.ideaType}\n\n`;
+    // Add the cleaned idea text as a paragraph
+    acc += `${idea.text}\n`;
+    return acc;
+  }, "");
+
+  return `ideas:\n\n${markdown}`;
+}
