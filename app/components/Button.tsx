d671b39e66ea7673c7b18a1b0a681ec58b1955e2
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-04 16:36:14 +0530

    Add more variants to button component

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,19 +1,43 @@
 "use client";
 import { motion, MotionProps } from "framer-motion";
+
 type CombinedProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
   MotionProps;
 
 interface ButtonProps extends CombinedProps {
   children: React.ReactNode;
+  primary?: boolean;
+  secondary?: boolean;
+  outlined?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
+const Button: React.FC<ButtonProps> = ({
+  children,
+  primary,
+  secondary,
+  outlined,
+  ...props
+}) => {
+  let buttonStyle = "bg-emerald-500 text-slate-700";
+
+  if (secondary) {
+    buttonStyle = "bg-fuchsia-700 text-slate-100";
+  } else if (outlined) {
+    buttonStyle = "border-2 border-slate-700 bg-transparent text-slate-700";
+  } else if (
+    typeof primary === "undefined" &&
+    typeof secondary === "undefined" &&
+    typeof outlined === "undefined"
+  ) {
+    primary = true;
+  }
+
   return (
     <motion.button
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
       {...props}
-      className={`bg-emerald-500 ring-1 ring-slate-700 text-slate-700 rounded-md px-2 py-1 font-medium capitalize disabled:opacity-50 px-8 py-2  ${
+      className={`${buttonStyle} ring-1 ring-slate-700 rounded-md font-medium capitalize disabled:opacity-50 px-8 py-2 ${
         props.className ? props.className : ""
       }`}
     >
