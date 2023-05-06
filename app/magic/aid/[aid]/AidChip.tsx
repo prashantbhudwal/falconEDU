cd703ea19839d2551171a2755fff6a23e21292eb
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-06 20:07:39 +0530

    Replace link with router

--- app/magic/aid/[aid]/AidChip.tsx
+++ app/magic/aid/[aid]/AidChip.tsx
@@ -5,13 +5,22 @@ import { useAtom } from "jotai";
 import { contentStreamCompletedAtom } from "@/app/atoms/lesson";
 import { aidType } from "@/types/ideaTypes";
 import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function AidChip({ aid }: { aid: aidType }) {
   const [contentStreamCompleted] = useAtom(contentStreamCompletedAtom);
   const pathname = usePathname();
+  const router = useRouter();
 
-  const linkContent = (
+  const handleClick = () => {
+    if (contentStreamCompleted) {
+      router.push(`/magic/aid/${aid}`);
+    }
+  };
+
+  return (
     <button
+      onClick={handleClick}
       className={`text-slate-300 px-3 py-2 rounded-md ${
         contentStreamCompleted
           ? "opacity-100 hover:bg-slate-800 hover:cursor-pointer"
@@ -23,5 +32,4 @@ export default function AidChip({ aid }: { aid: aidType }) {
       <p className={`capitalize `}>{`${getEmoji(aid)} ${getName(aid)}`}</p>
     </button>
   );
-  return <Link href={`/magic/aid/${aid}`}>{linkContent}</Link>;
 }
--- app/magic/handout/[handout]/HandoutChip.tsx
+++ app/magic/handout/[handout]/HandoutChip.tsx
@@ -5,13 +5,21 @@ import { useAtom } from "jotai";
 import { contentStreamCompletedAtom } from "@/app/atoms/lesson";
 import { handoutType } from "@/types";
 import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function HandoutChip({ aid }: { aid: handoutType }) {
   const [contentStreamCompleted] = useAtom(contentStreamCompletedAtom);
   const pathname = usePathname();
+  const router = useRouter();
 
-  const linkContent = (
+  const handleClick = () => {
+    if (contentStreamCompleted) {
+      router.push(`/magic/handout/${aid}`);
+    }
+  };
+  return (
     <button
+      onClick={handleClick}
       className={`text-slate-300 px-3 py-2 rounded-md ${
         contentStreamCompleted
           ? "opacity-100 hover:bg-slate-800 hover:cursor-pointer"
@@ -23,6 +31,4 @@ export default function HandoutChip({ aid }: { aid: handoutType }) {
       <p className={`text-lg capitalize`}>{`${getEmoji(aid)} ${aid}`}</p>
     </button>
   );
-
-  return <Link href={`/magic/handout/${aid}`}>{linkContent}</Link>;
 }
