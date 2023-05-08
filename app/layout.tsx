398ff8cd670a7a313eb361301b7edd3f6e6beb93
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-08 18:55:39 +0530

    Add AuthProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Footer from "@/app/Footer";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import AtomProviders from "./atoms/providers";
+import AuthProvider from "./providers/AuthProvider";
 import { Inter } from "next/font/google";
 
 const inter = Inter({
@@ -24,13 +25,15 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <body className="flex flex-col min-h-screen bg-slate-900 text-slate-400">
-        <AtomProviders>
-          <Header />
-          <main className="flex-grow mx-5 my-4 flex flex-col items-center min-h-screen">
-            {children}
-          </main>
-          <Footer />
-        </AtomProviders>
+        <AuthProvider>
+          <AtomProviders>
+            <Header />
+            <main className="flex-grow mx-5 my-4 flex flex-col items-center min-h-screen">
+              {children}
+            </main>
+            <Footer />
+          </AtomProviders>
+        </AuthProvider>
         <Analytics />
       </body>
     </html>
