5f7837284362fff759725de370026f641a5c271b
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-09 00:27:04 +0530

    Add styles and logic to boilerplate

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,6 +2,7 @@
 import { signIn, useSession } from "next-auth/react";
 import { useState, FormEvent } from "react";
 import { useRouter } from "next/navigation";
+import TextInput from "@/app/preferences/TextInput";
 type SignInResult =
   | {
       error?: string;
@@ -19,70 +20,50 @@ export default function Login() {
     try {
       result = await signIn("credentials", {
         redirect: true,
-        callbackUrl: "/NextAuth/Protected",
+        callbackUrl: "/preferences",
         username: email,
         password,
       });
       if (result?.error) {
         alert(result.error);
       } else {
-        window.location.href = "/NextAuth/Protected";
+        window.location.href = "/preferences";
       }
     } catch (e) {
       console.error(e);
     }
   };
   if (session && sessionStatus === "authenticated") {
-    router.push("/NextAuth/Protected");
+    router.push("/preferences");
     return null;
   }
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="rounded bg-white p-8 shadow-md">
-        <h1 className="mb-4 text-2xl font-bold">Sign In</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label
-              className="mb-2 block font-bold text-gray-700"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
-              id="email"
-              type="email"
-              placeholder="Email"
-              required
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="mb-2 block font-bold text-gray-700"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
-              id="password"
-              type="password"
-              placeholder="Password"
-              required
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </div>
-          <button
-            className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
-            type="submit"
-          >
-            Sign In
-          </button>
-        </form>
-      </div>
-    </div>
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-8 rounded p-8 mt-4"
+    >
+      <TextInput
+        id="email"
+        type="email"
+        placeholder="Email"
+        required
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <TextInput
+        id="password"
+        type="password"
+        placeholder="Password"
+        required
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <button
+        type="submit"
+        className={`bg-emerald-500 text-slate-800 px-28 py-4 rounded-lg text-lg font-semibold hover:bg-emerald-600 transition duration-200 ease-in-out`}
+      >
+        Sign In
+      </button>
+    </form>
   );
 }
