f863cd074a1f86bcc1e81a1edb80cec4440154dc
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-08 23:46:57 +0530

    Add signin button

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { signIn, useSession } from "next-auth/react";
+import Button from "./components/Button";
 
 const LandingPage = () => {
   const router = useRouter();
+  const { data: session } = useSession();
   useEffect(() => {
     router.prefetch("/preferences");
     router.prefetch("/preferences/topic");
@@ -12,6 +15,9 @@ const LandingPage = () => {
     router.prefetch("/merlin");
     router.prefetch("/magic/aid/lesson");
   }, []);
+
+  if (session) router.push("/preferences");
+
   return (
     <div className="flex flex-col items-center text-center pt-8 min-h-screen">
       <h1 className="my-6 text-4xl md:text-5xl text-slate-300 max-w-xl leading-10">
@@ -26,12 +32,12 @@ const LandingPage = () => {
         For any help or suggestions, call us or drop a WhatsApp message at +91
         8879881971.
       </p>
-      <Link
-        href={"/preferences"}
+      <button
+        onClick={() => signIn()}
         className={`bg-emerald-500 text-slate-800 px-28 py-4 rounded-lg text-lg font-semibold hover:bg-emerald-600 transition duration-200 ease-in-out`}
       >
-        Start App
-      </Link>
+        Sign In
+      </button>
     </div>
   );
 };
