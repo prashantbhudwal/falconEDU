9d386379d018585305b3d4f9c6d7ed8ab10d612f
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-09 18:56:44 +0530

    Make main heading font responsive

--- app/page.tsx
+++ app/page.tsx
@@ -7,27 +7,26 @@ const LandingPage = () => {
   const { data: session, status: sessionStatus } = useSession();
   const router = useRouter();
 
-
   useEffect(() => {
     if (session && sessionStatus === "authenticated") {
       router.push("/preferences");
     }
   }, [session, sessionStatus]);
 
-   useEffect(() => {
-     router.prefetch("/auth/login");
-     router.prefetch("/preferences");
-     if (session && sessionStatus === "authenticated") {
-       router.prefetch("/preferences/topic");
-       router.prefetch("/preferences/subtopic");
-       router.prefetch("/merlin");
-       router.prefetch("/magic/aid/lesson");
-     }
-   }, []);
+  useEffect(() => {
+    router.prefetch("/auth/login");
+    router.prefetch("/preferences");
+    if (session && sessionStatus === "authenticated") {
+      router.prefetch("/preferences/topic");
+      router.prefetch("/preferences/subtopic");
+      router.prefetch("/merlin");
+      router.prefetch("/magic/aid/lesson");
+    }
+  }, []);
 
   return (
     <div className="flex flex-col items-center text-center pt-8 min-h-screen">
-      <h1 className="my-6 text-4xl md:text-5xl text-slate-300 max-w-xl leading-10">
+      <h1 className="my-6 text-2xl md:text-5xl text-slate-300 max-w-xl leading-10 lg:text-5xl">
         <div className="inline-flex items-start">
           <p className="">Welcome to FalconAI</p>
           <span className="text-sm font-semibold bg-yellow-300 text-yellow-800 px-2 py-1 rounded -mt-1 ml-2">
