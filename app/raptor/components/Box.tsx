0fe341e1d5b47e204f0c11c72d30f9af548bd152
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-11 17:46:43 +0530

    Add box component

---
+++ app/raptor/components/Box.tsx
@@ -0,0 +1,34 @@
+export default function Box({
+  children,
+  className,
+  color = "primary",
+  ...rest
+}: {
+  children: React.ReactNode;
+  className?: string;
+  color?: "primary" | "secondary" | "gray";
+  rest?: any;
+}) {
+  let ringColorClass = "";
+  let shadowColorClass = "";
+
+  if (color === "primary") {
+    ringColorClass = "ring-emerald-500";
+    shadowColorClass = "shadow-emerald-500";
+  } else if (color === "secondary") {
+    ringColorClass = "ring-fuchsia-500";
+    shadowColorClass = "shadow-fuchsia-500";
+  } else if (color === "gray") {
+    ringColorClass = "ring-slate-500";
+    shadowColorClass = "shadow-slate-500";
+  }
+
+  return (
+    <div
+      className={`px-3 py-3 rounded-lg ring-2 ${ringColorClass} ${shadowColorClass} shadow-sm max-w-4xl w-full ${className}`}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+}
