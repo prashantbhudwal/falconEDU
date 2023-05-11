c1a5d2ae21b21dee68754cb736915ead9eb26756
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-11 18:16:12 +0530

    Canvas accepts props for header

--- app/raptor/components/Canvas.tsx
+++ app/raptor/components/Canvas.tsx
@@ -1,18 +1,23 @@
 import Box from "./Box";
+import Header, { HeaderProps, ColorOption } from "./Header";
 
 export default function Canvas({
   children,
   className,
   color = "primary",
-}: React.PropsWithChildren<{
-  className?: string;
-  color?: "primary" | "secondary" | "gray";
-}>) {
+  ...headerProps
+}: React.PropsWithChildren<
+  {
+    className?: string;
+    color?: ColorOption;
+  } & HeaderProps
+>) {
   return (
     <Box
       className={`${className} flex flex-col gap-6 text-slate-300`}
       color={color}
     >
+      <Header color={color} {...headerProps} />
       {children}
     </Box>
   );
