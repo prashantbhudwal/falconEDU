8bfb29858d95f41d8551acc6a979adcc897acf27
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-11 18:16:51 +0530

    Add primitive Header component

---
+++ app/raptor/components/Header.tsx
@@ -0,0 +1,58 @@
+export type ColorOption = "primary" | "secondary" | "gray";
+
+export type HeaderProps = {
+  className?: string;
+  color?: ColorOption;
+  heading?: React.ReactNode;
+  subheading?: React.ReactNode;
+  leftTop?: React.ReactNode;
+  leftBottom?: React.ReactNode;
+  rightTop?: React.ReactNode;
+  rightBottom?: React.ReactNode;
+};
+
+export default function Header({
+  className,
+  color = "primary",
+  heading,
+  subheading,
+  leftTop,
+  leftBottom,
+  rightTop,
+  rightBottom,
+}: HeaderProps) {
+  let textColorClass = "";
+  let borderClass = "";
+
+  if (color === "primary") {
+    textColorClass = "text-emerald-600";
+    borderClass = "border-emerald-700";
+  } else if (color === "secondary") {
+    textColorClass = "text-fuchsia-600";
+    borderClass = "border-fuchsia-700";
+  } else if (color === "gray") {
+    textColorClass = "text-slate-600";
+    borderClass = "border-slate-700";
+  }
+
+  return (
+    <header
+      className={`font-medium ${textColorClass} border-b border-solid ${borderClass} pb-2 flex flex-row justify-between w-full items-center ${className}`}
+    >
+      <div className="text-left">
+        {leftTop}
+        {leftBottom}
+      </div>
+      <div className="text-center max-w-xl">
+        {heading && <h1 className="text-2xl">{heading}</h1>}
+        {subheading && (
+          <p className={`text-lg ${textColorClass}`}>{subheading}</p>
+        )}
+      </div>
+      <div className="text-sm text-left">
+        {rightTop}
+        {rightBottom}
+      </div>
+    </header>
+  );
+}
