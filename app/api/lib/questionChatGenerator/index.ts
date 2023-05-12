85f636961b129ee61c1fcb2d6fc264a32e7e0b14
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-13 00:32:50 +0530

    Add prompts for all question types

--- app/api/lib/questionChatGenerator/index.ts
+++ app/api/lib/questionChatGenerator/index.ts
@@ -1,112 +1,85 @@
-import { QuestionPayload } from "@/types";
+import { QuestionPayload, QuestionType } from "@/types";
 import { ChatCompletionRequestMessage } from "openai";
 
-const skipIntroduction = `Do not refer to yourself in your answers. Or give any introductory text. Do not say stuff like "As an AI language model..." or "Sure, here is ..."`;
+const JSON_DIRECTIVE = "You only reply in JSON.";
 
-const SHORT_RESPONSES = `Keep your answers concise, as short as possible.`;
 function getEngineeredMessages(
-  grade: string,
-  topic: string,
-  subtopic: string,
-  board: string,
-  subject: string
+  payload: QuestionPayload
 ): ChatCompletionRequestMessage[] {
   return [
     {
       role: "system",
-      content: `You are a question generator that generates question at a particular "Bloom Level" for children. You only reply in JSON. Here is a sample JSON response: 
-      {
-      "question": <question>,
-      "type":<questionType>,
-      "options": <options>,
-      "bloomLevel": <level>
-      "levelExplanation":<whyLevelWasAssigned>,
-      "correctOption":<correctOption>,
-      "sampleAnswer":<sampleAnswer>,
-      "topic":<topic>,
-      "subtopic":<subtopic>,
-      "grade":<grade>,
-      "curriculum":<curriculum>
-      }
-`,
+      content: `You are a question generator that generates question at a particular "Bloom Level" for children. ${JSON_DIRECTIVE}`,
     },
   ];
 }
 
-export function getQuestionMessages(
-  payload: QuestionPayload
-): ChatCompletionRequestMessage[] {
-  const { grade, topic, subtopic, board, questionType, bloomLevel, subject } =
+function getJsonPayload(payload: QuestionPayload): string {
+  const { bloomLevel, topic, subtopic, grade, board, questionType } =
     payload.data;
-    console.log(payload.data)
-  const engineeredMessages = getEngineeredMessages(
-    grade,
+
+  let basePayload = {
+    question: "<question>",
+    type: questionType,
+    bloomLevel,
+    levelExplanation: "<whyLevelWasAssigned>",
     topic,
     subtopic,
-    board,
-    subject
-  );
+    grade,
+    curriculum: board,
+  };
+
+  let specificPayload;
 
   switch (questionType) {
     case "fillInTheBlanks":
-      return [
-        ...engineeredMessages,
-        {
-          role: "user",
-          content: `Give me one question {
-    "question": <question>,
-    "type": "fillInTheBlanks",
-    "options": <options>,
-    "bloomLevel": "${bloomLevel}",
-    "levelExplanation":<whyLevelWasAssigned>,
-    "correctOption": <correctOption>,
-    "sampleAnswer": <SampleAnswer>,
-    "topic": "${topic}",
-    "subtopic": "${subtopic}",
-    "grade": "${grade}",
-    "curriculum": "${board}"
-    }`,
-        },
-      ];
     case "multipleChoiceSingleCorrect":
-      return [
-        ...engineeredMessages,
-        {
-          role: "user",
-          content: `Describe a real-world application of the topic "${subtopic}", that students can relate to. ${SHORT_RESPONSES} ${skipIntroduction}`,
-        },
-      ];
     case "trueFalse":
-      return [
-        ...engineeredMessages,
-        {
-          role: "user",
-          content: `Explain one counterexample that helps students differentiate between correct and incorrect understanding of: ${subtopic}. ${SHORT_RESPONSES} ${skipIntroduction}`,
-        },
-      ];
+      specificPayload = {
+        ...basePayload,
+        options: "<options>",
+        correctOption: "<correctOption>",
+      };
+      break;
     case "shortAnswer":
-      return [
-        ...engineeredMessages,
-        {
-          role: "user",
-          content: `Compare and contrast this topic with other closely related topics to help students distinguish between them: ${subtopic}. ${SHORT_RESPONSES} ${skipIntroduction}`,
-        },
-      ];
     case "essay":
-      return [
-        ...engineeredMessages,
-        {
-          role: "user",
-          content: `Design a 5-question MCQ quiz that targets the core ideas and principles of: ${subtopic}, ensuring students grasp the most important aspects. Give answers at the end. ${SHORT_RESPONSES} ${skipIntroduction}`,
-        },
-      ];
+      specificPayload = {
+        ...basePayload,
+        sampleAnswer: "<SampleAnswer>",
+      };
+      break;
     default:
-      return [
-        ...engineeredMessages,
-        {
-          role: "user",
-          content: `Provide an example that clarifies the topic for students: ${subtopic}. ${SHORT_RESPONSES} ${skipIntroduction}`,
-        },
-      ];
+      throw new Error("Invalid question type");
   }
+
+  return JSON.stringify(specificPayload);
+}
+export function getQuestionMessages(
+  payload: QuestionPayload
+): ChatCompletionRequestMessage[] {
+  const questionType = payload.data.questionType;
+
+  if (
+    ![
+      "fillInTheBlanks",
+      "multipleChoiceSingleCorrect",
+      "trueFalse",
+      "shortAnswer",
+      "essay",
+    ].includes(questionType)
+  ) {
+    throw new Error("Invalid question type");
+  }
+
+  const engineeredMessages = getEngineeredMessages(payload);
+
+  return [
+    ...engineeredMessages,
+    {
+      role: "user",
+      content: `${JSON_DIRECTIVE} Give me one question ${getJsonPayload(
+        payload
+      )}`,
+    },
+  ];
 }
