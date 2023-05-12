26a643bf9960ef09aebaba303b70f99a016714ed
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-12 18:00:45 +0530

    Add new state atoms

--- app/atoms/worksheet.ts
+++ app/atoms/worksheet.ts
@@ -1,3 +1,15 @@
 import { atom } from "jotai";
+import { QuestionType } from "@/types";
+type CurrentQuestion = {
+  questionType: QuestionType;
+  bloomLevel: string;
+  subtopic: string;
+};
 
 export const worksheetSubtopicsAtom = atom([""]);
+
+export const currentQuestionAtom = atom<CurrentQuestion>({
+  questionType: "fillInTheBlanks",
+  bloomLevel: "",
+  subtopic: "",
+});
