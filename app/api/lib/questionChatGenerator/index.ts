a0faaf7893bc3b40651ca9fab20792f2ace50006
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-14 20:27:31 +0530

    Update question prompts

--- app/api/lib/questionChatGenerator/index.ts
+++ app/api/lib/questionChatGenerator/index.ts
@@ -1,17 +1,138 @@
 import { QuestionPayload, QuestionType } from "@/types";
 import { ChatCompletionRequestMessage } from "openai";
 
-const JSON_DIRECTIVE = "You only reply in JSON.";
+const JSON_DIRECTIVE = "Only respond with JSON.";
 
-function getEngineeredMessages(
+function getQuestionTypePrompt(questionType: string) {
+  switch (questionType) {
+    case "fillInTheBlanks":
+      return "Generate a '''fill in the blank question'''.";
+    case "multipleChoiceSingleCorrect":
+      return "Generate a '''multiple choice question with a single correct answer'''.";
+    case "trueFalse":
+      return "Generate a '''true or false question'''.";
+    case "shortAnswer":
+      return "Generate a '''short answer type question'''.";
+    case "essay":
+      return "Generate a '''essay type'''.";
+    default:
+      throw new Error("Invalid question type");
+  }
+}
+
+function getQuestionResponseFormat(questionType: QuestionType): string {
+  switch (questionType) {
+    case "trueFalse":
+      return `{"type":"trueFalse","question":<question>}`;
+    case "fillInTheBlanks":
+      return `{"type":"fillInTheBlanks","question":<question>}`;
+    case "multipleChoiceSingleCorrect":
+      return `{"type":"multipleChoiceSingleCorrect","question":<question>, "options":<options>, "answer":<correctOption>}`;
+    case "shortAnswer":
+      return `{"type":"shortAnswer","question":<question>, "answer":<sampleAnswer>}`;
+    case "essay":
+      return `{"type":"essay","question":<question>, "answer":<sampleAnswer>}`;
+    default:
+      throw new Error("Invalid question type");
+  }
+}
+
+function getBloomLevelPrompt(bloomLevel: string): string {
+  const lowerCaseBloomLevel = bloomLevel.toLowerCase();
+  switch (lowerCaseBloomLevel) {
+    case "remember":
+      return "Remembering";
+    case "understand":
+      return "Understanding";
+    case "apply":
+      return "Applying";
+    case "analyze":
+      return "Analyzing";
+    case "evaluate":
+      return "Evaluating";
+    case "create":
+      return "Creating";
+    default:
+      throw new Error("Invalid bloom level");
+  }
+}
+
+function getSystemMessage(
   payload: QuestionPayload
 ): ChatCompletionRequestMessage[] {
   return [
     {
       role: "system",
-      content: `You are a question generator that generates question at a particular "Bloom Level" for children. ${JSON_DIRECTIVE}`,
+      content: `Generate questions for a school worksheet. ${JSON_DIRECTIVE}`,
     },
   ];
+}
+
+function getInitialUserMessage(payload: QuestionPayload): string {
+  const { bloomLevel, topic, subtopic, grade, board, questionType } =
+    payload.data;
+  const prompt_QuestionType = getQuestionTypePrompt(questionType);
+  const prompt_BloomLevel = getBloomLevelPrompt(bloomLevel);
+  const questionFormat = getQuestionResponseFormat(questionType);
+  return `${prompt_QuestionType} The question should be for the topic "${subtopic}" from the chapter "${topic}". The students are prescribed, """${board}""" Textbook. Make sure you adhere to """Bloom's taxonomy""", and give the the question at the Bloom level of '''${prompt_BloomLevel}'''. Don't mention the textbook, or bloom level in the response. Reply with ONLY JSON in the following format: ${questionFormat}`;
+}
+
+export function getQuestionMessages(
+  payload: QuestionPayload
+): ChatCompletionRequestMessage[] {
+  const questionType = payload.data.questionType;
+  const systemMessage = getSystemMessage(payload);
+  const initialUserMessage = getInitialUserMessage(payload);
+  switch (questionType) {
+    case "fillInTheBlanks":
+      return [
+        ...systemMessage,
+        {
+          role: "user",
+          content: initialUserMessage,
+        },
+      ];
+    case "multipleChoiceSingleCorrect":
+      return [
+        ...systemMessage,
+        {
+          role: "user",
+          content: initialUserMessage,
+        },
+      ];
+    case "trueFalse":
+      return [
+        ...systemMessage,
+        {
+          role: "user",
+          content: initialUserMessage,
+        },
+      ];
+    case "shortAnswer":
+      return [
+        ...systemMessage,
+        {
+          role: "user",
+          content: initialUserMessage,
+        },
+      ];
+    case "essay":
+      return [
+        ...systemMessage,
+        {
+          role: "user",
+          content: initialUserMessage,
+        },
+      ];
+    default:
+      return [
+        ...systemMessage,
+        {
+          role: "user",
+          content: ``,
+        },
+      ];
+  }
 }
 
 function getJsonPayload(payload: QuestionPayload): string {
@@ -58,33 +179,4 @@ function getJsonPayload(payload: QuestionPayload): string {
   }
 
   return JSON.stringify(specificPayload);
-}
-export function getQuestionMessages(
-  payload: QuestionPayload
-): ChatCompletionRequestMessage[] {
-  const questionType = payload.data.questionType;
-
-  if (
-    ![
-      "fillInTheBlanks",
-      "multipleChoiceSingleCorrect",
-      "trueFalse",
-      "shortAnswer",
-      "essay",
-    ].includes(questionType)
-  ) {
-    throw new Error("Invalid question type");
-  }
-
-  const engineeredMessages = getEngineeredMessages(payload);
-
-  return [
-    ...engineeredMessages,
-    {
-      role: "user",
-      content: `${JSON_DIRECTIVE} Give me one question for the following data <${getJsonPayload(
-        payload
-      )}>. Reply with ONLY JSON in the following format: {"type":<questionType>,"question":<question>, "options":<options>}`,
-    },
-  ];
 }
