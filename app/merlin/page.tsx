0867b4d7e54c363cf362b9e2e06ca8cb98295f92
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-14 14:38:04 +0530

    Remove useDesktop from merlin

--- app/merlin/page.tsx
+++ app/merlin/page.tsx
@@ -6,18 +6,12 @@ import OutlineBlock from "./components/OutlineBlock";
 import { buttonsArray as promptsArray } from "../utils";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { DndProvider } from "react-dnd";
-import useDesktop from "@/app/hooks/useDesktop";
-import DesktopOnly from "@/app/components/DesktopOnly";
 import { getEmoji } from "../utils";
 import { useAtom } from "jotai";
 import { lessonIdeasAtom } from "../atoms/lesson";
 
 export default function Merlin() {
   const [lessonIdeas] = useAtom(lessonIdeasAtom);
-  const isDesktop = useDesktop();
-  if (!isDesktop) {
-    return <DesktopOnly />;
-  }
   return (
     <DndProvider backend={HTML5Backend}>
       <div className="grid grid-cols-12 gap-4 w-full select-none">
