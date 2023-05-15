4bde3fc55a489cd611cd6296974da9c05d86c82e
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-16 02:14:12 +0530

    Extract a question section into a component

---
+++ app/raptor/components/QuestionSection.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Section from "./Section";
+import BloomBoxes from "./BloomBoxes";
+import questionData from "@/app/data/questionMatrix.json";
+import { QuestionType, Questions, Question } from "@/types";
+
+const getQuestionTypeTitle = (questionType: QuestionType) => {
+  const title = questionData.find(
+    (question) => question.type === questionType
+  )?.title;
+  return title ? title : "";
+};
+
+type Props = {
+  questionTypeKey: QuestionType;
+  savedQuestions: Questions;
+};
+
+const QuestionSection: React.FC<Props> = ({ questionTypeKey, savedQuestions }) => {
+  return (
+    <Section
+      title={getQuestionTypeTitle(questionTypeKey)}
+      key={questionTypeKey}
+    >
+      <BloomBoxes questionTypeKey={questionTypeKey} />
+      {savedQuestions[questionTypeKey]?.map((question, index) => (
+        <div className="flex flex-col gap-1 pt-2 px-4" key={index}>
+          <div className="">
+            {index + 1}. {question.question}
+            <div className="pt-2">
+              {"options" in question &&
+                Array.isArray(question.options) && // type guard
+                question.options.map((option: string, optIndex: number) => (
+                  <div key={optIndex} className="flex flex-row gap-2">
+                    <div>{option}</div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+      ))}
+    </Section>
+  );
+};
+
+export default QuestionSection;
