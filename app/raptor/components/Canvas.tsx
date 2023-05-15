a45d7bb0dd90fcbf3dd0e16fe0a7d094097c264c
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-16 00:42:00 +0530

    Remove top padding

--- app/raptor/components/Canvas.tsx
+++ app/raptor/components/Canvas.tsx
@@ -14,7 +14,7 @@ export default function Canvas({
 >) {
   return (
     <Box
-      className={`${className} flex flex-col gap-6 text-slate-300`}
+      className={`${className} flex flex-col gap-6 text-slate-300 pt-0`}
       color={color}
     >
       <Header color={color} {...headerProps} />
