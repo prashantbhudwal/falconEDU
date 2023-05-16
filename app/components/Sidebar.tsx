7960699bf6b9d7a65c5068807dec0621491a2549
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-16 15:11:55 +0530

    Move general components to the base directory

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,15 +1,11 @@
 export default function Sidebar({
   children,
   className,
-  heading,
-}: React.PropsWithChildren<{ className?: string; heading: string }>) {
+}: React.PropsWithChildren<{ className?: string }>) {
   return (
     <div
-      className={`${className} flex flex-col gap-4 bg-slate-900 text-slate-300 px-5 py-3 rounded-lg ring-1 ring-slate-500`}
+      className={`${className} flex flex-col gap-6 text-slate-300 px-5 py-3 rounded-lg ring-1 ring-slate-500`}
     >
-      <header className="text-center font-medium text-slate-500 border-b border-solid border-slate-700 pb-2 ">
-        <p className="capitalize">{heading}</p>
-      </header>
       {children}
     </div>
   );
