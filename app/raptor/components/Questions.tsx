55b0d631d3f0d1d558c7fccdcf7b2a4b6d838a2b
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-18 04:34:06 +0530

    Update questions component

--- app/raptor/components/Questions.tsx
+++ app/raptor/components/Questions.tsx
@@ -3,12 +3,23 @@ import { QuestionType, Question } from "@/types";
 import { currentQuestionAtom } from "@/app/atoms/worksheet";
 import { useAtom } from "jotai";
 import { DropTargetMonitor, useDrop } from "react-dnd";
+import questionData from "@/app/data/questionMatrix.json";
+import BloomBoxes from "./BloomBoxes";
+import Section from "@/app/components/Section";
+import { getQuestionSectionShadow, getQuestionSectionBorderColor, getQuestionSectionTextColor } from "@/app/utils/index";
 
+const getQuestionTypeTitle = (questionType: QuestionType) => {
+  const title = questionData.find(
+    (question) => question.type === questionType
+  )?.title;
+  return title ? title : "";
+};
 type Props = {
   questions: Question[];
   type: QuestionType;
   droppable?: boolean;
   className?: string;
+  hasBloom?: boolean;
 };
 
 const Questions: React.FC<Props> = ({
@@ -16,6 +27,7 @@ const Questions: React.FC<Props> = ({
   type,
   droppable = false,
   className,
+  hasBloom,
 }) => {
   const [_, setCurrentQuestion] = useAtom(currentQuestionAtom);
 
@@ -34,43 +46,56 @@ const Questions: React.FC<Props> = ({
   };
   const [{ isOver, canDrop }, drop] = useDrop(() => specObject);
   return (
-    <div
-      ref={droppable ? drop : null}
-      className={`text-neutral-300 ${className} ${
-        isOver ? "bg-fuchsia-500" : "bg-slate-800"
-      } `}
+    <Section
+      title={getQuestionTypeTitle(type)}
+      key={type}
+      color="secondaryGray"
+      headingColor={getQuestionSectionTextColor(type)}
+      dividerColor={getQuestionSectionBorderColor(type)}
+      className={`px-5 py-3 rounded-lg shadow-sm ${getQuestionSectionShadow(
+        type
+      )}`}
     >
-      {questions.length === 0 && (
-        <div
-          className={`flex flex-col gap-1 pt-2 px-4 ${className} ${
-            isOver ? "bg-fuchsia-500" : ""
-          }`}
-        >
-          Drop a Question here
-        </div>
-      )}
-      {questions.map((question, index) => (
-        <div
-          key={index}
-          className={`flex flex-col gap-1 pt-2 px-4 ${className} ${
-            isOver ? "bg-fuchsia-500" : ""
-          }`}
-        >
-          <div className="">
-            {index + 1}. {question.question}
-            <div className="pt-2">
-              {"options" in question &&
-                Array.isArray(question.options) && // type guard
-                question.options.map((option: string, optIndex: number) => (
-                  <div key={optIndex} className="flex flex-row gap-2">
-                    <div>{option}</div>
-                  </div>
-                ))}
+      {hasBloom && <BloomBoxes questionTypeKey={type} />}
+
+      <div
+        ref={droppable ? drop : null}
+        className={`text-neutral-300 ${className} ${
+          isOver ? "bg-fuchsia-500" : "bg-slate-800"
+        } `}
+      >
+        {questions.length === 0 && (
+          <div
+            className={`flex flex-col gap-1 pt-2 px-4 ${className} ${
+              isOver ? "bg-fuchsia-500" : ""
+            }`}
+          >
+            Drop a Question here
+          </div>
+        )}
+        {questions.map((question, index) => (
+          <div
+            key={index}
+            className={`flex flex-col gap-1 pt-2 px-4 ${className} ${
+              isOver ? "bg-fuchsia-500" : ""
+            }`}
+          >
+            <div className="">
+              {index + 1}. {question.question}
+              <div className="pt-2">
+                {"options" in question &&
+                  Array.isArray(question.options) && // type guard
+                  question.options.map((option: string, optIndex: number) => (
+                    <div key={optIndex} className="flex flex-row gap-2">
+                      <div>{option}</div>
+                    </div>
+                  ))}
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Section>
   );
 };
 
