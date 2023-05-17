feca94e1c91ab82c5ef91ba561c3e6db048bc0c4
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-18 02:54:11 +0530

    Add theme to the section

--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -1,12 +1,24 @@
+import { ColorOption, ThemeColor } from "@/types";
+import { colors } from "../theme/colors";
+
 type SectionProps = {
   title: string;
   children: React.ReactNode;
+  color?: ColorOption;
 };
 
-const Section: React.FC<SectionProps> = ({ title, children }) => {
+const Section: React.FC<SectionProps> = ({
+  title,
+  children,
+  color = "primary",
+}) => {
+  const { text, h1, border, bg } = colors[color] as ThemeColor;
+
   return (
     <div className="pt-3">
-      <header className="text-left font-medium text-slate-500 border-b border-solid border-slate-700 pb-2 mb-4">
+      <header
+        className={`${text} ${bg} ${border} text-left font-medium border-b border-solid pb-2 mb-4`}
+      >
         <p className="capitalize">{title}</p>
       </header>
       <div className="flex flex-col gap-3">{children}</div>
