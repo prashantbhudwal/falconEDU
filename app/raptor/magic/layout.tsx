2c9a0d0b1ad91d5b93a3ab2c0a775ae31fde5d91
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-17 15:13:17 +0530

    Add base boilerplate code

--- app/raptor/magic/layout.tsx
+++ app/raptor/magic/layout.tsx
@@ -1,13 +1,48 @@
 "use client";
+import { worksheetAidType } from "@/types";
+const worksheetAids: worksheetAidType[] = [
+  "worksheet",
+  "answer key",
+  "support",
+];
 import Section from "@/app/components/Section";
-import Sidebar from "../../components/Sidebar";
+import Sidebar from "@/app/components/Sidebar";
+import SidebarButton from "@/app/magic/components/SidebarButton";
+import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
+import { getEmoji, getName } from "@/app/utils";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd";
 export default function AidLayout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const pathname = usePathname();
   return (
-    <div className="grid grid-cols-12 gap-4 w-full">
-      <Sidebar className="col-start-1 col-span-2 row-start-1">
-        <Section title="Teaching Aids">Teaching Aids</Section>
-      </Sidebar>
-      <div className="col-start-3 col-span-8 min-h-screen">{children}</div>
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className="grid grid-cols-12 gap-4 w-full">
+        <Sidebar className="col-start-1 col-span-2 row-start-1">
+          <Section title="Teaching Aids">
+            <SidebarButton
+              onClick={() => router.push(`raptor/magic/worksheet`)}
+              isActive={pathname === `raptor/magic/worksheet`}
+            >
+              {`${getEmoji("worksheet")} ${getName("worksheet")}`}
+            </SidebarButton>
+            {worksheetAids.map(
+              (aid: worksheetAidType) =>
+                aid != "worksheet" && (
+                  <SidebarButton
+                    onClick={() => router.push(`/magic/aid/${aid}`)}
+                    isActive={pathname === `/magic/aid/${aid}`}
+                    key={aid}
+                  >
+                    {`${getEmoji(aid)} ${getName(aid)}`}
+                  </SidebarButton>
+                )
+            )}
+          </Section>
+        </Sidebar>
+        <div className="col-start-3 col-span-8 min-h-screen">{children}</div>
+      </div>
+    </DndProvider>
   );
 }
