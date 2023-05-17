e99b08f65864a9f4296ebd539465e1f9e48df309
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-05-18 02:47:57 +0530

    Add custom theme

---
+++ app/theme/colors.ts
@@ -0,0 +1,31 @@
+import { Theme } from "@/types";
+export const colors: Theme = {
+  primary: {
+    text: "text-emerald-500",
+    h2: "text-emerald-600",
+    h1: "text-emerald-400",
+    border: "border-emerald-700",
+    bg: "bg-slate-900",
+  },
+  secondary: {
+    text: "text-fuchsia-500",
+    h2: "text-fuchsia-600",
+    h1: "text-fuchsia-400",
+    border: "border-fuchsia-700",
+    bg: "bg-slate-900",
+  },
+  gray: {
+    text: "text-slate-500",
+    h2: "text-slate-500",
+    h1: "text-slate-800",
+    border: "border-slate-700",
+    bg: "bg-slate-100",
+  },
+  white: {
+    text: "text-slate-500",
+    h2: "text-slate-600",
+    h1: "text-slate-800",
+    border: "border-slate-600",
+    bg: "bg-slate-100",
+  },
+};
--- types/colorTypes.ts
+++ types/colorTypes.ts
@@ -1,1 +1,12 @@
-export type ColorOption = "primary" | "secondary" | "gray" | "black" | "";
+export type ColorOption = "primary" | "secondary" | "gray" | "white" | "";
+export type ThemeColor = {
+  text: string;
+  h2: string;
+  h1: string;
+  border: string;
+  bg: string;
+};
+
+export type Theme = {
+  [K in ColorOption]?: ThemeColor;
+};
