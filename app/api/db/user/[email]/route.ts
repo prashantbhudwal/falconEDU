64018ffa3f55ba37c25f4b6555e245c8d614f958
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-06-02 05:25:00 +0530

    Get all user data

--- app/api/db/user/[email]/route.ts
+++ app/api/db/user/[email]/route.ts
@@ -16,14 +16,6 @@ export async function GET(
     where: {
       email: email,
     },
-    select: {
-      id: true,
-      name: true,
-      email: true,
-      phone: true,
-      avatar: true,
-      accountType: true,
-    },
   });
 
   return NextResponse.json(user);
