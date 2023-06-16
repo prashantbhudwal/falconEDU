a0863531c7b9100b3a4b832bd235cf54b6bc43f2
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-06-16 09:38:24 +0530

    Reset to new db structure

--- prisma/migrations/20230613113703_add_default_values/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Question" ALTER COLUMN "learningObjectives" SET DEFAULT ARRAY[]::TEXT[],
-ALTER COLUMN "tags" SET DEFAULT ARRAY[]::TEXT[];
--- prisma/migrations/20230615064222_drop_teaching_resource_title/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `title` on the `TeachingResource` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "TeachingResource" DROP COLUMN "title";
