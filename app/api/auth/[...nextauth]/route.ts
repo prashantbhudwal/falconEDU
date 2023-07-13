d99e98adbe918da6e7c5268849afdec1f9611b87
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-07-13 18:24:31 +0530

    Write google based route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,52 +1,87 @@
-import NextAuth from "next-auth/next";
-import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import Google from "next-auth/providers/google";
+import prisma from "@/prisma";
+import NextAuth from "next-auth";
+import { User } from "@prisma/client";
+import { Adapter } from "next-auth/adapters";
+import { AuthOptions } from "next-auth";
 
-const handler = NextAuth({
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
+// Assign trial user properties
+const assignTrialUserProperties = async () => {
+  const trialStartDate = new Date();
+  const trialEndDate = new Date(trialStartDate);
+  trialEndDate.setDate(trialStartDate.getDate() + 3);
 
-        const res: any = await fetch(
-          `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/auth/login`,
-          {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              username: credentials?.username,
-              password: credentials?.password,
-            }),
-          }
-        );
-        const user = await res.json();
-        if (res.ok && user.email) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/auth/login",
-  },
+  return {
+    role: "TRIAL",
+    userType: "TEACHER",
+    subscriptionStart: trialStartDate,
+    subscriptionEnd: trialEndDate,
+  };
+};
+
+// Find or create profile for a Google user
+const googleProfileHandler = async (profile: any, tokens: any) => {
+  const existingUser: User | null = await prisma.user.findUnique({
+    where: { email: profile.email },
+  });
+
+  if (existingUser) {
+    const { sub, ...profileWithoutSub } = profile;
+    return {
+      id: profile.sub,
+      ...profileWithoutSub,
+    };
+  } else {
+    // Set trial user properties for new users
+    const trialUserProperties = await assignTrialUserProperties();
+    return {
+      id: profile.sub, // Google returns the id as 'sub'
+      name: profile.name,
+      email: profile.email,
+      image: profile.picture,
+      ...trialUserProperties,
+    };
+  }
+};
+
+// Provider for Google authentication
+const GoogleProvider = () => {
+  return Google({
+    clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    profile: googleProfileHandler,
+  });
+};
+
+// Session callback to set session data
+const sessionCallback = ({ session, user }: any) => {
+  if (user) {
+    session.user.role = user.role;
+    session.user.subscriptionStart = user.subscriptionStart;
+    session.user.subscriptionEnd = user.subscriptionEnd;
+  }
+  return session;
+};
+
+// The main handler
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [GoogleProvider()],
   callbacks: {
-    async jwt({ token, user }) {
-      return { ...token, ...user };
-    },
-
-    async session({ session, token }) {
-      session.user = token as any;
-      return session;
-    },
+    session: sessionCallback,
   },
-});
+  session: {
+    strategy: "jwt",
+    maxAge: 3 * 24 * 60 * 60, // 3 days
+  },
+  jwt: {
+    maxAge: 3 * 24 * 60 * 60, // 3 days
+  },
+  //   pages: {
+  //     signIn: "/signIn",
+  //   },
+};
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
