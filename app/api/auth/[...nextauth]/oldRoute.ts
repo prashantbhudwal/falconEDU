fc8d22b80cd476cf49bcb6bca4ea4078c0f3539e
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-07-13 18:24:23 +0530

    Move credentials route to oldRoute

---
+++ app/api/auth/[...nextauth]/oldRoute.ts
@@ -0,0 +1,52 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+
+        const res: any = await fetch(
+          `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/auth/login`,
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              username: credentials?.username,
+              password: credentials?.password,
+            }),
+          }
+        );
+        const user = await res.json();
+        if (res.ok && user.email) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/login",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+
+    async session({ session, token }) {
+      session.user = token as any;
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
