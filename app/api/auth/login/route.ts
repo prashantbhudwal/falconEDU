c75366957e3dfdaadf866fa57bd51e5cd2f621cd
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-07-15 06:42:19 +0530

    Comment out old route

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,39 +1,39 @@
-import prisma from "@/prisma";
+// import prisma from "@/prisma";
 
-type LoginRequestBody = {
-  username: string;
-  password: string;
-};
+// type LoginRequestBody = {
+//   username: string;
+//   password: string;
+// };
 
-export async function POST(request: Request) {
-  const body: LoginRequestBody = await request.json();
-  const { username, password } = body;
+// export async function POST(request: Request) {
+//   const body: LoginRequestBody = await request.json();
+//   const { username, password } = body;
 
-  try {
-    const user = await prisma.teacher.findUnique({
-      where: {
-        email: username,
-      },
-    });
+//   try {
+//     const user = await prisma.user.findUnique({
+//       where: {
+//         email: username,
+//       },
+//     });
 
-    if (user && user.password === password) {
-      const { password, ...userWithoutPassword } = user;
-      return new Response(JSON.stringify(userWithoutPassword));
-    } else {
-      return new Response(
-        JSON.stringify({ message: "Username or password is incorrect" }),
-        {
-          status: 401,
-        }
-      );
-    }
-  } catch (error) {
-    console.error("Error retrieving user data:", error);
-    return new Response(
-      JSON.stringify({ message: "Error retrieving user data" }),
-      {
-        status: 500,
-      }
-    );
-  }
-}
+//     if (user && user.password === password) {
+//       const { password, ...userWithoutPassword } = user;
+//       return new Response(JSON.stringify(userWithoutPassword));
+//     } else {
+//       return new Response(
+//         JSON.stringify({ message: "Username or password is incorrect" }),
+//         {
+//           status: 401,
+//         }
+//       );
+//     }
+//   } catch (error) {
+//     console.error("Error retrieving user data:", error);
+//     return new Response(
+//       JSON.stringify({ message: "Error retrieving user data" }),
+//       {
+//         status: 500,
+//       }
+//     );
+//   }
+// }
