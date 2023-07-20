954f7de7ad2af5d59bc8d538b1778a671115badd
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-07-20 20:29:19 +0530

    Add block chat component

---
+++ app/merlin/components/Chat.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+export default function Chat() {
+  const [message, setMessage] = useState("");
+
+  const handleChange = (event: any) => {
+    setMessage(event.target.value);
+  };
+
+  return (
+    <div className="fixed bottom-2 w-7/12 shadow-lg join">
+      <textarea
+        value={message}
+        onChange={handleChange}
+        className=" h-12 join-item w-full textarea textarea-bordered textarea-sm overflow-y-hidden resize-none max-h-48"
+        placeholder="Type your message here..."
+      />
+      <button className="join-item btn-square btn btn-primary text-xl">{`>`}</button>
+    </div>
+  );
+}
