ca4cd31c4f01e1c623f636e67606fb722862bcc7
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-07-21 18:46:23 +0530

    Update prompt

--- app/api/blockChat/route.ts
+++ app/api/blockChat/route.ts
@@ -21,13 +21,11 @@ export async function POST(request: NextRequest) {
   const { MODEL, TEMPERATURE, MAX_TOKENS, STREAM } = ideaOptions;
 
   const { prompt, content } = await request.json();
-console.log("prompt", prompt)
-console.log("content", content)
-  
+
   const messages: ChatCompletionRequestMessage[] = [
     {
       role: "system",
-      content: `You take a '''block''' of educational content and then transform it based on '''instructions''' provided by the teacher. Reply only with the content of the new transformed block. Don't include any flags like - block or instruction. Reject any other questions that do not involve transforming one block to another. In case of rejection, first, inform about rejection -- give a reason for rejection and then, reply with original block.`,
+      content: `You take a '''block''' of educational content and then transform it based on '''instructions''' provided by the teacher. Reply only with the content of the new transformed block. Don't include any flags like - block or instruction. Reject any other questions that do not involve transforming one block to another. In case of rejection, reply with content of the original block.`,
     },
     {
       role: "user",
