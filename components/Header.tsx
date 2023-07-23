b824bc80daea4df73087d8d8f2f6708e5a05c85c
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-07-23 08:42:12 +0530

    Move theme to root

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import { ColorOption, ThemeColor } from "@/types";
-import { colors } from "../app/theme/colors";
+import { colors } from "../theme/colors";
 export type HeaderProps = {
   className?: string;
   color?: ColorOption;
--- components/Section.tsx
+++ components/Section.tsx
@@ -1,5 +1,5 @@
 import { ColorOption, ThemeColor } from "@/types";
-import { colors } from "../app/theme/colors";
+import { colors } from "../theme/colors";
 
 type SectionProps = {
   title: string;
