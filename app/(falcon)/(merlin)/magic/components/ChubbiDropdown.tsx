e5c7093fb6ce498614d8a8c6927a466e99e201d5
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-07-25 06:46:46 +0530

    Add dropdown component for main menu

---
+++ app/(falcon)/(merlin)/magic/components/ChubbiDropdown.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FiChevronDown } from "react-icons/fi";
+export default function ChubbiDropdown() {
+  return (
+    <div className="dropdown">
+      <label
+        tabIndex={0}
+        className="btn btn-md btn-ghost flex flex-row gap-1 shadow-md shadow-slate-950"
+      >
+        <Image src={"/chubbi.png"} height={25} width={25} alt="Falcon Logo" />
+        <FiChevronDown className="text-secondary font-medium text-base" />
+      </label>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+      >
+        <li>
+          <Link href="/preferences">Lesson Settings</Link>
+        </li>
+        <li>
+          <Link href="https://www.youtube.com/watch?v=Rh9pBJRJ0zI">
+            Watch the demo
+          </Link>
+        </li>
+        <li>
+          <Link href="https://www.linkedin.com/company/falconassistant/">
+            LinkedIn
+          </Link>
+        </li>
+        <li>
+          <Link href="https://falconai.in">Website</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
