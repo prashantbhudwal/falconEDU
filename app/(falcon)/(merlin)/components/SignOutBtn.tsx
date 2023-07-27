96bb00ebf375df479e8543beeff4f5449a394192
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-07-27 07:57:54 +0530

    Add signout button to the upgrade page

--- app/(falcon)/(merlin)/components/SignOutBtn.tsx
+++ app/(falcon)/(merlin)/components/SignOutBtn.tsx
@@ -1,6 +1,15 @@
 "use client";
+
 import { signOut } from "next-auth/react";
 
-export default function SignOutButton() {
-  return <button onClick={() => signOut()}>Sign Out</button>;
+interface SignOutButtonProps {
+  className?: string;
+}
+
+export default function SignOutButton({ className }: SignOutButtonProps) {
+  return (
+    <button className={className} onClick={() => signOut()}>
+      Sign Out
+    </button>
+  );
 }
