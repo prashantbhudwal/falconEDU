f8ecb388df94ec9cf33c49d3e52f684634f2d1cd
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-07 10:40:31 +0530

    Add plans config

---
+++ app/(user)/pricing/plans.ts
@@ -0,0 +1,34 @@
+export const plans = [
+  {
+    title: "Trial",
+    description: "Give it a try, see if it fits your needs.",
+    price: "₹0",
+    duration: "/ 14 days",
+    features: ["CBSE Curriculum", "Unlimited Lessons", "Latest AI Models"],
+  },
+  {
+    title: "Pro",
+    description: "Relevant for multiple users, extended & premium support.",
+    price: "₹299",
+    duration: "/month",
+    features: [
+      "CBSE Curriculum",
+      "Unlimited Lessons",
+      "Unlimited Worksheets",
+      "Latest AI Models",
+    ],
+  },
+  {
+    title: "Schools",
+    description: "Relevant for multiple users, extended & premium support.",
+    price: "Custom",
+    duration: "",
+    features: [
+      "CBSE Curriculum",
+      "Unlimited Lessons",
+      "Unlimited Worksheets",
+      "Latest AI Models",
+      "24x7 Support",
+    ],
+  },
+];
