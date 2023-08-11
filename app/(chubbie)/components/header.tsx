4dce4b4fabfe70a610c80f87f27a806c6ce22566
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-11 10:20:12 +0530

    De-clutter component

--- app/(chubbie)/components/header.tsx
+++ app/(chubbie)/components/header.tsx
@@ -1,27 +1,21 @@
 import * as React from "react";
 import Link from "next/link";
 
-import { cn } from "../lib/utils";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import { clearChats } from "../actions";
-import { Button, buttonVariants } from "../components/ui/button";
 import { Sidebar } from "../components/sidebar";
 import { SidebarList } from "../components/sidebar-list";
 import {
-  IconGitHub,
   IconNextChat,
-  IconSeparator,
-  IconVercel,
 } from "../components/ui/icons";
 import { SidebarFooter } from "../components/sidebar-footer";
 import { ClearHistory } from "../components/clear-history";
-import { UserMenu } from "../components/user-menu";
 
 export async function Header() {
   const session = await getServerSession(authOptions);
   return (
-    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
+    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 backdrop-blur-xl">
       <div className="flex items-center">
         {session?.user ? (
           <Sidebar>
@@ -39,36 +33,6 @@ export async function Header() {
             <IconNextChat className="hidden w-6 h-6 mr-2 dark:block" />
           </Link>
         )}
-        <div className="flex items-center">
-          <IconSeparator className="w-6 h-6 text-muted-foreground/50" />
-          {session?.user ? (
-            <UserMenu user={session.user} />
-          ) : (
-            <Button variant="link" asChild className="-ml-2">
-              <Link href="/sign-in?callbackUrl=/">Login</Link>
-            </Button>
-          )}
-        </div>
-      </div>
-      <div className="flex items-center justify-end space-x-2">
-        <a
-          target="_blank"
-          href="https://github.com/vercel/nextjs-ai-chatbot/"
-          rel="noopener noreferrer"
-          className={cn(buttonVariants({ variant: "outline" }))}
-        >
-          <IconGitHub />
-          <span className="hidden ml-2 md:flex">GitHub</span>
-        </a>
-        <a
-          href="https://github.com/vercel/nextjs-ai-chatbot/"
-          target="_blank"
-          className={cn(buttonVariants())}
-        >
-          <IconVercel className="mr-2" />
-          <span className="hidden sm:block">Deploy to Vercel</span>
-          <span className="sm:hidden">Deploy</span>
-        </a>
       </div>
     </header>
   );
