47482c9748199398e9c6aa02ec76c55f8e8447dd
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-11 12:03:57 +0530

    Remove unused code

--- app/(chubbie)/layout.tsx
+++ app/(chubbie)/layout.tsx
@@ -1,14 +1,8 @@
 import { TooltipProvider } from "./components/ui/tooltip";
-import { Header } from "./components/header";
 export default function ChubbiLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <TooltipProvider>
-      <Header />
-      {children}
-    </TooltipProvider>
-  );
+  return <TooltipProvider>{children}</TooltipProvider>;
 }
