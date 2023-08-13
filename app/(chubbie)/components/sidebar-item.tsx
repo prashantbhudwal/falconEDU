2e689719e67d49d2d43cef621aaf5efdd48b8f2d
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-13 08:35:36 +0530

    Remove unused code

--- app/(chubbie)/components/sidebar-item.tsx
+++ app/(chubbie)/components/sidebar-item.tsx
@@ -1,39 +1,33 @@
-'use client'
-import SidebarButton from '@/app/(falcon)/(merlin)/magic/components/SidebarButton'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+"use client";
+import SidebarButton from "@/app/(falcon)/(merlin)/magic/components/SidebarButton";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { type Chat } from "../lib/types";
-import { cn } from '../lib/utils'
-import { buttonVariants } from '../components/ui/button'
-import { IconMessage, IconUsers } from '../components/ui/icons'
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger
-} from '../components/ui/tooltip'
-
+import { cn } from "../lib/utils";
+import { buttonVariants } from "../components/ui/button";
+import { IconMessage } from "../components/ui/icons";
 interface SidebarItemProps {
-  chat: Chat
-  children: React.ReactNode
+  chat: Chat;
+  children: React.ReactNode;
 }
 
 export function SidebarItem({ chat, children }: SidebarItemProps) {
-  const pathname = usePathname()
-  const isActive = pathname === chat.path
+  const pathname = usePathname();
+  const isActive = pathname === chat.path;
 
-  if (!chat?.id) return null
+  if (!chat?.id) return null;
 
   return (
-    <div className="relative">
-      <div className="absolute left-2 top-1 flex h-6 w-6 items-center justify-center"> 
-          <IconMessage className="mr-2" />
+    <div className="relative w-full">
+      <div className="absolute left-2 top-1 flex h-6 w-6 items-center justify-center">
+        <IconMessage className="mr-2" />
       </div>
       <Link
         href={chat.path}
         className={cn(
-          buttonVariants({ variant: 'ghost' }),
-          'group w-full pl-8 pr-16',
-          isActive && 'bg-accent'
+          buttonVariants({ variant: "ghost" }),
+          "group w-full pl-8 pr-16",
+          isActive && "bg-slate-600"
         )}
       >
         <div
@@ -45,5 +39,5 @@ export function SidebarItem({ chat, children }: SidebarItemProps) {
       </Link>
       {isActive && <div className="absolute right-2 top-1">{children}</div>}
     </div>
-  )
+  );
 }
