c4996b6db5b4ec8bf53f04b6ef317deb0b69fe26
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-13 16:26:28 +0530

    Remove Metadata

--- app/(chubbie)/chat/[id]/page.tsx
+++ app/(chubbie)/chat/[id]/page.tsx
@@ -1,51 +1,32 @@
-import { type Metadata } from 'next'
-import { notFound, redirect } from 'next/navigation'
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions'
-import { getChat } from '../../actions'
-import { Chat } from '../../components/chat'
-
+import { type Metadata } from "next";
+import { notFound, redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
+import { getChat } from "../../actions";
+import { Chat } from "../../components/chat";
 
 export interface ChatPageProps {
   params: {
-    id: string
-  }
-}
-
-export async function generateMetadata({
-  params
-}: ChatPageProps): Promise<Metadata> {
-  const session = await getServerSession(authOptions);
-
-  if (!session?.user) {
-    return {}
-  }
-  if (!session?.user?.id) {
-    return {};
-  }
-
-  const chat = await getChat(params.id, session?.user?.id)
-  return {
-    title: chat?.title.toString().slice(0, 50) ?? 'Chat'
-  }
+    id: string;
+  };
 }
 
 export default async function ChatPage({ params }: ChatPageProps) {
   const session = await getServerSession(authOptions);
 
   if (!session?.user || !session?.user?.id) {
-    redirect(`/`)
+    redirect(`/`);
   }
 
-  const chat = await getChat(params.id, session.user.id)
+  const chat = await getChat(params.id, session.user.id);
 
   if (!chat) {
-    notFound()
+    notFound();
   }
 
   if (chat?.userId !== session?.user?.id) {
-    notFound()
+    notFound();
   }
 
-  return <Chat id={chat.id} initialMessages={chat.messages} />
+  return <Chat id={chat.id} initialMessages={chat.messages} />;
 }
