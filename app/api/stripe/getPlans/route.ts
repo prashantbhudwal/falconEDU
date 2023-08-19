c71b5044f73cce2bb3638719f3f37223d1f7647c
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-19 18:56:54 +0530

    Get prices from stripe

--- app/api/stripe/getPlans/route.ts
+++ app/api/stripe/getPlans/route.ts
@@ -1,11 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
-
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/authOptions";
+export const dynamic = "force-dynamic";
 export async function GET(req: NextRequest, res: NextResponse) {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
     apiVersion: "2023-08-16",
   });
-
   const prices = await stripe.prices.list();
   console.log(prices);
+  return NextResponse.json(prices);
 }
