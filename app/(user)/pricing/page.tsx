a1b75aa2b91721eff38ad4412d43a8c85888cdb3
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-24 04:09:44 +0530

    Fix pricing page

--- app/(user)/pricing/page.tsx
+++ app/(user)/pricing/page.tsx
@@ -1,11 +1,27 @@
 import Navbar from "@/app/(falcon)/(merlin)/components/Navbar";
-import PlansGrid from "./PlansGrid";
+import Plans from "./Plans";
+import { cache } from "react";
+import { stripe } from "@/utils/stripe";
+import Stripe from "stripe";
 
-export default function Pricing() {
+export type GetPricesReturnType = Stripe.ApiList<Stripe.Price>;
+export type GetProductsReturnType = Stripe.ApiList<Stripe.Product>;
+
+export const revalidate = 3600; // revalidate the data at most every hour
+
+const getProducts = cache(async () => {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+  return products;
+});
+
+export default async function Pricing() {
+  const products = await getProducts();
   return (
     <div className="h-screen w-screen bg-base-300">
       <Navbar />
-      <PlansGrid />
+      <Plans products={products.data} />
     </div>
   );
 }
