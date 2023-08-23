cc5af585496c9beb15a743ad6e34311427abfd26
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-24 04:09:25 +0530

    Remove unused code

--- utils/stripe/index.ts
+++ utils/stripe/index.ts
@@ -1,14 +1,9 @@
 import Stripe from "stripe";
-import { cache } from "react";
-import { headers } from "next/headers";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2023-08-16",
 });
 
-export type GetPricesReturnType = Stripe.ApiList<Stripe.Price>;
 
-export const getPrices = cache(async () => {
-  const prices = await stripe.prices.list();
-  return prices;
-});
+
+
