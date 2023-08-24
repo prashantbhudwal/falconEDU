784e9919b8ad577bbd7acfb361f55adb9fa53e60
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-24 07:49:06 +0530

    Extract function

--- app/(user)/pricing/page.tsx
+++ app/(user)/pricing/page.tsx
@@ -3,18 +3,12 @@ import Plans from "./Plans";
 import { cache } from "react";
 import { stripe } from "@/utils/stripe";
 import Stripe from "stripe";
+import { getProducts } from "@/utils/stripe";
 
 export type GetPricesReturnType = Stripe.ApiList<Stripe.Price>;
 export type GetProductsReturnType = Stripe.ApiList<Stripe.Product>;
 
 export const revalidate = 3600; // revalidate the data at most every hour
-
-const getProducts = cache(async () => {
-  const products = await stripe.products.list({
-    expand: ["data.default_price"],
-  });
-  return products;
-});
 
 export default async function Pricing() {
   const products = await getProducts();
--- utils/stripe/index.ts
+++ utils/stripe/index.ts
@@ -1,9 +1,17 @@
 import Stripe from "stripe";
+import { cache } from "react";
+
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2023-08-16",
 });
 
+export const getProducts = cache(async () => {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+  return products;
+});
 
 
 
