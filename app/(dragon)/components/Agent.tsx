b3e494eb940426db465982ed0b115e4b152a1b9f
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-26 23:31:38 +0530

    Add dragon boilerplate

---
+++ app/(dragon)/components/Agent.tsx
@@ -0,0 +1,7 @@
+export default function Name() {
+  return (
+    <div>
+      <h1>Agent One</h1>
+    </div>
+  );
+}
---
+++ app/(dragon)/dragon/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Dragon() {
+  return (
+    <div>
+      <h1>Hello Page</h1>
+    </div>
+  );
+}
