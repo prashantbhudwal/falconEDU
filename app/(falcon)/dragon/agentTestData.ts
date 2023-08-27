4e76b297f03c5a6f42dc2a5e85015d4f098687e1
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-27 06:40:29 +0530

    Add more dummy data

--- app/(falcon)/dragon/agentTestData.ts
+++ app/(falcon)/dragon/agentTestData.ts
@@ -13,6 +13,9 @@ export const agentDataArray = [
   agentData,
   { ...agentData, id: "222" },
   { ...agentData, id: "333" },
+  { ...agentData, id: "444" },
+  { ...agentData, id: "555" },
+  { ...agentData, id: "666" },
 ];
 
 export type AgentTestData = typeof agentData;
