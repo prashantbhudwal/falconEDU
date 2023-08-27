a442d6433cac0e19005bb2dfc21d6dc05c5044cb
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-28 00:50:25 +0530

    Move chubbi utils to root

--- app/(chubbi)/components/ui/sheet.tsx
+++ app/(chubbi)/components/ui/sheet.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import * as SheetPrimitive from "@radix-ui/react-dialog";
 
-import { cn } from "../../lib/utils";
+import { cn } from "../../../../lib/utils";
 import { IconClose } from "./icons";
 
 const Sheet = SheetPrimitive.Root;
