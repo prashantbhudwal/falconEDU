b20875b3538266c229631885e24b185737e64afc
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-28 08:07:35 +0530

    Add theme providers

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { Provider } from "jotai";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "./theme-provider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider>
-      <Provider>{children}</Provider>
-    </SessionProvider>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+      <SessionProvider>
+        <Provider>{children}</Provider>
+      </SessionProvider>
+    </ThemeProvider>
   );
 }
