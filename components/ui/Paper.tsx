f3a66235de28a9af111de8fc616d7ca85f72fcf4
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-28 18:37:49 +0530

    Add paper component

--- components/ui/Paper.tsx
+++ components/ui/Paper.tsx
@@ -1,7 +1,20 @@
-export default function Paper() {
-  return (
-    <div>
-      <h1> Component</h1>
-    </div>
-  );
-}
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+const Paper = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "max-w-5xl w-5/6 shadow-md rounded-sm shadow-base-300 my-4 p-4",
+      className
+    )}
+    {...props}
+  />
+));
+Paper.displayName = "Paper";
+
+export { Paper };
