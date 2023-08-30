e53bb38206e7dfbb724fc7a6483a2ada52b7cad3
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-30 22:45:58 +0530

    Reorganize schema

--- app/(falcon)/dragon/agentSchema.ts
+++ app/(falcon)/dragon/agentSchema.ts
@@ -1,21 +1,29 @@
 import { z } from "zod";
+export const grades = [
+  "Grade 1",
+  "Grade 2",
+  "Grade 3",
+  "Grade 4",
+  "Grade 5",
+] as const;
+export const humorLevel = ["High", "Moderate", "Low"] as const;
 
 export const agentSchema = z.object({
   // Core Config
-  instructions: z.string(), // Textbox
+  nickname: z.string().min(1).max(20),
+  instructions: z.string(),
   teacherIntro: z.string(),
-  tone: z.enum(["Friendly", "Professional", "Casual"]),
-  responseTime: z.union([
-    z.literal("Instant"),
-    z.literal("1 Min"),
-    z.literal("5 Min"),
-  ]),
   subjects: z.array(z.string()),
+  grades: z.array(z.enum(grades)),
+  targetBoards: z.array(z.string()),
+  studentTechComfort: z.enum(["Beginner", "Intermediate", "Advanced"]),
 
   // Interaction
+  tone: z.enum(["Friendly", "Professional", "Casual"]),
   languagePreferences: z.array(z.string()),
   feedbackLoop: z.boolean(),
-  humorLevel: z.enum(["High", "Moderate", "Low"]),
+  humorLevel: z.enum(humorLevel),
+  languageGradeLevel: z.enum(grades),
 
   // Customization & Personalization
   customPhrases: z.array(z.string()),
@@ -28,13 +36,4 @@ export const agentSchema = z.object({
   debateTolerance: z.enum(["Encouraged", "Neutral", "Discouraged"]),
 
   // Student Info
-  targetGradeLevels: z.array(
-    z.enum(["Elementary", "High School", "College", "Postgraduate"])
-  ),
-  targetBoards: z.array(z.string()),
-  targetCourseTypes: z.array(z.string()),
-  studentTechComfort: z.enum(["Beginner", "Intermediate", "Advanced"]),
-  parentInvolvement: z.enum(["High", "Low", "None"]),
 });
-
-
