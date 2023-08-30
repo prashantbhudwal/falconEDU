ea31735f481b8b998b66618455a0d31e375f678d
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-30 07:18:59 +0530

    Remove unused code

--- app/(falcon)/dragon/components/Agent.tsx
+++ app/(falcon)/dragon/components/Agent.tsx
@@ -1,7 +1,5 @@
 import { Paper } from "@/components/ui/paper";
-import { Button } from "@ui/button";
-import { TextareaAutosize } from "@ui/textarea-autosize";
-import AgentForm from "./agent-config";
+import AgentForm from "./agent-config-form";
 type AgentProps = {
   data?: {};
   id?: string;
@@ -11,9 +9,6 @@ export default function Agent({ id, data }: AgentProps) {
   return (
     <Paper className="flex flex-col mx-auto h-full">
       <AgentForm />
-      <TextareaAutosize />
-      <h1>Agent One</h1>
-      <Button>Click Me</Button>
     </Paper>
   );
 }
