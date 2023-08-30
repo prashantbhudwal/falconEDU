71bc0c7b326c22fca33c88dad8ed1f05a2622ff1
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-30 21:05:04 +0530

    Move dashboard to dragon root

--- app/(falcon)/dragon/page.tsx
+++ app/(falcon)/dragon/page.tsx
@@ -1,7 +1,18 @@
-import { nanoid } from "@/lib";
-import Agent from "./components/agent";
-
-export default function Dragon() {
-  const id = nanoid();
-  return <Agent id={id} />;
+import { getAgents } from "./actions";
+import AgentPreview from "./components/agent-preview-card";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
+import { getServerSession } from "next-auth";
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+  const id = session?.user?.id;
+  const agents = await getAgents(id);
+  return (
+    <div className="flex justify-center w-full">
+      <div className="flex flex-wrap gap-4 w-11/12">
+        {agents.map((agent) => (
+          <AgentPreview key={agent.id} data={agent} />
+        ))}
+      </div>
+    </div>
+  );
 }
