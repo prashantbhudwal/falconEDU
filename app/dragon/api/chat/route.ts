9ea5433533b2878164f8bd8a8e97b52822c62c1a
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-08 00:23:10 +0530

    Implement route with langchain

--- app/dragon/api/chat/route.ts
+++ app/dragon/api/chat/route.ts
@@ -2,35 +2,39 @@ import { NextRequest } from "next/server";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 import { ChatCompletionRequestMessage } from "openai-edge";
 import { openai } from "@/app/api/lib/openAI/config";
+import { LangChainStream } from "ai";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { HumanMessage, AIMessage, SystemMessage } from "langchain/schema";
 
 const handleCompletion = async (completion: string, json: any) => {
   console.log(completion);
   console.log(json);
 };
-const base: ChatCompletionRequestMessage[] = [
-  {
-    role: "system",
-    content: `You are a dancing cow!`,
-  },
-];
-export async function POST(req: NextRequest) {
 
+export async function POST(req: NextRequest) {
   const json = await req.json();
   console.log(json);
   let { messages } = json;
-  messages = [...base, ...messages];
-  const res = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages,
-    temperature: 0.7,
-    stream: true,
-  });
 
-  const stream = OpenAIStream(res, {
+  const { stream, handlers, writer } = LangChainStream({
     async onCompletion(completion) {
       await handleCompletion(completion, json);
     },
   });
+
+  const llm = new ChatOpenAI({
+    streaming: true,
+  });
+
+  const history = messages.map((m: any) =>
+    m.role == "user" ? new HumanMessage(m.content) : new AIMessage(m.content)
+  );
+  const systemMessage = new SystemMessage(
+    "You will only answer questions about food."
+  );
+  const array = [systemMessage, ...history];
+
+  llm.call(array, {}, [handlers]).catch(console.error);
 
   return new StreamingTextResponse(stream);
 }
