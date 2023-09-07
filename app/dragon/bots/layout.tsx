f0c94f9238c1bdf0bbe9eeb8e8cc7c4820fc52b7
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-08 00:16:34 +0530

    Fix: Horizontal scrollbar appears

--- app/dragon/bots/layout.tsx
+++ app/dragon/bots/layout.tsx
@@ -6,11 +6,9 @@ export default function ChatLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen w-full">
       <Navbar />
-      <main className="flex flex-col flex-1 w-screen">
-        {children}
-      </main>
+      <main className="flex flex-col flex-1">{children}</main>
     </div>
   );
 }
