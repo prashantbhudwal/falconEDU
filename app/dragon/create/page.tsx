92d809c595e3a9a1e6c83718798a1b34cff265a8
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-08 12:10:33 +0530

    Display new bot card

--- app/dragon/create/page.tsx
+++ app/dragon/create/page.tsx
@@ -2,6 +2,7 @@ import { getBots } from "./actions";
 import BotPreview from "./components/bot-preview-card";
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import { getServerSession } from "next-auth";
+import { NewBotCard } from "./components/new-bot-card";
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
   const id = session?.user?.id;
@@ -9,6 +10,7 @@ export default async function Dashboard() {
   return (
     <div className="flex justify-center w-full">
       <div className="flex flex-wrap gap-4 w-11/12 py-6">
+        <NewBotCard />
         {bots.map((bot) => (
           <BotPreview key={bot.id} data={bot} />
         ))}
