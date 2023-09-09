dbf280cdbf27b75b7ec335439605244c51189d7b
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-09 06:40:08 +0530

    Boilerplate for all class component

---
+++ app/dragon/create/class/page.tsx
@@ -0,0 +1,50 @@
+import { cache } from "react";
+import prisma from "@/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
+import Link from "next/link";
+export const revalidate = 3600; // revalidate the data at most every hour
+
+const getClassesByTeacher = cache(async function (teacherId: string) {
+  const classes = await prisma.class.findMany({
+    where: {
+      teacherId: teacherId,
+    },
+    select: {
+      id: true,
+      name: true,
+      // Add more fields as needed
+    },
+  });
+  return classes;
+});
+
+export default async function Classes() {
+  const session = await getServerSession(authOptions);
+  const teacherId = session?.user?.id;
+  if (!teacherId) {
+    return null;
+  }
+
+  const userType = await prisma.user.findUnique({
+    where: {
+      id: teacherId,
+    },
+    select: {
+      userType: true,
+    },
+  });
+
+  if (userType?.userType !== "TEACHER") {
+    return null;
+  }
+  const classes = await getClassesByTeacher(teacherId);
+  console.log(classes);
+
+  return (
+    <div>
+      Classes
+      <Link href={`/dragon/create/class/edit/${teacherId}`}>Test</Link>
+    </div>
+  );
+}
