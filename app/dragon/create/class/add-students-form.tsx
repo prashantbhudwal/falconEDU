00d486f728625a47ef42d01a96fad20432233899
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-09 06:22:20 +0530

    Add form to add students

---
+++ app/dragon/create/class/add-students-form.tsx
@@ -0,0 +1,63 @@
+"use client";
+import prisma from "@/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
+import { Button } from "@/components/ui/button";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormItem,
+  FormLabel,
+  FormField,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const classFormSchema = z.object({
+  emails: z.string().email().nonempty(),
+});
+
+export default function ClassForm({
+  onSubmit,
+  className,
+}: {
+  onSubmit: (values: z.infer<typeof classFormSchema>) => void;
+  className?: string;
+}) {
+  const form = useForm<z.infer<typeof classFormSchema>>({
+    resolver: zodResolver(classFormSchema),
+    defaultValues: {
+      emails: "",
+    },
+  });
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-row gap-6"
+      >
+        <FormField
+          control={form.control}
+          name="emails"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type="email"
+                  placeholder="Enter student email."
+                  className="w-72"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Add to class</Button>
+      </form>
+    </Form>
+  );
+}
