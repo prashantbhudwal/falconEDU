c294e19f10bef71b5082ee5ca1024950be196651
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-10 05:50:54 +0530

    Fix html nesting

--- components/beta.tsx
+++ components/beta.tsx
@@ -1,7 +1,7 @@
 export default function Beta({ children }: { children: React.ReactNode }) {
   return (
     <div className="inline-flex items-start">
-      <p className="">{children}</p>
+      <div className="">{children}</div>
       <span className="-mt-1 ml-2 rounded bg-yellow-300 px-2 py-1 text-sm font-semibold text-yellow-800">
         beta
       </span>
