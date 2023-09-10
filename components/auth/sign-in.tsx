bb389601863815e7334279149ed2fe6aee083d12
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-10 13:30:14 +0530

    Add sign-in handler and use effect

--- components/auth/sign-in.tsx
+++ components/auth/sign-in.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { useEffect } from "react";
 import { signIn, useSession } from "next-auth/react";
 import { PropagateLoader } from "react-spinners";
 import Beta from "@/components/beta";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import {
   Card,
   CardContent,
@@ -12,7 +14,25 @@ import {
   CardTitle,
 } from "../ui/card";
 export default function SignIn() {
+  const router = useRouter();
   const { data: session, status: sessionStatus } = useSession();
+
+  useEffect(() => {
+    console.log(session);
+    if (session && sessionStatus === "authenticated") {
+      if (session.user.userType === "TEACHER") router.push("/preferences");
+      else if (session.user.userType === "STUDENT") router.push("/dragon/bots");
+    }
+  }, [session, sessionStatus, router]);
+
+  const handleSignIn = async (provider: string, callbackUrl: string) => {
+    try {
+      const data = await signIn(provider, { callbackUrl });
+      console.log(data);
+    } catch (error) {
+      console.log("SignIn Error", error);
+    }
+  };
 
   return (
     <div className="flex flex-col items-center pt-8 text-center">
@@ -27,7 +47,7 @@ export default function SignIn() {
         is easy to use and strictly follows your syllabus.
       </div>
       <button
-        onClick={() => signIn("google", { callbackUrl: "/preferences" })}
+        onClick={() => handleSignIn("google", "/preferences")}
         className={`rounded-lg bg-emerald-500 px-28 py-4 text-lg font-semibold text-slate-800 transition duration-200 ease-in-out hover:bg-emerald-600`}
       >
         {sessionStatus === "loading"
@@ -40,6 +60,16 @@ export default function SignIn() {
         Works on large screens only. Use chrome, edge or any major browser for
         access.
       </div>
+      <button
+        onClick={() => handleSignIn("google-student", "/preferences")}
+        className={`rounded-lg bg-emerald-500 px-28 py-4 text-lg font-semibold text-slate-800 transition duration-200 ease-in-out hover:bg-emerald-600`}
+      >
+        {sessionStatus === "loading"
+          ? "Signing you in..."
+          : sessionStatus === "authenticated"
+          ? "Taking you to the app..."
+          : "Sign In as Student"}
+      </button>
       {(sessionStatus === "loading" || sessionStatus === "authenticated") && (
         <div className="flex h-12 flex-col items-center justify-center gap-2">
           <PropagateLoader color={"#10B981"} />
