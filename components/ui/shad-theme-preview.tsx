d0820fd8661866e673ca4a0d531b7d6da6f88d0c
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-11 06:35:00 +0530

    Add component to preview shad theme

---
+++ components/ui/shad-theme-preview.tsx
@@ -0,0 +1,35 @@
+import { Card, CardContent } from "@/components/ui/card";
+export default function ShadThemePreview() {
+  const themeColors = [
+    { css: "bg-background", text: "text-foreground" },
+    { css: "bg-card", text: "text-card-foreground" },
+    { css: "bg-popover", text: "text-popover-foreground" },
+    { css: "bg-primary", text: "text-primary-foreground" },
+    { css: "bg-secondary", text: "text-secondary-foreground" },
+    { css: "bg-muted", text: "text-muted-foreground" },
+    { css: "bg-accent", text: "text-accent-foreground" },
+    { css: "bg-destructive", text: "text-destructive-foreground" },
+    { css: "bg-border", text: "text-base-200" },
+    { css: "bg-input", text: "text-base-200t" },
+    { css: "bg-ring", text: "text-base-200" },
+    { css: "bg-base", text: "text-base-200" },
+    { css: "bg-base-100", text: "text-base-200" },
+    { css: "bg-base-200", text: "text-base-300" },
+    { css: "bg-base-300", text: "text-base-100" },
+  ];
+
+  return (
+    <Card className="w-full p-4 flex flex-col  gap-3">
+      <h1 className="text-3xl">Theme</h1>
+      {themeColors.map((color, index) => (
+        <Card key={index}>
+          <CardContent className={color.css}>
+            <p className={`${color.text} text-lg`}>
+              {color.css}, {color.text}
+            </p>
+          </CardContent>
+        </Card>
+      ))}
+    </Card>
+  );
+}
