b36c3f643e2a65b4d05713dddaf72cd77dbad8e8
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-11 07:10:32 +0530

    Add page to fix HMR Error

---
+++ app/dragon/classes/page.tsx
@@ -0,0 +1,5 @@
+//BUG Added this page because in the current version of next js: 13.4.9, the HMR does not work with the layout in @parallel routes without this. I had to rebuild every time to see any changes.
+
+export default function Page() {
+  return null;
+}
