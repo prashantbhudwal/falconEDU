abe63fe3a9385a640bd7187084df527747ddaecc
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-11 14:54:29 +0530

    Fix styles

--- app/dragon/classes/class/[classId]/bots/components/bot-preview-card.tsx
+++ app/dragon/classes/class/[classId]/bots/components/bot-preview-card.tsx
@@ -1,33 +1,29 @@
-import Link from "next/link";
-import { BotTestData } from "../../../test-data";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardContent,
+  CardDescription,
+} from "@/components/ui/card";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import Avvvatars from "avvvatars-react";
-import { getBotLink } from "../config";
-import { PreviewCard } from "./ui/preview-card";
-type BotPreviewProps = {
-  data: BotTestData;
+type ClassCardProps = {
+  name?: string;
+  children?: React.ReactNode;
+  className?: string;
 };
-import { nanoid } from "nanoid";
-
-export default function BotPreview({ data }: BotPreviewProps) {
-  const botLink = getBotLink(data.id);
+export default function BotCard({
+  name,
+  children,
+  className,
+  ...props
+}: ClassCardProps) {
   return (
-    <Link href={botLink}>
-      <PreviewCard>
-        <figure className="py-6">
-          <Avvvatars value={nanoid()} style="shape" size={100} />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title justify-center">{data.name}</h2>
-          <div className="card-actions justify-center">
-            <div className="badge badge-accent">{data.curriculum}</div>
-            {data.subjects.map((subject) => (
-              <div className="badge badge-info" key={subject}>
-                {subject}
-              </div>
-            ))}
-          </div>
-        </div>
-      </PreviewCard>
-    </Link>
+    <Card {...props}>
+      <CardHeader>
+        <CardTitle className={className}>{children}</CardTitle>
+      </CardHeader>
+    </Card>
   );
 }
