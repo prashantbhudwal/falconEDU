b030f2380ea27bdb1009747a35271e2c33471f91
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-14 18:20:45 +0530

    Remove redundant data

---
+++ prisma/migrations/20230914125027_remove_redundant_defualtchat_id/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `defaultChatId` on the `Bot` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Bot" DROP COLUMN "defaultChatId";
