6b3799789e60580c28b44f5253f88fadcd4e6e0b
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-14 15:04:35 +0530

    Remove unused code

--- app/dragon/teacher/class/[classId]/students/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function ClassLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div>{children}</div>;
-}
--- app/dragon/teacher/class/[classId]/students/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import ClassForm from "../add-students-form";
-import { Paper } from "@/components/ui/Paper";
-export default function NewClass() {
-  return (
-    <Paper className="w-5/6 mx-auto flex flex-col items-center h-screen">
-      <ClassForm />
-    </Paper>
-  );
-}
