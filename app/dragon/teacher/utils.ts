e7e4a1c9da70bf43e1d4421b93f27f77c141c438
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-14 09:21:11 +0530

    Extract function to utils

--- app/dragon/teacher/utils.ts
+++ app/dragon/teacher/utils.ts
@@ -8,5 +8,21 @@ export const getTeacherId = cache(async function (userId: string) {
   });
   console.log("getTeacherId function ends", teacherProfile?.id);
   return teacherProfile?.id;
-  
+});
+
+export const getBotConfigs = cache(async (userId: string, classId: string) => {
+  const teacherProfile = await prisma.teacherProfile.findUnique({
+    where: { userId },
+  });
+
+  if (!teacherProfile) {
+    throw new Error(`TeacherProfile with userId ${userId} not found`);
+  }
+  const botConfigs = await prisma.botConfig.findMany({
+    where: {
+      teacherId: teacherProfile.id,
+      classId,
+    },
+  });
+  return botConfigs;
 });
