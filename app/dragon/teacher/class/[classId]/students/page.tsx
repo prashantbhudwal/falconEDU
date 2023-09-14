5b137a217f61ebdeb6b5ac065d46e12bc5c07012
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-14 15:06:29 +0530

    Display students in the class

--- app/dragon/teacher/class/[classId]/students/page.tsx
+++ app/dragon/teacher/class/[classId]/students/page.tsx
@@ -1,50 +1,23 @@
-import { cache } from "react";
-import prisma from "@/prisma";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
-import Link from "next/link";
+import ClassForm from "./add-students-form";
+import { StudentTable } from "./students-table";
+import { getStudentsByClassId } from "../../../queries";
+
 export const revalidate = 3600; // revalidate the data at most every hour
 
-const getClassesByTeacher = cache(async function (teacherId: string) {
-  const classes = await prisma.class.findMany({
-    where: {
-      teacherId: teacherId,
-    },
-    select: {
-      id: true,
-      name: true,
-      // Add more fields as needed
-    },
-  });
-  return classes;
-});
-
-export default async function Classes() {
-  const session = await getServerSession(authOptions);
-  const teacherId = session?.user?.id;
-  if (!teacherId) {
-    return null;
-  }
-
-  const userType = await prisma.user.findUnique({
-    where: {
-      id: teacherId,
-    },
-    select: {
-      userType: true,
-    },
-  });
-
-  if (userType?.userType !== "TEACHER") {
-    return null;
-  }
-  const classes = await getClassesByTeacher(teacherId);
-  console.log(classes);
-
+type EditClassProps = {
+  params: {
+    classId: string;
+  };
+};
+export default async function EditStudents({ params }: EditClassProps) {
+  const { classId } = params;
+  const students = await getStudentsByClassId(classId);
   return (
-    <div>
-      Classes
-      <Link href={`/dragon/class/edit/${teacherId}`}>Test</Link>
+    <div className="flex flex-col gap-10">
+      <div className="mx-auto mt-4">
+        <ClassForm />
+      </div>
+      <StudentTable students={students} />
     </div>
   );
 }
