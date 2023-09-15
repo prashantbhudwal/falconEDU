251d836d4b7bcc064299e487e87c8ce59dae9f50
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-15 18:53:09 +0530

    Abstract chat logic into a component

--- app/dragon/student/bot/[botId]/chat/[id]/page.tsx
+++ app/dragon/student/bot/[botId]/chat/[id]/page.tsx
@@ -1,63 +1,31 @@
-"use client";
-import { bots } from "@/app/dragon/test-data";
-import { ChatPanel } from "@/components/chat/chat-panel";
-import { ChatList } from "@/components/chat/chat-list";
-import { ChatScrollAnchor } from "@/components/chat/chat-scroll-anchor";
-import { cn } from "@/lib/utils";
-import { Message, useChat } from "ai/react";
-import { toast } from "react-hot-toast";
+import { Message } from "ai/react";
+import { getStudentChatApiURL } from "@/lib/urls";
+import { getBotConfigByChatId } from "@/app/dragon/student/queries";
+import { Chat } from "@/components/chat/chat-dragon";
 export interface ChatPageProps {
   params: {
     id: string;
+    botId: string;
   };
 }
 
-export default function ChatPage({ params }: ChatPageProps) {
+export default async function ChatPage({ params }: ChatPageProps) {
   const id = params.id;
+  const botId = params.botId;
+  const botConfig = await getBotConfigByChatId(id);
   const initialMessages: Message[] = [];
 
-  const { messages, append, reload, stop, isLoading, input, setInput } =
-    useChat({
-      api: "/dragon/student/api/chat",
-      initialMessages,
-      id,
-      body: {
-        id,
-      },
-      onResponse(response) {
-        if (response.status === 401) {
-          toast.error(response.statusText);
-        }
-      },
-    });
-  const className = "";
   return (
     <div className="overflow-y-scroll custom-scrollbar">
-      <div className={cn("pb-[200px] pt-4 md:pt-10", className)}>
-        {messages.length ? (
-          <>
-            <ChatList messages={messages} />
-            <ChatScrollAnchor trackVisibility={isLoading} />
-          </>
-        ) : (
-          <div className="mx-auto max-w-2xl px-4">
-            <div className="rounded-sm p-8">
-              <h1 className="mb-2 text-lg">
-                Hello I am Chubbi! Your personal teaching assistant.
-              </h1>
-            </div>
-          </div>
-        )}
-      </div>
-      <ChatPanel
+      <Chat
+        initialMessages={initialMessages}
         id={id}
-        isLoading={isLoading}
-        stop={stop}
-        append={append}
-        reload={reload}
-        messages={messages}
-        input={input}
-        setInput={setInput}
+        apiPath={getStudentChatApiURL()}
+        emptyMessage={"Start chatting with your teacher"}
+        chatBody={{
+          chatId: id,
+          botConfig: botConfig,
+        }}
       />
     </div>
   );
