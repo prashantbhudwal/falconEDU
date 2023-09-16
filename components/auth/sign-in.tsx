c74250dc7cce4358614aad4256c2c2b0dd1d73a8
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-16 15:41:27 +0530

    Refactor signin page

--- components/auth/sign-in.tsx
+++ components/auth/sign-in.tsx
@@ -13,15 +13,48 @@ import {
   CardHeader,
   CardTitle,
 } from "../ui/card";
+import { Button } from "../ui/button";
+import { ButtonProps } from "../ui/button";
+
+const signInConfig = [
+  {
+    type: "student",
+    headline: "AI for Students",
+    description:
+      "Access your teacher's lesson plans, worksheets, activities and assessments with AI that is easy to use and strictly follows your syllabus.",
+    buttonText: "Student SignIn",
+    buttonVariant: "default",
+    auth: "google-student",
+    subtext:
+      "Works on large screens only. Use chrome, edge or any major browser for access.",
+    image: "/chubbi.png",
+    callbackUrl: "/dragon/student",
+  },
+  {
+    type: "teacher",
+    headline: "AI for Teachers",
+    description:
+      "Access your teacher's lesson plans, worksheets, activities and assessments with AI that is easy to use and strictly follows your syllabus.",
+    buttonText: "Teacher SignIn",
+    buttonVariant: "secondary",
+    auth: "google",
+    subtext:
+      "Works on large screens only. Use chrome, edge or any major browser for access.",
+    image: "/chubbi.png",
+    callbackUrl: "/preferences",
+  },
+];
+
 export default function SignIn() {
   const router = useRouter();
   const { data: session, status: sessionStatus } = useSession();
 
   useEffect(() => {
-    console.log(session);
     if (session && sessionStatus === "authenticated") {
-      if (session.user.userType === "TEACHER") router.push("/preferences");
-      else if (session.user.userType === "STUDENT") router.push("/dragon/bots");
+      if (session.user.userType === "TEACHER")
+        router.push(signInConfig[1].callbackUrl);
+      else if (session.user.userType === "STUDENT")
+        router.push(signInConfig[0].callbackUrl);
     }
   }, [session, sessionStatus, router]);
 
@@ -35,46 +68,50 @@ export default function SignIn() {
   };
 
   return (
-    <div className="flex flex-col items-center pt-8 text-center">
-      <Image src={"/chubbi.png"} height={200} width={200} alt="Falcon Logo" />
-      <Beta>
-        <h1 className="my-6 max-w-xl text-2xl leading-10 text-slate-300 md:text-5xl lg:text-5xl">
+    <div className="flex flex-col items-center mt-4 gap-6 px-2">
+      <div className="flex flex-col items-center mt-12 gap-4">
+        <Image src={"/chubbi.png"} height={150} width={150} alt="Falcon Logo" />
+        <h1 className="max-w-xl text-4xl leading-10 text-slate-300">
           Welcome to FalconAI
         </h1>
-      </Beta>
-      <div className={"mb-12 mt-6 max-w-xl text-lg text-gray-500 md:text-xl"}>
-        Create Lesson Plans, Worksheets, Activities and Assessments with AI that
-        is easy to use and strictly follows your syllabus.
-      </div>
-      <button
-        onClick={() => handleSignIn("google", "/preferences")}
-        className={`rounded-lg bg-emerald-500 px-28 py-4 text-lg font-semibold text-slate-800 transition duration-200 ease-in-out hover:bg-emerald-600`}
-      >
-        {sessionStatus === "loading"
-          ? "Signing you in..."
-          : sessionStatus === "authenticated"
-          ? "Taking you to the app..."
-          : "Sign In"}
-      </button>
-      <div className="mt-4 text-xs">
-        Works on large screens only. Use chrome, edge or any major browser for
-        access.
-      </div>
-      <button
-        onClick={() => handleSignIn("google-student", "/preferences")}
-        className={`rounded-lg bg-emerald-500 px-28 py-4 text-lg font-semibold text-slate-800 transition duration-200 ease-in-out hover:bg-emerald-600`}
-      >
-        {sessionStatus === "loading"
-          ? "Signing you in..."
-          : sessionStatus === "authenticated"
-          ? "Taking you to the app..."
-          : "Sign In as Student"}
-      </button>
-      {(sessionStatus === "loading" || sessionStatus === "authenticated") && (
-        <div className="flex h-12 flex-col items-center justify-center gap-2">
-          <PropagateLoader color={"#10B981"} />
+        <div
+          className={
+            "mb-12 mt-6 text-lg text-gray-500 md:text-xl max-w-lg text-center"
+          }
+        >
+          {` Access your teacher's lesson plans, worksheets, activities and
+          assessments with AI that is easy to use and strictly follows your
+          syllabus.`}
         </div>
-      )}
+      </div>
+      <div className="flex px-4 text-center">
+        {signInConfig.map((config) => (
+          <Card
+            className="w-1/2 bg-base-200 flex flex-col gap-4 border-none "
+            key={config.auth}
+          >
+            <CardHeader>
+              <CardTitle className="my-6 max-w-2xl text-3xl leading-10 text-slate-400">
+                {config.headline}
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col items-center">
+              <Button
+                size={"lg"}
+                variant={config.buttonVariant as ButtonProps["variant"]}
+                onClick={() => handleSignIn(config.auth, config.callbackUrl)}
+              >
+                {sessionStatus === "loading"
+                  ? "Signing you in..."
+                  : sessionStatus === "authenticated"
+                  ? "Taking you to the app..."
+                  : config.buttonText}
+              </Button>
+            </CardContent>
+            <CardFooter className="flex flex-col items-center"></CardFooter>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
