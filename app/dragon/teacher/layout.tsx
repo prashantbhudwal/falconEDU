6b9874440772b65b3443e0a2301116d5bb35f89e
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-24 04:11:36 +0530

    remove unused styles and components
    Paper is un-needed, so are flex properties

--- app/dragon/teacher/layout.tsx
+++ app/dragon/teacher/layout.tsx
@@ -1,6 +1,4 @@
 import Navbar from "@/components/navbar/navbar";
-import { Paper } from "@/components/ui/paper";
-import ClassBreadcrumbs from "./components/class-breadcrumbs";
 import { TeacherNav } from "./components/teacher-nav";
 export default function DragonLayout({
   children,
@@ -12,9 +10,9 @@ export default function DragonLayout({
       <Navbar />
       <div className="flex flex-row h-full overflow-hidden">
         <TeacherNav />
-        <Paper className="flex-1 mx-auto max-w-screen-xl flex flex-col space-y-4 h-full overflow-y-auto custom-scrollbar bg-slate-950">
+        <div className="shadow-inner shadow-slate-900 p-8 flex-1 h-full overflow-y-auto custom-scrollbar bg-slate-950">
           {children}
-        </Paper>
+        </div>
       </div>
     </div>
   );
