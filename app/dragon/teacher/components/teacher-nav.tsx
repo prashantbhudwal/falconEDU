465d08b9a714beb5bfb2fdb3bf0b86c78398b331
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-25 00:03:31 +0530

    Add basic layout and elements

--- app/dragon/teacher/components/teacher-nav.tsx
+++ app/dragon/teacher/components/teacher-nav.tsx
@@ -1,7 +1,16 @@
+"use client";
+import clsx from "clsx";
+import Link from "next/link";
+import {
+  useSelectedLayoutSegment,
+  useSelectedLayoutSegments,
+} from "next/navigation";
+import { useTeacherLayoutSegments } from "../hooks/use-layout-segment-ids";
+
 const teacherNavConfig = [
   {
-    name: "Dashboard",
-    path: "/dashboard",
+    name: "Bots",
+    path: "/bots",
     icon: "dashboard",
   },
   {
@@ -10,33 +19,44 @@ const teacherNavConfig = [
     icon: "user",
   },
   {
-    name: "Courses",
-    path: "/courses",
+    name: "Tests",
+    path: "/tests",
     icon: "book",
-  },
-  {
-    name: "Assignments",
-    path: "/assignments",
-    icon: "file",
-  },
-  {
-    name: "Grades",
-    path: "/grades",
-    icon: "file",
-  },
-  {
-    name: "Settings",
-    path: "/settings",
-    icon: "setting",
   },
 ];
 
-export function TeacherNav() {
+export function TeacherNav({ classId }: { classId: string }) {
   return (
-    <nav className="bg-base-200 w-full flex flex-col custom-scrollbar overflow-y-auto"></nav>
+    <nav className="bg-base-200 w-full flex flex-col custom-scrollbar overflow-y-auto h-full">
+      {teacherNavConfig.map((item) => (
+        <TeacherNavItem key={item.path} item={item} close={() => {}} />
+      ))}
+    </nav>
   );
 }
 
-function TeacherNavItem() {
-  return null;
+type TeacherNavItemProps = {
+  item: (typeof teacherNavConfig)[number];
+  close: () => void;
+};
+
+function TeacherNavItem({ item, close }: TeacherNavItemProps) {
+  const { currentSegment, classId, botId, testId } = useTeacherLayoutSegments();
+  console.log({ currentSegment, classId, botId, testId });
+
+  const isActive = false;
+  return (
+    <Link
+      href={item.path}
+      className={clsx(
+        "block rounded-md px-3 py-2 text-sm font-medium hover:text-neutral",
+        {
+          "text-gray-400 hover:bg-gray-800": !isActive,
+          "text-white": isActive,
+        }
+      )}
+    >
+      {item.name}
+    </Link>
+  );
 }
