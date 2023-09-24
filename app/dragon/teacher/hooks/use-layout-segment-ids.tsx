e8c11af02706c5de388c02752b0ed203c50ae231
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-25 00:57:55 +0530

    Fix: Return null when ids not found

--- app/dragon/teacher/hooks/use-layout-segment-ids.tsx
+++ app/dragon/teacher/hooks/use-layout-segment-ids.tsx
@@ -6,18 +6,12 @@ import {
 
 export function useTeacherLayoutSegments() {
   const layoutSegments = useSelectedLayoutSegments();
-  const currentSegment = useSelectedLayoutSegment();
-  console.log(layoutSegments.indexOf("class"));
-  console.log(layoutSegments.indexOf("bot"));
-  console.log(layoutSegments.indexOf("test"));
-
-  const classId =
-    layoutSegments[
-      layoutSegments.findIndex((segment) => (segment = "class")) + 1
-    ];
-  console.log({ classId });
-  const botId = layoutSegments[layoutSegments.indexOf("bot") + 1];
-  const testId = layoutSegments[layoutSegments.indexOf("test") + 1];
-  console.log({ currentSegment, classId, botId, testId });
-  return { currentSegment, classId, botId, testId };
+  const layoutSegment = useSelectedLayoutSegment();
+  const classIndex = layoutSegments.indexOf("class");
+  const classId = classIndex === -1 ? null : layoutSegments[classIndex + 1];
+  const botIndex = layoutSegments.indexOf("bot");
+  const botId = botIndex === -1 ? null : layoutSegments[botIndex + 1];
+  const testIndex = layoutSegments.indexOf("test");
+  const testId = testIndex === -1 ? null : layoutSegments[testIndex + 1];
+  return { layoutSegment, layoutSegments, classId, botId, testId };
 }
