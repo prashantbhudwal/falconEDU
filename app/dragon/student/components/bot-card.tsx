042a7aac482c5d84c8a602efbf42b26a2d2dcd16
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-27 04:19:54 +0530

    style(BotCard): add priority label and resize text

--- app/dragon/student/components/bot-card.tsx
+++ app/dragon/student/components/bot-card.tsx
@@ -5,6 +5,14 @@ import Link from "next/link";
 import { BotsByUserId } from "../queries";
 import type { ArrayElement } from "../queries";
 import { getStudentBotURL } from "@/lib/urls";
+import { cn } from "@/lib/utils";
+const testPriorities = ["HIGH", "MEDIUM", "LOW"] as const;
+
+const priorityColor: Record<(typeof testPriorities)[number], string> = {
+  HIGH: "bg-destructive",
+  MEDIUM: "bg-info",
+  LOW: "",
+};
 
 type BotCardProps = {
   bot: ArrayElement<BotsByUserId>;
@@ -12,19 +20,26 @@ type BotCardProps = {
 export default function BotCard({ bot }: BotCardProps) {
   return (
     <Link href={getStudentBotURL(bot.id)}>
-      <Card className="flex flex-row max-w-sm space-x-5 p-4">
-        <Avatar>
-          <AvatarImage
-            src={bot.BotConfig.teacher.User.image!}
-            alt="Bot Image"
-          />
-          <AvatarFallback>
-            <Avvvatars value={bot.name} style="character" />
-          </AvatarFallback>
-        </Avatar>
-        <div className="flex flex-col space-y-2">
-          <h1 className="text-xl font-bold">{bot.name}</h1>
-          <p className="text-sm text-gray-500">{bot.BotConfig.name}</p>
+      <Card className="relative flex flex-row max-w-sm border-none">
+        <div
+          className={cn("absolute inset-y-0 w-4", priorityColor["HIGH"])}
+        ></div>
+        <div className="flex flex-row space-x-5 px-4 py-5">
+          <div className="pl-2">
+            <Avatar>
+              <AvatarImage
+                src={bot.BotConfig.teacher.User.image!}
+                alt="Bot Image"
+              />
+              <AvatarFallback>
+                <Avvvatars value={bot.name} style="character" />
+              </AvatarFallback>
+            </Avatar>
+          </div>
+          <div className="flex flex-col space-y-1">
+            <h1 className="">{bot.BotConfig.name}</h1>
+            <p className="text-xs text-muted-foreground">by {bot.name}</p>
+          </div>
         </div>
       </Card>
     </Link>
