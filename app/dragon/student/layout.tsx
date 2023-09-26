cefc88707d8042103300e31f58d326f62bb3cec0
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-09-26 22:28:50 +0530

    Not allow teachers to access student side pages

--- app/dragon/student/layout.tsx
+++ app/dragon/student/layout.tsx
@@ -1,10 +1,14 @@
 import Navbar from "@/components/navbar/navbar";
-
-export default function ChatLayout({
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
+import { notFound } from "next/navigation";
+export default async function StudentLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+  if (session?.user.userType !== "STUDENT") notFound();
   return (
     <div className="flex flex-col min-h-screen w-full">
       <Navbar />
