19f4c9bcae226de80252d6aa4d8c140d5a0246aa
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-10-13 00:49:00 +0530

    feat: add tabs for test report feature
    helps move between the test form and the reports

--- app/dragon/teacher/class/[classId]/tests/edit-test/[testBotId]/page.tsx
+++ app/dragon/teacher/class/[classId]/tests/edit-test/[testBotId]/page.tsx
@@ -1,6 +1,7 @@
 import TestPreferencesForm from "./test-preferences-form";
 import { fetchTestBotConfig } from "@/app/dragon/teacher/queries";
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { TestReport } from "./test-report";
 export interface BotPageProps {
   params: {
     classId: string;
@@ -13,11 +14,22 @@ export default async function BotPage({ params }: BotPageProps) {
   const botData = await fetchTestBotConfig(testBotId);
 
   return (
-    <TestPreferencesForm
-      preferences={botData?.preferences}
-      botConfig={botData?.bot!}
-      classId={classId}
-      botId={testBotId}
-    />
+    <Tabs defaultValue="test">
+      <TabsList className="grid w-2/5 grid-cols-2 mx-auto bg-base-100">
+        <TabsTrigger value="test">Test</TabsTrigger>
+        <TabsTrigger value="report">Report</TabsTrigger>
+      </TabsList>
+      <TabsContent value="test">
+        <TestPreferencesForm
+          preferences={botData?.preferences}
+          botConfig={botData?.bot!}
+          classId={classId}
+          botId={testBotId}
+        />
+      </TabsContent>
+      <TabsContent value="report">
+        <TestReport />
+      </TabsContent>
+    </Tabs>
   );
 }
---
+++ app/dragon/teacher/class/[classId]/tests/edit-test/[testBotId]/test-report.tsx
@@ -0,0 +1,11 @@
+export function TestReport() {
+  return (
+    <div>
+      <h1>Test Results</h1>
+      <p>
+        This page is not yet implemented. It will be used to display the results
+        of a test.
+      </p>
+    </div>
+  );
+}
