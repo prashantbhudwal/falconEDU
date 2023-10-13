b72eca6b78d6b1a40225cf9d826bd582c0528e1e
Deepesh76780 <da76780@gmail.com>
2023-10-14 04:53:32 +0530

    add mutations in bot page

--- app/dragon/teacher/class/[classId]/bots/page.tsx
+++ app/dragon/teacher/class/[classId]/bots/page.tsx
@@ -7,6 +7,12 @@ import { ItemCardChip, ItemCard } from "../../../components/item-card";
 import AddBotForm from "./add-bot-form";
 import { getBotConfigs } from "../../../queries";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { FiArchive, FiCornerRightUp, FiTrash } from "react-icons/fi";
+import {
+  archiveAllBotsOfBotConfig,
+  unArchiveAllBotsOfBotConfig,
+  deleteBotConfigAndDeactivateBots,
+} from "./mutations";
 
 type BotDashboardProps = {
   params: {
@@ -32,46 +38,99 @@ export default async function Dashboard({ params }: BotDashboardProps) {
             <TabsTrigger value="archived">Archived</TabsTrigger>
           </TabsList>
           <TabsContent value="active" className="flex space-y-5 flex-col">
-            {botConfigs.map((bot) => (
-              <Link href={getEditBotURL(classId, bot.id)} key={bot.id}>
-                <ItemCard title={bot.name}>
-                  <div className="flex flex-row space-x-4">
-                    <ItemCardChip
-                      label="Created"
-                      value={getFormattedDate(bot.createdAt.toDateString())}
-                    />
-                    <ItemCardChip
-                      label="Status"
-                      value={bot.published ? "Published" : "Not Published"}
-                      valueColor={
-                        bot.published ? "text-primary" : "text-destructive"
-                      }
-                    />
-                  </div>
-                </ItemCard>
-              </Link>
-            ))}
+            {botConfigs
+              .filter((botConfig) => botConfig.isActive)
+              .map((bot) => (
+                <Link href={getEditBotURL(classId, bot.id)} key={bot.id}>
+                  <ItemCard
+                    title={bot.name}
+                    actions={[
+                      {
+                        title: "Archive Bot",
+                        description:
+                          "are you sure you want to archive this bot?",
+                        name: "Archive Bot: Instantly disables the bot for all students.",
+                        icon: <FiArchive />,
+                        action: archiveAllBotsOfBotConfig,
+                        actionParams: [bot.id],
+                      },
+                    ]}
+                  >
+                    <div className="flex flex-row space-x-4">
+                      <ItemCardChip
+                        label="Created"
+                        value={getFormattedDate(bot.createdAt.toDateString())}
+                      />
+                      <ItemCardChip
+                        label="Status"
+                        value={bot.published ? "Published" : "Not Published"}
+                        valueColor={
+                          bot.published ? "text-primary" : "text-destructive"
+                        }
+                      />
+                      <ItemCardChip
+                        label="Active"
+                        value={bot.isActive ? "Yes" : "No"}
+                        valueColor={
+                          bot.isActive ? "text-primary" : "text-destructive"
+                        }
+                      />
+                    </div>
+                  </ItemCard>
+                </Link>
+              ))}
           </TabsContent>
           <TabsContent value="archived" className="flex space-y-5 flex-col">
-            {botConfigs.map((bot) => (
-              <Link href={getEditBotURL(classId, bot.id)} key={bot.id}>
-                <ItemCard title={bot.name}>
-                  <div className="flex flex-row space-x-4">
-                    <ItemCardChip
-                      label="Created"
-                      value={getFormattedDate(bot.createdAt.toDateString())}
-                    />
-                    <ItemCardChip
-                      label="Status"
-                      value={bot.published ? "Published" : "Not Published"}
-                      valueColor={
-                        bot.published ? "text-primary" : "text-destructive"
-                      }
-                    />
-                  </div>
-                </ItemCard>
-              </Link>
-            ))}
+            {botConfigs
+              .filter((botConfig) => !botConfig.isActive)
+              .map((bot) => (
+                <Link href={getEditBotURL(classId, bot.id)} key={bot.id}>
+                  <ItemCard
+                    title={bot.name}
+                    actions={[
+                      {
+                        title: "Unarchive Bot",
+                        description:
+                          "are you sure you want to unarchive this bot?",
+                        name: "Activate Bot: Instantly activates the bot for all students.",
+                        icon: <FiCornerRightUp />,
+                        action: unArchiveAllBotsOfBotConfig,
+                        actionParams: [bot.id],
+                      },
+                      {
+                        title: "Delete Bot",
+                        description:
+                          "are you sure you want to delete this bot?",
+                        name: "DELETE: This will delete the bot permanently. The students will still be able to see the bot if they have already attempted it.",
+                        icon: <FiTrash />,
+                        action: deleteBotConfigAndDeactivateBots,
+                        actionParams: [bot.id],
+                      },
+                    ]}
+                  >
+                    <div className="flex flex-row space-x-4">
+                      <ItemCardChip
+                        label="Created"
+                        value={getFormattedDate(bot.createdAt.toDateString())}
+                      />
+                      <ItemCardChip
+                        label="Status"
+                        value={bot.published ? "Published" : "Not Published"}
+                        valueColor={
+                          bot.published ? "text-primary" : "text-destructive"
+                        }
+                      />
+                      <ItemCardChip
+                        label="Active"
+                        value={bot.isActive ? "Yes" : "No"}
+                        valueColor={
+                          bot.isActive ? "text-primary" : "text-destructive"
+                        }
+                      />
+                    </div>
+                  </ItemCard>
+                </Link>
+              ))}
           </TabsContent>
         </Tabs>
       </div>
