9a888cab34f4043f2a940dc4a69d20fd5b08b735
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-10-24 06:24:37 +0530

    style(chat-list): remove separator between messages

--- components/chat/chat-list.tsx
+++ components/chat/chat-list.tsx
@@ -1,6 +1,5 @@
 import { type Message } from "ai";
 
-import { Separator } from "@ui/separator";
 import { ChatMessage } from "./chat-message";
 
 export interface ChatList {
@@ -15,7 +14,7 @@ export function ChatList({ messages, botImage, studentImage }: ChatList) {
   }
 
   return (
-    <div className="relative mx-auto max-w-2xl px-4">
+    <div className="relative mx-auto max-w-2xl px-4 flex flex-col space-y-4 ">
       {messages.map((message, index) => (
         <div key={index}>
           <ChatMessage
@@ -23,9 +22,6 @@ export function ChatList({ messages, botImage, studentImage }: ChatList) {
             botImage={botImage}
             studentImage={studentImage}
           />
-          {index < messages.length - 1 && (
-            <Separator className="my-4 md:my-8" />
-          )}
         </div>
       ))}
     </div>
