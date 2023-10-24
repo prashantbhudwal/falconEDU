16a5a6ae07c8449a83e785677f22649b1d63a4a1
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-10-24 07:01:49 +0530

    style: hide submitted when no bots submitted

--- app/dragon/student/teacher/[teacherId]/page.tsx
+++ app/dragon/student/teacher/[teacherId]/page.tsx
@@ -1,14 +1,12 @@
 import { getChats } from "@/app/(falcon)/chubbi/actions";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
-import { bots } from "@/app/dragon/test-data";
 import { ItemCard } from "../../components/item-card";
 import Link from "next/link";
 import { getStudentBotURL } from "@/lib/urls";
 import {
   getBotsByTeacherAndStudentID,
   getTeacherDetailsByTeacherId,
-  getTeachersByUserId,
 } from "../../queries";
 import { AvatarNavbar } from "../../components/student-navbar";
 import { Separator } from "@/components/ui/separator";
@@ -44,6 +42,8 @@ export default async function TeacherDashboard({
       </>
     );
   }
+  const unSubmittedBots = bots.filter((bot) => !bot.isSubmitted);
+  const submittedBots = bots.filter((bot) => bot.isSubmitted);
   return (
     <div>
       <AvatarNavbar
@@ -52,29 +52,28 @@ export default async function TeacherDashboard({
         avatarUrl={teacher?.User.image!}
       />
       <div className="pt-1 pb-20 w-full overflow-y-auto h-screen custom-scrollbar">
-        {bots
-          .filter((bot) => !bot.isSubmitted)
-          .map((bot) => (
-            <Link href={getStudentBotURL(bot.id)} key={bot.id}>
-              <ItemCard
-                title={bot.BotConfig.name!}
-                description={getBotDescription(bot.BotConfig.type!)}
-              />
-            </Link>
-          ))}
-        <Separator className="my-2" />
-        <h1 className="px-4">Submitted</h1>
-        <Separator className="my-2" />
-        {bots
-          .filter((bot) => bot.isSubmitted)
-          .map((bot) => (
-            <Link href={getStudentBotURL(bot.id)} key={bot.id}>
-              <ItemCard
-                title={bot.BotConfig.name!}
-                description={getBotDescription(bot.BotConfig.type!)}
-              />
-            </Link>
-          ))}
+        {unSubmittedBots.map((bot) => (
+          <Link href={getStudentBotURL(bot.id)} key={bot.id}>
+            <ItemCard
+              title={bot.BotConfig.name!}
+              description={getBotDescription(bot.BotConfig.type!)}
+            />
+          </Link>
+        ))}
+        {submittedBots.length > 0 && (
+          <>
+            <h1 className="px-4 my-2 font-semibold">Submitted</h1>
+            <Separator className="my-2" />
+            {submittedBots.map((bot) => (
+              <Link href={getStudentBotURL(bot.id)} key={bot.id}>
+                <ItemCard
+                  title={bot.BotConfig.name!}
+                  description={getBotDescription(bot.BotConfig.type!)}
+                />
+              </Link>
+            ))}
+          </>
+        )}
       </div>
     </div>
   );
