4de642964908ed1b1c890f071ca593d729ffd41b
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-10-24 06:35:18 +0530

    style: add padding to prevent chat overlap

--- components/chat/chat-list.tsx
+++ components/chat/chat-list.tsx
@@ -14,7 +14,7 @@ export function ChatList({ messages, botImage, studentImage }: ChatList) {
   }
 
   return (
-    <div className="relative mx-auto max-w-2xl px-4 flex flex-col space-y-4 ">
+    <div className="relative mx-auto max-w-2xl px-4 flex flex-col space-y-4 pb-10">
       {messages.map((message, index) => (
         <div key={index}>
           <ChatMessage
