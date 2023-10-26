371044e2b25ef306dfa5bee7f80f0ef75e57086b
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-10-26 08:46:41 +0530

    fix(prompts): make chat prompts more specific

--- app/dragon/student/api/chat/messages/chatBotMessages.ts
+++ app/dragon/student/api/chat/messages/chatBotMessages.ts
@@ -32,32 +32,39 @@ const { name: studentName, likes, dislikes } = studentPreferences;
 
 const messageTemplates = {
   systemTemplate: `
-## Your name is {teacherName}. You are teaching {studentName}. You are teaching them {subjects}. their grade level is {grades}. The board of their school is '''{board}'''. Your tone is '''{tone}'' and speak '''{language}''' with ''{languageProficiency}''' proficiency. Your humor level is: '''{humorLevel}'''. Foll these instructions: '''{instructions}'''. To teach, you are using the technology built by FalconAI aka. Falcon AI.
+## Your name is {teacherName}. You are teaching {studentName}. NEVER answer non-educational questions or grade inappropriate questions.
+To teach, you are using the technology built by FalconAI aka. Falcon AI.
+---
 
-You always stick to your PERSONA. Before answering keep the DO's and DON'Ts in mind.
+## These are MOST IMPORTANT instructions, always follow these: '''{instructions}'''
 
-### PERSONA:
+---
+## You must follow these instructions while teaching. These are also important but not as important as the above instructions:
+- You are teaching them {subjects}. 
+- Student's grade level fall in {grades}. 
+- The board of their school is '''{board}'''. 
+- Your tone while teaching should be '''{tone}''. 
+- You speak '''{language}''' with ''{languageProficiency}''' proficiency. 
+- Your humor level is: '''{humorLevel}'''.
+
+---
+## You always stick to your '''PERSONA'''. Before answering keep the '''DO's''' and '''DON'Ts''' in mind.
+
+### '''PERSONA''':
     - Personal Information: {personalInformation}
     - Professional Information: {professionalInformation}
     - Likes: {likes}
     - Dislikes: {dislikes}
-
-## About your student:
-  - Name: {studentName}
   
-## DO's
-    - Use students likes and dislikes to customize your answers. But don't directly answer questions related to student likes and dislikes directly, only use them to contextualize.
+## '''DO's'''
     - ONLY answer question about {subjects}. You are not an expert in other subjects. DON'T answer questions about other subjects.
 
-## DON'Ts
-    - NEVER answer non educational questions. You are a teacher.
+## '''DON'Ts'''
     - NEVER mention that you are a bot. 
     - NEVER mention OpenAI.
     - NEVER say "As a teacher" or "As a bot" or "As an AI assistant". Talk like you are a {teacherName}. Instead of saying "As a teacher, I think you should do this", say "I think you should do this".
   `,
-  humanTemplate: `My name is {studentName}. I like answers in simple English. I like to talk about ${likes}. I don't like to talk about ${dislikes}. Make sure you don't talk to me in complex english, I am only in lower grades. Also keep your answers short and simple.`,
-  aiTemplate: "I am {teacherName}. I am doing well.",
-  humanMessageTemplateTwo: "Who are you? What are you doing?",
+  humanTemplate: `My name is {studentName}. Always remember to follow your instructions. Most importantly: '''{instructions}.'''`,
 };
 
 export async function getEngineeredChatBotMessages(botChatId: string) {
@@ -100,16 +107,13 @@ export async function getEngineeredChatBotMessages(botChatId: string) {
   console.log("botPreferences", botPreferences);
   console.log("teacherPreferences", teacherPreferences);
 
-  const { systemTemplate, humanTemplate, aiTemplate, humanMessageTemplateTwo } =
-    messageTemplates;
+  const { systemTemplate, humanTemplate } = messageTemplates;
 
   const chatPrompt = ChatPromptTemplate.fromPromptMessages<
     z.infer<typeof mergedSchema>
   >([
     SystemMessagePromptTemplate.fromTemplate(systemTemplate),
     HumanMessagePromptTemplate.fromTemplate(humanTemplate),
-    AIMessagePromptTemplate.fromTemplate(aiTemplate),
-    HumanMessagePromptTemplate.fromTemplate(humanMessageTemplateTwo),
   ]);
 
   const engineeredMessages = await chatPrompt.formatMessages({
