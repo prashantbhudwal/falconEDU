66ba62a4325f0942f9c5aaf18f7b89dea2fcfc34
ankit <ankitbisht0205@gmail.com>
2023-10-30 05:37:44 +0530

    fix : added info message if no students are in test in teacher page

--- app/dragon/teacher/class/[classId]/tests/edit-test/[testBotId]/test-report.tsx
+++ app/dragon/teacher/class/[classId]/tests/edit-test/[testBotId]/test-report.tsx
@@ -16,40 +16,46 @@ export async function TestReport({
   const students = await getStudentsByBotConfigId(testBotId);
   return (
     <Paper variant={"gray"} className="w-full max-w-5xl min-h-screen">
-      {students.map((student) => (
-        <Link
-          href={
-            student.isSubmitted
-              ? `/dragon/teacher/class/${classId}/tests/edit-test/${testBotId}/report/${student.studentBotId}`
-              : ``
-          }
-          key={student.email}
-        >
-          <ItemCard
-            title={student.name!}
-            avatarUrl={student.image!}
-            className="mx-auto"
+      {students ? (
+        students.map((student) => (
+          <Link
+            href={
+              student.isSubmitted
+                ? `/dragon/teacher/class/${classId}/tests/edit-test/${testBotId}/report/${student.studentBotId}`
+                : ``
+            }
+            key={student.email}
           >
-            <div className="flex gap-2">
-              <ItemCardChip label="Email" value={student.email} />
-              <ItemCardChip
-                label="Status"
-                value={student.isSubmitted ? "Submitted" : "Not Submitted"}
-                valueColor={
-                  student.isSubmitted ? "text-primary" : "text-secondary"
-                }
-              />
-              <ItemCardChip
-                label="Active"
-                value={student.isActive ? "Yes" : "No"}
-                valueColor={
-                  student.isActive ? "text-primary" : "text-secondary"
-                }
-              />
-            </div>
-          </ItemCard>
-        </Link>
-      ))}
+            <ItemCard
+              title={student.name!}
+              avatarUrl={student.image!}
+              className="mx-auto"
+            >
+              <div className="flex gap-2">
+                <ItemCardChip label="Email" value={student.email} />
+                <ItemCardChip
+                  label="Status"
+                  value={student.isSubmitted ? "Submitted" : "Not Submitted"}
+                  valueColor={
+                    student.isSubmitted ? "text-primary" : "text-secondary"
+                  }
+                />
+                <ItemCardChip
+                  label="Active"
+                  value={student.isActive ? "Yes" : "No"}
+                  valueColor={
+                    student.isActive ? "text-primary" : "text-secondary"
+                  }
+                />
+              </div>
+            </ItemCard>
+          </Link>
+        ))
+      ) : (
+        <h1 className="text-center font-semibold text-2xl mt-10">
+          You have no Students in this Test
+        </h1>
+      )}
     </Paper>
   );
 }
