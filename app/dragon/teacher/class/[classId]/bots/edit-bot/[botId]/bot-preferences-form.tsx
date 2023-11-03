fdc9781137b6980ee43b34324794ad66660aaaed
ankit <ankitbisht0205@gmail.com>
2023-11-03 07:32:47 +0530

    style(teacher-bots/test): fix layout of bot/test name

--- app/dragon/teacher/class/[classId]/bots/edit-bot/[botId]/bot-preferences-form.tsx
+++ app/dragon/teacher/class/[classId]/bots/edit-bot/[botId]/bot-preferences-form.tsx
@@ -172,7 +172,7 @@ export default function BotPreferencesForm({
                   value={botName}
                   onChange={onBotNameChange}
                   onBlur={updateBotNameHandler}
-                  className="outline-none border-none md:text-3xl font-bold tracking-wide focus-visible:ring-0 "
+                  className="outline-none border-none md:text-3xl pl-0 font-bold tracking-wide focus-visible:ring-0 "
                 />
                 {error && (
                   <div className="text-red-500 text-sm mt-3">{error}</div>
