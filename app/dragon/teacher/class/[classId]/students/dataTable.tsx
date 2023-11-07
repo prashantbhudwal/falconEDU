254719a0df4657f5b1b3d3e4f519c3a6ac9ae0fe
ankit <ankitbisht0205@gmail.com>
2023-11-08 03:23:01 +0530

    feat(teacher/students): add data table for students
    removing the card layout for showing total students and replacing it with data-table layout

---
+++ app/dragon/teacher/class/[classId]/students/dataTable.tsx
@@ -0,0 +1,287 @@
+"use client";
+
+import * as React from "react";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { ArrowUpDown, ChevronDown } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { StudentsByClassId } from "../../../../teacher/queries";
+import Image from "next/image";
+import { FiTrash } from "react-icons/fi";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { ClassDialog } from "../../../components/class-dialog";
+import { removeStudentFromClass } from "./mutations";
+
+type StudentData = NonNullable<StudentsByClassId>[0];
+
+export const columns: ColumnDef<StudentData>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    id: "userImage",
+    accessorKey: "User.image",
+    header: "",
+    cell: ({ row }) => (
+      <Image
+        src={row.original?.User.image!}
+        alt="User"
+        width={30}
+        height={30}
+        className="rounded-full"
+      />
+    ),
+  },
+  {
+    id: "email",
+    accessorKey: "User.email",
+    enableSorting: true,
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="lowercase">{row.original?.User.email}</div>
+    ),
+  },
+  {
+    id: "name",
+    accessorKey: "User.name",
+    header: () => <div className="text-right">Name</div>,
+    cell: ({ row }) => {
+      return (
+        <div className="text-right font-medium">{row.original?.User.name}</div>
+      );
+    },
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const id = row.original?.id;
+      const classId = row.original?.classId;
+      return (
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <ClassDialog
+                title="Delete Student"
+                description="are you sure you want to delete this student?"
+                action={() => removeStudentFromClass(id, classId!)}
+                trigger={
+                  <Button
+                    variant={"ghost"}
+                    size={"icon"}
+                    className="hover:bg-slate-600"
+                  >
+                    <FiTrash />
+                  </Button>
+                }
+              />
+            </TooltipTrigger>
+            <TooltipContent className="bg-slate-600 text-black">
+              Delete Student
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      );
+    },
+  },
+];
+
+export function DataTable({ students }: { students: StudentData[] }) {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
+
+  const table = useReactTable({
+    data: students,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
+  return (
+    <div className="w-full">
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter emails..."
+          value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("email")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Columns
+              <ChevronDown className="ml-2 h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id} className="hover:bg-transparent">
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow key={row.id} className="hover:bg-transparent">
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="flex-1 text-sm text-muted-foreground">
+          {table.getFilteredSelectedRowModel().rows.length} of{" "}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/dragon/teacher/class/[classId]/students/page.tsx
+++ app/dragon/teacher/class/[classId]/students/page.tsx
@@ -1,8 +1,6 @@
 import AddStudentForm from "./add-students-form";
 import { getStudentsByClassId } from "../../../queries";
-import { ItemCard } from "../../../components/item-card";
-import { removeStudentFromClass } from "./mutations";
-import { FiTrash } from "react-icons/fi";
+import { DataTable } from "./dataTable";
 
 export const revalidate = 3600; // revalidate the data at most every hour
 
@@ -18,29 +16,7 @@ export default async function EditStudents({ params }: EditClassProps) {
     <div className="flex flex-col gap-10 w-full min-h-[calc(100vh-100px)] max-h-full overflow-y-scroll custom-scrollbar p-10">
       <div className="flex space-y-5 flex-col w-full">
         <AddStudentForm classId={classId} />
-        {students?.map((student) => {
-          return (
-            <ItemCard
-              title={student.User.name!}
-              key={student.id}
-              avatarUrl={student.User.image!}
-              actions={[
-                {
-                  title: "Delete Student",
-                  description: "are you sure you want to delete this student?",
-                  name: "Delete Student",
-                  icon: <FiTrash />,
-                  action: removeStudentFromClass,
-                  actionParams: [student.id, student.classId!],
-                },
-              ]}
-            >
-              <span className="text-sm text-base-content">
-                {student.User.email}
-              </span>
-            </ItemCard>
-          );
-        })}
+        <DataTable students={students || []} />
       </div>
     </div>
   );
