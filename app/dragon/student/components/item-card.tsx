5a4b95a3479a163872603538e99206d304ccc04a
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-11-08 13:30:50 +0530

    style(item-card): extend to show icon, read status

--- app/dragon/student/components/item-card.tsx
+++ app/dragon/student/components/item-card.tsx
@@ -1,9 +1,8 @@
 import { Card } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Avvvatars from "avvvatars-react";
-import { BotsByUserId } from "../queries";
-import type { ArrayElement } from "../queries";
 import { cn } from "@/lib/utils";
+import React from "react";
 const testPriorities = ["HIGH", "MEDIUM", "LOW"] as const;
 
 const priorityColor: Record<(typeof testPriorities)[number], string> = {
@@ -17,24 +16,37 @@ type ItemCardProps = {
   priority?: (typeof testPriorities)[number];
   title: string;
   description?: string;
+  icon?: React.ReactNode;
+  isRead?: boolean;
 };
 export function ItemCard({
   imageUrl,
   priority = "LOW",
   title,
   description,
+  icon,
+  isRead = false,
 }: ItemCardProps) {
   return (
     <Card className="relative flex flex-row max-w-sm border-none">
       <div
         className={cn("absolute inset-y-0 w-4", priorityColor[priority])}
       ></div>
+      <div className={cn("absolute top-5 right-5")}>
+        {isRead && (
+          <div className="flex gap-1 items-center">
+            <div className="h-3 w-3 rounded-full bg-primary"></div>
+          </div>
+        )}
+      </div>
       <div className="flex flex-row space-x-5 px-4 py-5">
         <div className="pl-2">
           <Avatar>
-            <AvatarImage src={imageUrl} alt="Bot Image" />
-            <AvatarFallback className="bg-base-100">
-              <Avvvatars value={title} style="shape" />
+            <AvatarImage src="some-image-url" alt="User Avatar" />
+            <AvatarFallback className="bg-base-300">
+              {(icon && <div className="w-7">{icon}</div>) || (
+                <Avvvatars value={title} style="shape" />
+              )}
             </AvatarFallback>
           </Avatar>
         </div>
