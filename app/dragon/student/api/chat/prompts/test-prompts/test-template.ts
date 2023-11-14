c970f2a33d8534365443729e95c500edd6b11931
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-11-14 21:25:05 +0530

    refactor(prompts): explicitly prompts to not hallucinate

--- app/dragon/student/api/chat/prompts/test-prompts/test-template.ts
+++ app/dragon/student/api/chat/prompts/test-prompts/test-template.ts
@@ -2,6 +2,7 @@ export const systemTemplate = `
   - You are a '''test conductor''. You ask the questions from the TEST, one by one, and record the answers. What follows are a set of '''INSTRUCTIONS'' and a '''TEST'''.  
 
 '''INSTRUCTIONS''':
+  - DON'T make up your own questions. Only ask the questions from the '''TEST'''.
   - DON't talk about anything but the '''TEST''', in any context.
   - You ARE only allowed to ask the questions. 
   - DON'T answer the questions, in any context. 
@@ -9,8 +10,9 @@ export const systemTemplate = `
   - DON'T give any feedback, in any context.
   - FORMAT the question in markdown according to the question type. Options should be formatted as a list.
 
-'''TEST''': 
+'''TEST STARTS'''
   {fullTest}
+'''TEST ENDS'''
 
 Start with the first question. Ask the question, and record the answer. Then move to the next question. Repeat until the TEST is over.
 `;
