f73055d5e620f377168a528fa61be2c434a8aaab
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-11-14 06:24:52 +0530

    style(report-modal): add basic styles

--- app/dragon/teacher/class/[classId]/tests/edit-test/[testBotId]/@modal/(.)report/[studentBotId]/modal.tsx
+++ app/dragon/teacher/class/[classId]/tests/edit-test/[testBotId]/@modal/(.)report/[studentBotId]/modal.tsx
@@ -35,13 +35,10 @@ export default function Modal({ children }: { children: React.ReactNode }) {
   return (
     <div
       ref={overlay}
-      className="fixed z-10 left-0 right-0 top-0 bottom-0 mx-auto bg-black/60 w-full"
+      className="fixed z-10 left-0 right-0 top-0  bg-black/60 w-full flex items-start justify-center h-screen"
       onClick={onClick}
     >
-      <div
-        ref={wrapper}
-        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full sm:w-10/12 md:w-8/12 lg:w-1/2 p-6"
-      >
+      <div ref={wrapper} className="w-4/5 relative top-2">
         {children}
       </div>
     </div>
--- app/dragon/teacher/class/[classId]/tests/edit-test/[testBotId]/@modal/(.)report/[studentBotId]/page.tsx
+++ app/dragon/teacher/class/[classId]/tests/edit-test/[testBotId]/@modal/(.)report/[studentBotId]/page.tsx
@@ -1,5 +1,6 @@
 import Report from "../../../report/[studentBotId]/report";
 import Modal from "./modal";
+import { _TestOverflow } from "@/components/_test-overflow";
 
 type ReportProps = {
   params: {
@@ -11,7 +12,7 @@ type ReportProps = {
 export default async function ReportModal({ params }: ReportProps) {
   return (
     <Modal>
-      <div className="w-full overflow-y-scroll custom-scrollbar pt-10 bg-base-300 py-20 h-full">
+      <div className="w-full py-10 overflow-y-scroll custom-scrollbar h-screen bg-base-300 rounded-xl ring-1 ring ring-slate-700 ">
         <Report params={params} />
       </div>
     </Modal>
