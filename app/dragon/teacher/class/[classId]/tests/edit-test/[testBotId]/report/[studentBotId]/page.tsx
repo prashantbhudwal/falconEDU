c823d427530f79ab2cc68aba01c97a25d4cd5a1f
ankit <ankitbisht0205@gmail.com>
2023-11-18 11:12:59 +0530

    fix(teacher/tests): fix scroll overflow of intercepting report page

--- app/dragon/teacher/class/[classId]/tests/edit-test/[testBotId]/report/[studentBotId]/page.tsx
+++ app/dragon/teacher/class/[classId]/tests/edit-test/[testBotId]/report/[studentBotId]/page.tsx
@@ -1,31 +1,3 @@
-import { ChatList } from "@/components/chat/chat-list";
-import {
-  getDefaultChatMessagesByStudentBotId,
-  getSingleStudentByStudentBotId,
-  getUserImageByStudentBotId,
-} from "../../../../queries";
-import { Paper } from "@/components/ui/paper";
-import PieChartComponent from "./pieChart";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { Progress } from "@/components/progress";
-import { InformationCircleIcon } from "@heroicons/react/24/solid";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-import { getTestResultsByBotId } from "@/app/dragon/teacher/queries";
-import { type TestResultsByBotId } from "@/app/dragon/teacher/queries";
 import Report from "./report";
 
 type ReportProps = {
@@ -38,7 +10,7 @@ type ReportProps = {
 
 export default async function ReportPage({ params }: ReportProps) {
   return (
-    <div className="w-full overflow-y-scroll custom-scrollbar pt-10">
+    <div className="w-full h-full overflow-y-scroll custom-scrollbar pt-10">
       <Report params={params} />
     </div>
   );
