44d7f6c889c3d0524ebf493da69ab6057c13a07a
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-11-26 03:55:53 +0530

    refactor(teacher): use query from router

--- app/dragon/teacher/layout.tsx
+++ app/dragon/teacher/layout.tsx
@@ -1,23 +1,8 @@
-import Navbar from "@/components/navbar/navbar";
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import { getServerSession } from "next-auth";
 import { notFound, redirect } from "next/navigation";
-import prisma from "@/prisma";
 import ConditionalNav from "./components/conditional-navbar";
-
-async function getTeacherOrgMode(userId: string) {
-  const teacherProfile = await prisma.teacherProfile.findUnique({
-    where: {
-      userId: userId,
-    },
-    select: {
-      orgMode: true,
-    },
-  });
-  if (!teacherProfile) return notFound();
-
-  return teacherProfile.orgMode;
-}
+import { db } from "./routers";
 
 export default async function TeacherLayout({
   children,
@@ -27,7 +12,8 @@ export default async function TeacherLayout({
   const session = await getServerSession(authOptions);
   if (!session) return notFound();
   if (session?.user.userType !== "TEACHER") return notFound();
-  const orgMode = await getTeacherOrgMode(session.user.id);
+  const userId = session.user.id;
+  const orgMode = await db.teacher.teacherHasOrgMode({ userId });
   if (!orgMode) return redirect("/dragon/auth/request-access");
 
   return (
