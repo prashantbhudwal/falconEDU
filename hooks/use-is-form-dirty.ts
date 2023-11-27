a8f3e36ac8891b1f6349174abc0d82c4a7f10d23
ankit <ankitbisht0205@gmail.com>
2023-11-27 20:16:09 +0530

    fix(teacher/test): fix upload question failed after one try

--- hooks/use-is-form-dirty.ts
+++ hooks/use-is-form-dirty.ts
@@ -7,11 +7,17 @@ export function useIsFormDirty<T extends FieldValues>(
 ) {
   const [isDirty, setIsDirty] = useState(initialValue);
 
+  // using form.watch inside useEffect cause of it's weird behaviour coupled with form.formState it wont change the state after just one change you have to do at least 2 changes inside form to see a change in state
   useEffect(() => {
     if (Object.keys(form.formState.dirtyFields).length > 0) {
       setIsDirty(true);
     }
-  }, [form.formState.dirtyFields]);
+    form.watch(() => {
+      if (Object.keys(form.formState.dirtyFields).length > 0) {
+        setIsDirty(true);
+      }
+    });
+  }, [form.formState.dirtyFields, form]);
 
   return { isDirty, setIsDirty };
 }
