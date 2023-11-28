64d98d1dc06f69ae7bc3e5d5fa7972b5422af428
ankit <ankitbisht0205@gmail.com>
2023-11-29 04:00:30 +0530

    fix(nextconfig): disable pwa for development

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 const withPWA = require("next-pwa")({
   dest: "public",
+  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
