2a1ebb4150168afc1040a55bc3d942e06868772f
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-12-11 04:59:05 +0530

    feat(tasks-nav): add link to back button

--- app/dragon/teacher/class/[classId]/(tasks)/[taskId]/tasks-navbar.tsx
+++ app/dragon/teacher/class/[classId]/(tasks)/[taskId]/tasks-navbar.tsx
@@ -5,6 +5,7 @@ import { getTaskIcon } from "../../utils";
 import { BotConfig } from "@prisma/client";
 import { ChevronLeftIcon } from "@heroicons/react/24/solid";
 import { cn } from "@/lib/utils";
+import { getClassURL } from "@/lib/urls";
 
 export async function TasksNavbar({
   classId,
@@ -23,14 +24,16 @@ export async function TasksNavbar({
   return (
     <div className="navbar flex w-full bg-base-300 border-b border-base-200">
       <div className="navbar-start gap-4 pr-2 flex">
-        <Button
-          className="flex items-center gap-1 hover:bg-base-300 hover:text-slate-100 hover:font-semibold"
-          variant="ghost"
-          size="sm"
-        >
-          <ChevronLeftIcon className="w-6" />
-          <div>{nameOfClass}</div>
-        </Button>
+        <Link href={getClassURL(classId)}>
+          <Button
+            className="flex items-center gap-1 hover:bg-base-300 hover:text-slate-100 hover:font-semibold"
+            variant="ghost"
+            size="sm"
+          >
+            <ChevronLeftIcon className="w-6" />
+            <div>{nameOfClass}</div>
+          </Button>
+        </Link>
       </div>
       <div className="navbar-center">
         <div className={cn("flex items-center gap-1", iconColor)}>
