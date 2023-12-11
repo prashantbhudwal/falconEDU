1d4df46e1cb98c6e1e16640697c4377ce07d602e
ankit <ankitbisht0205@gmail.com>
2023-12-11 07:23:10 +0530

    fix(teacher-preferences): add messages to the zod schema

--- app/dragon/schema.ts
+++ app/dragon/schema.ts
@@ -96,23 +96,43 @@ export const LIMITS_teacherPreferencesSchema = {
 export const teacherPreferencesSchema = z.object({
   professionalInformation: z
     .string()
-    .min(LIMITS_teacherPreferencesSchema.professionalInformation.minLength)
-    .max(LIMITS_teacherPreferencesSchema.professionalInformation.maxLength)
+    .min(LIMITS_teacherPreferencesSchema.professionalInformation.minLength, {
+      message:
+        "Professional information must adhere to a character limit of 50-500.",
+    })
+    .max(LIMITS_teacherPreferencesSchema.professionalInformation.maxLength, {
+      message:
+        "Professional information must adhere to a character limit of 50-500.",
+    })
     .optional(),
   personalInformation: z
     .string()
-    .min(LIMITS_teacherPreferencesSchema.personalInformation.minLength)
-    .max(LIMITS_teacherPreferencesSchema.personalInformation.maxLength)
+    .min(LIMITS_teacherPreferencesSchema.personalInformation.minLength, {
+      message:
+        "Personal information must adhere to a character limit of 50-500.",
+    })
+    .max(LIMITS_teacherPreferencesSchema.personalInformation.maxLength, {
+      message:
+        "Personal information must adhere to a character limit of 50-500.",
+    })
     .optional(),
   likes: z
     .string()
-    .min(LIMITS_teacherPreferencesSchema.likes.minLength)
-    .max(LIMITS_teacherPreferencesSchema.likes.maxLength)
+    .min(LIMITS_teacherPreferencesSchema.likes.minLength, {
+      message: "Likes must adhere to a character limit of 50-500.",
+    })
+    .max(LIMITS_teacherPreferencesSchema.likes.maxLength, {
+      message: "Likes must adhere to a character limit of 50-500.",
+    })
     .optional(),
   dislikes: z
     .string()
-    .min(LIMITS_teacherPreferencesSchema.dislikes.minLength)
-    .max(LIMITS_teacherPreferencesSchema.dislikes.maxLength)
+    .min(LIMITS_teacherPreferencesSchema.dislikes.minLength, {
+      message: "Dislikes must adhere to a character limit of 50-500.",
+    })
+    .max(LIMITS_teacherPreferencesSchema.dislikes.maxLength, {
+      message: "Dislikes must adhere to a character limit of 50-500.",
+    })
     .optional(),
 });
 
