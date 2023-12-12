904bb57cde2359c988523a00389862b509039abb
ankit <ankitbisht0205@gmail.com>
2023-12-12 08:24:29 +0530

    fix(student): add a button to scroll down in chat

--- components/chat/chat-dragon.tsx
+++ components/chat/chat-dragon.tsx
@@ -4,7 +4,10 @@ import { cn } from "../../lib/utils";
 import { ChatList } from "./chat-list";
 import { ChatPanel } from "./chat-panel";
 import { toast } from "react-hot-toast";
-import { useRef, useLayoutEffect } from "react";
+import { useRef, useLayoutEffect, useState, useEffect } from "react";
+import { MdOutlineKeyboardDoubleArrowDown } from "react-icons/md";
+import { Button } from "../ui/button";
+import { useInView } from "react-intersection-observer";
 
 export interface ChatProps extends React.ComponentProps<"div"> {
   initialMessages?: Message[];
@@ -43,61 +46,79 @@ export function Chat({
       },
     });
   const containerRef = useRef<HTMLDivElement | null>(null);
-  useLayoutEffect(() => {
-    const scrollToEnd = () => {
-      const container = containerRef.current;
-      if (container) {
-        container.scrollTop = container.scrollHeight; //  might need to adjust this if there's a fixed header/footer.
-      }
-    };
+  const { ref: referenceContainerRef, inView, entry } = useInView();
+  const [autoScrolling, setAutoScrolling] = useState(false);
 
-    if (messages.length) {
+  const scrollToEnd = () => {
+    const container = containerRef.current;
+    if (container) {
+      container.scrollTop = container.scrollHeight; //  might need to adjust this if there's a fixed header/footer.
+    }
+  };
+
+  useLayoutEffect(() => {
+    if (messages.length && autoScrolling) {
       scrollToEnd();
     }
-  }, [messages]);
+    if (inView && !isLoading) {
+      setAutoScrolling(false);
+    }
+  }, [messages, autoScrolling, inView, isLoading]);
 
   return (
-    <div
-      className="overflow-y-scroll custom-scrollbar h-screen"
-      ref={containerRef}
-    >
-      <div className={cn("pb-[200px] pt-4 md:pt-10", className)}>
-        {messages.length ? (
-          <>
-            <ChatList
-              messages={messages}
-              botImage={botImage}
-              isLoading={isLoading}
-              hideActions={type === "test"}
-            />
-          </>
-        ) : (
-          <div className="mx-auto max-w-2xl px-4 pt-8">
-            <div className="bg-slate-900 py-4 rounded-md flex place-content-center">
-              <h1 className="text-xl font-medium text-slate-500">
-                {emptyMessage}
-              </h1>
+    <div className="relative">
+      <div
+        className="overflow-y-scroll custom-scrollbar h-screen"
+        ref={containerRef}
+      >
+        <div className={cn("pb-[200px] pt-4 md:pt-10", className)}>
+          {messages.length ? (
+            <>
+              <ChatList
+                messages={messages}
+                botImage={botImage}
+                isLoading={isLoading}
+                hideActions={type === "test"}
+              />
+            </>
+          ) : (
+            <div className="mx-auto max-w-2xl px-4 pt-8">
+              <div className="bg-slate-900 py-4 rounded-md flex place-content-center">
+                <h1 className="text-xl font-medium text-slate-500">
+                  {emptyMessage}
+                </h1>
+              </div>
             </div>
+          )}
+          <div ref={referenceContainerRef}></div>
+        </div>
+        {!isDisabled && !isSubmitted ? (
+          <ChatPanel
+            id={id}
+            isLoading={isLoading}
+            stop={stop}
+            append={append}
+            reload={reload}
+            messages={messages}
+            input={input}
+            setInput={setInput}
+          />
+        ) : (
+          <div className="fixed inset-x-0 bottom-0 bg-gray-900 text-white p-4 rounded-t-lg shadow-lg">
+            <h1 className="text-center text-lg font-semibold">
+              {!isSubmitted ? "No longer active." : "Already submitted."}
+            </h1>
           </div>
         )}
       </div>
-      {!isDisabled && !isSubmitted ? (
-        <ChatPanel
-          id={id}
-          isLoading={isLoading}
-          stop={stop}
-          append={append}
-          reload={reload}
-          messages={messages}
-          input={input}
-          setInput={setInput}
-        />
-      ) : (
-        <div className="fixed inset-x-0 bottom-0 bg-gray-900 text-white p-4 rounded-t-lg shadow-lg">
-          <h1 className="text-center text-lg font-semibold">
-            {!isSubmitted ? "No longer active." : "Already submitted."}
-          </h1>
-        </div>
+      {!inView && (
+        <Button
+          variant="secondary"
+          onClick={() => setAutoScrolling(true)}
+          className="absolute bottom-[200px] right-[46%] -translate-x-1/2 hover:bg-secondary rounded-full"
+        >
+          <MdOutlineKeyboardDoubleArrowDown className="text-2xl" />
+        </Button>
       )}
     </div>
   );
