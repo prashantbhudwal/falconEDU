7f4511460be8f210551459e749e90fb0a75411bd
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-12-19 00:42:51 +0530

    fix: add response to email route

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,4 +1,5 @@
 import sendgrid from "@sendgrid/mail";
+import { NextResponse } from "next/server";
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY ?? "");
 
 export async function GET() {
@@ -9,9 +10,9 @@ export async function GET() {
       subject: `Test Email`,
       html: `<div>You've got a mail</div>`,
     });
+    return new NextResponse("Email sent successfully", { status: 200 });
   } catch (e) {
     console.log(e);
+    return new NextResponse("Email not sent", { status: 500 });
   }
-
-  console.log("Email sent");
 }
