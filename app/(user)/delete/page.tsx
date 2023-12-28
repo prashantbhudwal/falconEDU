78bdaab8d4da288dd0913bf6d132a842edbad529
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-12-28 23:06:38 +0530

    feat(internal): add functionality to make all bot chats submitted

--- app/(user)/delete/page.tsx
+++ app/(user)/delete/page.tsx
@@ -4,6 +4,8 @@ import prisma from "@/prisma";
 import { useState } from "react";
 const EMAIL = "trial.prashant@gmail.com";
 import { deleteUserByEmail } from "./actions";
+import { makeAllBotChatIsSubmitted } from "@/app/dragon/scripts";
+import { set } from "date-fns";
 
 export default function Page() {
   const [loading, setLoading] = useState(false);
@@ -13,6 +15,17 @@ export default function Page() {
     setLoading(true);
     try {
       await deleteUserByEmail(email);
+    } catch (err) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const setSubmitted = async () => {
+    setLoading(true);
+    try {
+      await makeAllBotChatIsSubmitted();
     } catch (err) {
       setError(true);
     } finally {
@@ -34,6 +47,20 @@ export default function Page() {
         disabled={loading}
       >
         Delete User
+      </Button>
+
+      <h1>Make all bot chats submitted</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error</p>}
+      <Button
+        variant={"destructive"}
+        size={"lg"}
+        onClick={() => {
+          setSubmitted();
+        }}
+        disabled={loading}
+      >
+        Make all bot chats submitted
       </Button>
     </div>
   );
