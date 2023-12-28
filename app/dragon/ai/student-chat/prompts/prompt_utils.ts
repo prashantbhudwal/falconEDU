0ab4922429e8b79471d9af5a78903c123edd4125
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-12-28 14:49:53 +0530

    prompt: update emoji directive in prompt_utils.ts

--- app/dragon/ai/student-chat/prompts/prompt_utils.ts
+++ app/dragon/ai/student-chat/prompts/prompt_utils.ts
@@ -10,7 +10,7 @@ RESPONSE FORMAT ENDS HERE
 `;
 
 export const EMOJI_DIRECTIVE = `
-## Use emojis in your responses. Use emojis to express emotions. Use emojis to express your tone. But don't overuse emojis, or use the same emoji again and again.
+## Use emojis in your responses. Use emojis to express emotions. Use emojis to express your tone. DON'T use the same emoji again and again.
 `;
 
 export const ONE_PARAGRAPH_DIRECTIVE_SYSTEM = `
