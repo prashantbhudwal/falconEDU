78e2e31e0dd8ff6036f7132c1ac63e82bb1866d4
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-12-28 19:42:17 +0530

    feat: add teacher training boilerplate

---
+++ app/dragon/teacher/(settings)/teacher-training/gallery/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TeacherTrainingPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Teacher Training Page</h1>
+      {/* Add your content here */}
+    </div>
+  );
+};
+
+export default TeacherTrainingPage;
---
+++ app/dragon/teacher/(settings)/teacher-training/player/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const VideoPlayer: React.FC = () => {
+  return (
+    <div>
+      <h1>Video Player</h1>
+      {/* Add your content here */}
+    </div>
+  );
+};
+
+export default VideoPlayer;
