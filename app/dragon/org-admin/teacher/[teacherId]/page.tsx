9d35f9f92d9886fcda7689dd17c80f4845c6e176
ankit <ankitbisht0205@gmail.com>
2024-01-12 03:14:13 +0530

    fix(admin): redirect user from teacher page if no teacher in org

--- app/dragon/org-admin/teacher/[teacherId]/page.tsx
+++ app/dragon/org-admin/teacher/[teacherId]/page.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import AdminNavbar from "../../_components/admin-navbar";
 import { Title } from "@tremor/react";
 import Timeline from "../../_components/timeline";
-import { getTeacherTasksWithTeacherId } from "../../queries";
+import { getOrgByUserId, getTeacherTasksWithTeacherId } from "../../queries";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
+import RedirectComponent from "../../_components/redirect";
 
 const TeacherPage = async ({ params }: { params: { teacherId: string } }) => {
+  const session = await getServerSession(authOptions); // get this from layout instead of calling again
+  const org = await getOrgByUserId(session?.user?.id || "");
   const teacher = await getTeacherTasksWithTeacherId({
     teacherId: params.teacherId,
   });
 
+  if (org && org.teacher.length === 0) {
+    return <RedirectComponent redirectUrl="/dragon/org-admin" />;
+  }
   return (
     <div>
       <AdminNavbar title="Teacher" />
