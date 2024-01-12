88fdeb44e3d0f62e2b0dabbbba56ddc2d38ba651
ankit <ankitbisht0205@gmail.com>
2024-01-12 06:00:47 +0530

    fix(admin): cache queries

--- app/dragon/org-admin/queries.ts
+++ app/dragon/org-admin/queries.ts
@@ -19,7 +19,7 @@ const getUserId = async (): Promise<string> => {
   return session?.user.id || "";
 }; //TODO: dont use this function to get userId pass from the layout to all components
 
-export const getAllPublishedTasksByDate = async () => {
+export const getAllPublishedTasksByDate = cache(async () => {
   try {
     const userId = await getUserId();
     const org = await prisma.orgAdminProfile.findUnique({
@@ -129,11 +129,11 @@ export const getAllPublishedTasksByDate = async () => {
     console.error(err);
     return null;
   }
-};
+});
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-export const getAllTeachersInAnOrg = async () => {
+export const getAllTeachersInAnOrg = cache(async () => {
   try {
     const userId = await getUserId();
     const org = await prisma.orgAdminProfile.findUnique({
@@ -247,7 +247,7 @@ export const getAllTeachersInAnOrg = async () => {
     console.error(err);
     return null;
   }
-};
+});
 
 export const getOrgByUserId = cache(async (userId: string) => {
   try {
@@ -278,7 +278,7 @@ export const getOrgByUserId = cache(async (userId: string) => {
   }
 });
 
-export const getTeacherWithOrgId = async () => {
+export const getTeacherWithOrgId = cache(async () => {
   try {
     const userId = await getUserId();
     const org = await prisma.orgAdminProfile.findUnique({
@@ -308,7 +308,7 @@ export const getTeacherWithOrgId = async () => {
     console.error(err);
     return null;
   }
-};
+});
 
 export const getTeacherTasksWithTeacherId = cache(
   async ({ teacherId }: { teacherId: string }) => {
