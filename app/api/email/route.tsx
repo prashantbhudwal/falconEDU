648ce2db986c635b9c8a003c065168d88173d43c
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-01-18 17:36:30 +0530

    refactor(email): refactor email route and component

--- app/api/email/route.tsx
+++ app/api/email/route.tsx
@@ -1,13 +1,13 @@
 import sendgrid from "@sendgrid/mail";
 import { NextResponse } from "next/server";
-import FalconAiInviteUserEmail from "../../emails/invite-email";
+import { InviteStudentsEmail } from "../../emails/invite-email";
 import { render } from "@react-email/render";
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY ?? "");
 
 export async function POST(request: Request) {
   const data = await request.json();
   const emailHtml = render(
-    <FalconAiInviteUserEmail
+    <InviteStudentsEmail
       teacherImage={data.teacherImage}
       invitedByEmail={data.teacherEmail}
       nameOfClass={data.nameOfClass}
@@ -18,7 +18,7 @@ export async function POST(request: Request) {
     await sendgrid.send({
       to: data.studentEmail, // Your email where you'll receive emails
       from: "hello@falconai.in", // your website email address here
-      subject: `Invitation`,
+      subject: `Join FalconAI. Learn with an AI teacher!`,
       html: emailHtml,
     });
     return new NextResponse("Email sent successfully", { status: 200 });
