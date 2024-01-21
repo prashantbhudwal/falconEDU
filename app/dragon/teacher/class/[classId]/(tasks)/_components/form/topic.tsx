3da8e7c505869261a80f88c821694404dff24bc3
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-01-21 07:53:48 +0530

    feat: add TopicField component to form

---
+++ app/dragon/teacher/class/[classId]/(tasks)/_components/form/topic.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useFormContext } from "react-hook-form";
+import { FiInfo } from "react-icons/fi";
+
+export const TopicField = ({ name }: { name: string }) => {
+  const form = useFormContext();
+  const [inputFocus, setInputFocus] = useState("");
+
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel
+            className={`mb-3 flex w-full justify-between align-middle font-bold ${
+              inputFocus === "topic" ? "text-white" : ""
+            }`}
+          >
+            <div className="flex items-center gap-2 text-xs">
+              Topic
+              <FiInfo />
+            </div>
+          </FormLabel>
+          <FormControl>
+            <Input
+              autoComplete="off"
+              className="text-sm"
+              placeholder="Topic you want to teach. For multiple topics, separate them with commas."
+              {...field}
+              onFocus={() => setInputFocus("topic")}
+              onBlur={() => setInputFocus("")}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
