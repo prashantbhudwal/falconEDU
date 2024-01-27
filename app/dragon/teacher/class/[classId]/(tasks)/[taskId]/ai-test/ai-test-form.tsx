09b6e68f0fcd6b95393c71b79242a27a4847d508
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-01-28 02:36:54 +0530

    Refactor AI test form component

--- app/dragon/teacher/class/[classId]/(tasks)/[taskId]/ai-test/ai-test-form.tsx
+++ app/dragon/teacher/class/[classId]/(tasks)/[taskId]/ai-test/ai-test-form.tsx
@@ -1,28 +1,14 @@
 "use client";
 import { Grade, type BotConfig } from "@prisma/client";
 import { ChangeEvent, useState } from "react";
-import { FormProvider, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { db } from "@/lib/routers";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Separator } from "@/components/ui/separator";
-
-import { Button } from "@/components/ui/button";
-import { FiInfo } from "react-icons/fi";
+import { Form } from "@/components/ui/form";
 import { Paper } from "@/components/ui/paper";
-import subjectsArray from "../../../../../../../data/subjects.json";
 import { useIsFormDirty } from "@/hooks/use-is-form-dirty";
 import { Input } from "@/components/ui/input";
-import TextAreaWithUpload from "../../_components/textarea-with-upload";
-import { getFormattedGrade } from "@/app/dragon/teacher/utils";
 import { SubjectsField } from "../../_components/task-form/fields/subjects-old";
 import { HumorLevelField } from "../../_components/task-form/fields/humor-level";
 import { TopicField } from "../../_components/task-form/fields/topic";
@@ -33,6 +19,7 @@ import {
   LIMITS_AITestPreferencesSchema,
 } from "@/lib/schema";
 import { MediumOfInstructionField } from "../../_components/task-form";
+import { TextAreaField } from "../../_components/task-form/fields/textarea";
 
 const MAX_CHARS = LIMITS_AITestPreferencesSchema.content.maxLength;
 
@@ -64,7 +51,6 @@ export default function AITestForm({
 }: AITestFormProps) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-  const [inputFocus, setInputFocus] = useState("");
   const [AITestName, setAITestName] = useState<string | undefined>(
     taskConfig?.name,
   );
@@ -92,17 +78,6 @@ export default function AITestForm({
       console.error("Update failed:", result.error);
       setError("Failed to update bot config. Please try again."); // set the error message
     }
-  };
-
-  const updateSubjectsHandler = () => {
-    const gradeNumber = getFormattedGrade({
-      grade,
-      options: { numberOnly: true },
-    });
-    const gradeObject = subjectsArray.filter(
-      (subject) => subject.grade === gradeNumber,
-    )[0];
-    return gradeObject.subjects;
   };
 
   const updateBotNameHandler = async () => {
@@ -141,7 +116,7 @@ export default function AITestForm({
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
           <Paper variant="gray" className="w-full max-w-5xl bg-base-200">
-            <div className="flex flex-wrap justify-between p-5">
+            <div className="flex flex-wrap justify-between ">
               <div className="w-[50%]">
                 <Input
                   type="text"
@@ -162,39 +137,11 @@ export default function AITestForm({
             </div>
             <TopicField name="topic" />
             <SubjectsField name="subjects" grade={grade} />
-            {/* ------------------------Content ------------------------- */}
-            <FormField
-              control={form.control}
+            <TextAreaField
               name="content"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel
-                    className={`mb-5 flex w-full justify-between align-middle font-bold ${
-                      inputFocus === "content" ? "text-white" : ""
-                    }`}
-                  >
-                    <div className="flex gap-2">
-                      Content
-                      <FiInfo />
-                    </div>
-                  </FormLabel>
-                  <FormProvider {...form}>
-                    <FormControl className="rounded-md border border-input">
-                      <TextAreaWithUpload
-                        placeholder="Add any additional reference material"
-                        counter
-                        maxChars={MAX_CHARS}
-                        required
-                        hasDocUploader
-                        setIsDirty={setIsDirty}
-                        className="border-none bg-base-200"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormProvider>
-                </FormItem>
-              )}
+              maxChars={MAX_CHARS}
+              placeholder="Add any additional reference material"
+              label="Content"
             />
             <MediumOfInstructionField name="mediumOfInstruction" />
             <HumorLevelField name="humorLevel" />
@@ -204,84 +151,3 @@ export default function AITestForm({
     </>
   );
 }
-
-/**
- *  
-            <FormField
-              control={form.control}
-              name="tone"
-              render={({ field }) => (
-                <FormItem className="space-y-3">
-                  <FormLabel className="mb-5 flex items-center gap-2 font-bold">
-                    Tone
-                    <SpeakerWaveIcon className="h-4 w-4" />
-                  </FormLabel>
-                  <FormControl>
-                    <RadioGroup
-                      onValueChange={() => {
-                        field.onChange;
-                      }}
-                      defaultValue={field.value}
-                      className="flex flex-row space-x-6 space-y-1"
-                    >
-                      {tone.map((tone) => (
-                        <FormItem
-                          className="flex flex-row items-center space-x-3 space-y-0"
-                          key={tone}
-                        >
-                          <FormControl>
-                            <RadioGroupItem
-                              value={tone}
-                              className=" transition-all duration-200 hover:scale-[1.2] active:scale-90"
-                            />
-                          </FormControl>
-                          <FormLabel className="font-normal">{tone}</FormLabel>
-                        </FormItem>
-                      ))}
-                    </RadioGroup>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
- *  <FormField
-              control={form.control}
-              name="languageProficiency"
-              render={({ field }) => (
-                <FormItem className="space-y-3">
-                  <FormLabel className="flex items-center gap-2 font-bold">
-                    Language Proficiency
-                    <LanguageIcon className="h-4 w-4" />
-                  </FormLabel>
-                  <FormControl>
-                    <RadioGroup
-                      onValueChange={() => {
-                        field.onChange;
-                      }}
-                      defaultValue={field.value}
-                      className="flex flex-row space-x-6 space-y-1"
-                    >
-                      {languageProficiency.map((languageProficiency) => (
-                        <FormItem
-                          className="flex flex-row items-center space-x-3 space-y-0"
-                          key={languageProficiency}
-                        >
-                          <FormControl>
-                            <RadioGroupItem
-                              value={languageProficiency}
-                              className=" transition-all duration-200 hover:scale-[1.2] active:scale-90"
-                            />
-                          </FormControl>
-                          <FormLabel className="font-normal">
-                            {languageProficiency}
-                          </FormLabel>
-                        </FormItem>
-                      ))}
-                    </RadioGroup>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
- * 
- */
