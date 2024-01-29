2e162a20a24be0a537f08eb6acb006322c0ec6e1
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-01-29 08:35:38 +0530

    Add student management functionality

--- app/dragon/org-admin/(settings)/manage/[orgId]/students/page.tsx
+++ app/dragon/org-admin/(settings)/manage/[orgId]/students/page.tsx
@@ -1,13 +1,33 @@
-import { ComingSoon } from "@/app/dragon/org-admin/_components/coming-soon";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
+import { AddStudentForm } from "@/app/dragon/org-admin/_components/add-user-form";
+import { UserManagementCard } from "@/app/dragon/org-admin/_components/user-card";
+import { db } from "@/lib/routers";
+import { getServerSession } from "next-auth";
 
 export default async function ManageStudents({
   params: { orgId },
 }: {
   params: { orgId: string };
 }) {
+  const session = await getServerSession(authOptions);
+  const userId = session?.user?.id;
+  if (!userId) return null;
+  const students = await db.org.getStudentsInOrg({ userId });
   return (
-    <div className="flex flex-col space-y-4 p-2">
-      <ComingSoon />
-    </div>
+    <>
+      <div className="mx-auto my-10 w-11/12">
+        <AddStudentForm orgId={orgId} />
+        {students?.map((student) => (
+          <UserManagementCard
+            key={student.id}
+            name={student.User.name ?? ""}
+            email={student.User.email ?? ""}
+            image={student.User.image}
+            userId={student.userId}
+            removeUserFunction={db.org.removeStudentFromOrg}
+          />
+        ))}
+      </div>
+    </>
   );
 }
