3abed2624a65e2c6460a089d17afc904cd2d47a4
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-01-29 08:35:27 +0530

    Refactor teacher management page

--- app/dragon/org-admin/(settings)/manage/[orgId]/teachers/page.tsx
+++ app/dragon/org-admin/(settings)/manage/[orgId]/teachers/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { AddTeacherForm } from "./add-teacher-form";
-import { getTeachersWithUserId } from "../../../../queries";
-import { Card, Flex, Text, Title } from "@tremor/react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import { getServerSession } from "next-auth";
-import { TeacherCard } from "./teacher-card";
+import { AddTeacherForm } from "@/app/dragon/org-admin/_components/add-user-form";
+import { UserManagementCard } from "@/app/dragon/org-admin/_components/user-card";
+import { db } from "@/lib/routers";
 
 export default async function AddTeacherPage({
   params: { orgId },
@@ -15,13 +13,20 @@ export default async function AddTeacherPage({
   const session = await getServerSession(authOptions);
   const userId = session?.user?.id;
   if (!userId) return null;
-  const teachers = await getTeachersWithUserId({ userId });
+  const teachers = await db.org.getTeachersInOrg({ userId });
   return (
     <>
       <div className="mx-auto my-10 w-11/12">
         <AddTeacherForm orgId={orgId} />
         {teachers?.map((teacher) => (
-          <TeacherCard teacher={teacher} key={teacher.id} />
+          <UserManagementCard
+            key={teacher.id}
+            name={teacher.User.name ?? ""}
+            email={teacher.User.email ?? ""}
+            image={teacher.User.image}
+            userId={teacher.userId}
+            removeUserFunction={db.org.removeTeacherFromOrg}
+          />
         ))}
       </div>
     </>
