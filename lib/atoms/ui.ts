a9d1a122574482896916234982760b7baf02daa0
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-02-01 11:38:42 +0530

    Add video modal and video URL atom

--- lib/atoms/ui.ts
+++ lib/atoms/ui.ts
@@ -5,3 +5,7 @@ export const evalDrawerAtom = atom<boolean>(false);
 export const newTaskModalAtom = atom<boolean>(false);
 
 export const submitTestModalAtom = atom<boolean>(false);
+
+export const showVideoModalAtom = atom<boolean>(false);
+
+export const videoUrlAtom = atom<string>("");
