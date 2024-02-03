edb6ea0e7df047647c339420115c10fec3c9fcfe
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-02-03 17:36:24 +0530

    Refactor code for improved performance

--- components/chat/chat-dragon.tsx
+++ components/chat/chat-dragon.tsx
@@ -10,24 +10,12 @@ import { Button } from "../ui/button";
 import { useInView } from "react-intersection-observer";
 import { chatIsLoadingAtom } from "@/lib/atoms/student";
 import { useUI } from "@/hooks/ai/use-ui";
-import { useAtom } from "jotai";
+import { useAtomValue, useSetAtom } from "jotai";
 import { useFirstMessage } from "./use-first-message";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { showVideoModalAtom, videoUrlAtom } from "@/lib/atoms/ui";
 import ReactPlayer from "react-player";
-import { AspectRatio } from "../ui/aspect-ratio";
 import { motion } from "framer-motion";
-
-const InDev = ({ component }: { component: React.ReactNode }) => {
-  if (process.env.NODE_ENV === "production") {
-    return null;
-  }
-  return (
-    <div className="fixed bottom-3 right-2 min-h-36 w-fit border border-dotted border-accent/60 p-2">
-      {component}
-    </div>
-  );
-};
 
 export interface ChatProps extends React.ComponentProps<"div"> {
   initialMessages?: Message[];
@@ -39,7 +27,32 @@ export interface ChatProps extends React.ComponentProps<"div"> {
   isDisabled?: boolean;
   isSubmitted?: boolean;
   type: string;
+  hidePanel?: boolean;
 }
+
+const scrollToPercentage = (
+  percent: number,
+  containerRef: React.RefObject<HTMLElement>,
+) => {
+  const container = containerRef.current;
+  if (container) {
+    const scrollPosition = container.scrollHeight * (percent / 100);
+    container.scrollTo({
+      top: scrollPosition,
+      behavior: "smooth",
+    });
+  }
+};
+
+const scrollToEnd = (containerRef: React.RefObject<HTMLElement>) => {
+  const container = containerRef.current;
+  if (container) {
+    container.scrollTo({
+      top: container.scrollHeight,
+      behavior: "smooth",
+    }); //  might need to adjust this if there's a fixed header/footer.
+  }
+};
 
 export function Chat({
   id,
@@ -52,26 +65,14 @@ export function Chat({
   isDisabled = false,
   isSubmitted = false,
   type,
-}: ChatProps) {
-  const borderAnimation = {
-    initial: {
-      borderColor: "rgba(255, 255, 255, 0)", // Transparent border
-    },
-    animate: {
-      borderColor: "rgba(255, 255, 255, 1)", // Solid white border
-    },
-  };
-
-  const shadowAnimation = {
-    initial: {
-      boxShadow: "0px 0px 0px rgba(0, 0, 0, 0)", // No shadow
-    },
-    animate: {
-      boxShadow: "0px 5px 15px rgba(0, 0, 0, 0.5)", // Visible shadow
-    },
-  };
-  const [showVideoModal, setShowVideoModal] = useAtom(showVideoModalAtom);
-  const [videoUrl, setVideoUrl] = useAtom(videoUrlAtom);
+  hidePanel,
+}: Readonly<ChatProps>) {
+  const [autoScrolling, setAutoScrolling] = useState(false);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const showVideoModal = useAtomValue(showVideoModalAtom);
+  const setIsLoadingAtom = useSetAtom(chatIsLoadingAtom);
+  const videoUrl = useAtomValue(videoUrlAtom);
+  const { ref: referenceContainerRef, inView } = useInView();
   const { messages, append, reload, stop, isLoading, input, setInput, data } =
     useChat({
       api: apiPath,
@@ -84,54 +85,53 @@ export function Chat({
         }
       },
     });
-
   useUI({ data, isLoading });
-
-  const [, setIsLoadingAtom] = useAtom(chatIsLoadingAtom);
   useEffect(() => {
     setIsLoadingAtom(isLoading);
   }, [isLoading, setIsLoadingAtom]);
 
-  const scrollToPercentage = (percent: any) => {
-    const container = containerRef.current;
-    if (container) {
-      const scrollPosition = container.scrollHeight * (percent / 100);
-      container.scrollTo({
-        top: scrollPosition,
-        behavior: "smooth",
-      });
-    }
-  };
-
   useEffect(() => {
     if (messages.length) {
-      scrollToPercentage(100);
+      scrollToPercentage(100, containerRef);
     }
   }, [messages.length]);
-  const containerRef = useRef<HTMLDivElement | null>(null);
-  const { ref: referenceContainerRef, inView, entry } = useInView();
-  const [autoScrolling, setAutoScrolling] = useState(false);
-
-  const scrollToEnd = () => {
-    const container = containerRef.current;
-    if (container) {
-      container.scrollTo({
-        top: container.scrollHeight,
-        behavior: "smooth",
-      }); //  might need to adjust this if there's a fixed header/footer.
-    }
-  };
 
   useFirstMessage({ messages, append, type });
 
   useLayoutEffect(() => {
     if (messages.length && autoScrolling) {
-      scrollToEnd();
+      scrollToEnd(containerRef);
     }
     if (inView && !isLoading) {
       setAutoScrolling(false);
     }
   }, [messages, autoScrolling, inView, isLoading]);
+
+  function renderChatPanel() {
+    if (hidePanel) return null;
+
+    if (!isDisabled && !isSubmitted) {
+      return (
+        <ChatPanel
+          id={id}
+          isLoading={isLoading}
+          stop={stop}
+          append={append}
+          reload={reload}
+          messages={messages}
+          input={input}
+          setInput={setInput}
+        />
+      );
+    }
+    return (
+      <div className="fixed inset-x-0 bottom-0 rounded-t-lg bg-gray-900 p-4 text-white shadow-lg">
+        <h1 className="text-center text-lg font-semibold">
+          {!isSubmitted ? "No longer active." : "Already submitted."}
+        </h1>
+      </div>
+    );
+  }
 
   return (
     <div className="relative">
@@ -149,66 +149,14 @@ export function Chat({
                 isLoading={isLoading}
                 hideActions={type === "test"}
               />
-              {showVideoModal && (
-                <motion.div
-                  initial="initial"
-                  animate="animate"
-                  variants={shadowAnimation}
-                  transition={{ duration: 0.5 }}
-                >
-                  <Card className="mx-auto w-fit shadow-lg shadow-secondary">
-                    <CardHeader>
-                      <CardTitle>Recommended Video</CardTitle>
-                    </CardHeader>
-                    <CardContent className="bg-base-200">
-                      {ReactPlayer.canPlay(videoUrl) ? (
-                        <ReactPlayer
-                          url={videoUrl}
-                          controls={true}
-                          height={200}
-                          width={"100%"}
-                        />
-                      ) : (
-                        <div className="flex h-full flex-col items-center justify-center">
-                          <p className="text-center text-gray-500">
-                            This video is not supported.
-                          </p>
-                        </div>
-                      )}
-                    </CardContent>
-                  </Card>
-                </motion.div>
-              )}
+              {showVideoModal && <VideoCard videoUrl={videoUrl} />}
             </>
           ) : (
-            <div className="mx-auto max-w-2xl px-4 pt-8">
-              <div className="flex place-content-center rounded-md bg-slate-900 py-4">
-                <h1 className="text-xl font-medium text-slate-500">
-                  {emptyMessage}
-                </h1>
-              </div>
-            </div>
+            <EmptyMessage message={emptyMessage} />
           )}
           <div ref={referenceContainerRef}></div>
         </div>
-        {!isDisabled && !isSubmitted ? (
-          <ChatPanel
-            id={id}
-            isLoading={isLoading}
-            stop={stop}
-            append={append}
-            reload={reload}
-            messages={messages}
-            input={input}
-            setInput={setInput}
-          />
-        ) : (
-          <div className="fixed inset-x-0 bottom-0 rounded-t-lg bg-gray-900 p-4 text-white shadow-lg">
-            <h1 className="text-center text-lg font-semibold">
-              {!isSubmitted ? "No longer active." : "Already submitted."}
-            </h1>
-          </div>
-        )}
+        {renderChatPanel()}
       </div>
       {!inView && (
         <Button
@@ -222,3 +170,66 @@ export function Chat({
     </div>
   );
 }
+
+const EmptyMessage = ({ message }: { message: string }) => {
+  return (
+    <div className="mx-auto max-w-2xl px-4 pt-8">
+      <div className="flex place-content-center rounded-md bg-slate-900 py-4">
+        <h1 className="text-xl font-medium text-slate-500">{message}</h1>
+      </div>
+    </div>
+  );
+};
+
+const VideoCard = ({ videoUrl }: { videoUrl: string }) => {
+  const shadowAnimation = {
+    initial: {
+      boxShadow: "0px 0px 0px rgba(0, 0, 0, 0)", // No shadow
+    },
+    animate: {
+      boxShadow: "0px 5px 15px rgba(0, 0, 0, 0.5)", // Visible shadow
+    },
+  };
+
+  return (
+    <motion.div
+      initial="initial"
+      animate="animate"
+      variants={shadowAnimation}
+      transition={{ duration: 0.5 }}
+    >
+      <Card className="mx-auto w-fit shadow-lg shadow-secondary">
+        <CardHeader>
+          <CardTitle>Recommended Video</CardTitle>
+        </CardHeader>
+        <CardContent className="bg-base-200">
+          {ReactPlayer.canPlay(videoUrl) ? (
+            <ReactPlayer
+              url={videoUrl}
+              controls={true}
+              height={200}
+              width={"100%"}
+            />
+          ) : (
+            <div className="flex h-full flex-col items-center justify-center">
+              <p className="text-center text-gray-500">
+                This video is not supported.
+              </p>
+            </div>
+          )}
+        </CardContent>
+      </Card>
+    </motion.div>
+  );
+};
+
+const InDev = ({ component }: { component: React.ReactNode }) => {
+  if (process.env.NODE_ENV === "production") {
+    return null;
+  }
+  return (
+    <div className="fixed bottom-3 right-2 min-h-36 w-fit border border-dotted border-accent/60 p-2">
+      {component}
+    </div>
+  );
+};
