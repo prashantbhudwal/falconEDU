86b08e159732f319fc073bfd6580b2360e39ea6a
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-02-06 06:25:55 +0530

    Refactor AI test form to use updated goals generator

--- app/dragon/teacher/class/[classId]/(tasks)/[taskId]/ai-test/ai-test-form.tsx
+++ app/dragon/teacher/class/[classId]/(tasks)/[taskId]/ai-test/ai-test-form.tsx
@@ -19,8 +19,7 @@ import {
 } from "@/lib/schema";
 import { MediumOfInstructionField } from "../../_components/task-form";
 import { TextAreaField } from "../../_components/task-form/fields/textarea";
-import { generateLearningGoals } from "@/app/dragon/ai/tasks/ai-test/goals-generator";
-import { LearningGoals } from "@/app/dragon/ai/tasks/ai-test/goals-generator/model";
+import { generateLearningGoalsWithAI } from "@/app/dragon/ai/tasks/ai-test/goals-generator";
 import { TaskName } from "../../_components/task-form/fields/task-name";
 
 const MAX_CHARS = LIMITS_AITestPreferencesSchema.content.maxLength;
@@ -60,46 +59,32 @@ export default function AITestForm({
   });
   const { isDirty, setIsDirty } = useIsFormDirty(form);
 
-  const getLearningGoals = async (
-    content: z.infer<(typeof AITestPreferenceSchema)["shape"]["content"]>,
-  ) => {
-    const { goals, error, message } = await generateLearningGoals({ content });
-    if (error || !goals) {
-      setError(message);
-      return;
-    }
-    return goals;
-  };
-
-  const saveLearningGoals = async (goals: LearningGoals) => {
-    await db.learningGoals.createLearningGoals({
-      configId: taskId,
-      learningGoals: goals,
-    });
-  };
-
   const onSubmit = async (data: z.infer<typeof AITestPreferenceSchema>) => {
     setLoading(true);
     const { content } = data;
-    const goals = await getLearningGoals(content);
-
-    if (goals) {
-      const test = await saveLearningGoals(goals);
-    }
-
-    const result = await db.botConfig.updateBotConfig({
-      classId,
-      botId: taskId,
-      data,
-      configType: "ai-test",
-    });
-    setLoading(false);
-    if (result.success) {
-      setIsDirty(false);
-      setError(null); // clear any existing error
-    } else {
-      console.error("Update failed:", result.error);
-      setError("Failed to update bot config. Please try again."); // set the error message
+    try {
+      const learningGoals = await generateLearningGoalsWithAI({ content });
+      if (learningGoals) {
+        const updatedGoals = await db.learningGoals.saveLearningGoals({
+          configId: taskId,
+          learningGoals,
+        });
+        const updatedConfig = await db.botConfig.updateTaskConfig({
+          classId,
+          botId: taskId,
+          data,
+          configType: "ai-test",
+        });
+        setError("");
+        setIsDirty(false);
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      }
+      setError("Something went wrong. Please try again.");
+    } finally {
+      setLoading(false);
     }
   };
 
