aaa40ff49902a381b7cbf48b1aff17499c0114cf
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-02-07 19:43:22 +0530

    Refactor checkAITest function

--- app/dragon/ai/tasks/ai-test/submission/index.ts
+++ app/dragon/ai/tasks/ai-test/submission/index.ts
@@ -4,26 +4,22 @@ import { getCheckingContext } from "./context";
 import { mapGoalsWithResults } from "./mapper";
 
 export async function checkAITest({ botChatId }: { botChatId: string }) {
-  try {
-    const context = await getCheckingContext(botChatId);
+  const context = await getCheckingContext(botChatId);
 
-    if (!context) {
-      throw new Error("Test not found");
-    }
-    const { goals, messages } = context;
-
-    const results = await getAITestResultsFromOpenAI({
-      goals,
-      messages,
-    });
-
-    const finalTestResults = mapGoalsWithResults({
-      goals,
-      results,
-    });
-
-    return finalTestResults;
-  } catch (err) {
-    console.error(err);
+  if (!context) {
+    throw new Error("Test not found");
   }
+  const { goals, messages } = context;
+
+  const results = await getAITestResultsFromOpenAI({
+    goals,
+    messages,
+  });
+
+  const finalTestResults = mapGoalsWithResults({
+    goals,
+    results,
+  });
+
+  return finalTestResults;
 }
