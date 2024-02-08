4b248f34c0e894ddac7b1ffe0a3fe0a3d68e4a89
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-02-08 17:46:35 +0530

    Add ChatSuggestions component

---
+++ components/chat/suggestions.tsx
@@ -0,0 +1,68 @@
+import { Button } from "../ui/button";
+import { UseChatHelpers } from "ai/react/dist";
+import { CreateMessage } from "ai";
+
+const suggestions: Array<{
+  keyword: string;
+  suggestion: string;
+  prompt: CreateMessage;
+}> = [
+  {
+    keyword: "next",
+    suggestion: "What's next?",
+    prompt: {
+      role: "assistant",
+      content: "What's next?",
+    },
+  },
+  {
+    keyword: "example",
+    suggestion: "Give me an example of this",
+    prompt: {
+      role: "assistant",
+      content: "Can you give me an example?",
+    },
+  },
+  {
+    keyword: "confused",
+    suggestion: "I am confused",
+    prompt: {
+      role: "assistant",
+      content: "I am confused about this, can you help me?",
+    },
+  },
+  {
+    keyword: "story",
+    suggestion: "Tell me a story",
+    prompt: {
+      role: "assistant",
+      content: "Can you tell me a story to explain this?",
+    },
+  },
+];
+
+export function ChatSuggestions({
+  append,
+}: {
+  append: UseChatHelpers["append"];
+}) {
+  const onClick = async (suggestion: CreateMessage) => {
+    await append(suggestion);
+  };
+
+  return (
+    <div className="scrollbar-hide flex flex-row space-x-3 overflow-x-auto pl-3">
+      {suggestions.map((suggestion) => (
+        <Button
+          onClick={() => onClick(suggestion.prompt)}
+          key={suggestion.keyword}
+          variant={"outline"}
+          size={"sm"}
+          className="w-fit whitespace-nowrap rounded-2xl border-accent/50 bg-base-300 text-xs hover:bg-base-300 hover:text-inherit active:bg-base-300 active:text-inherit"
+        >
+          {suggestion.suggestion}
+        </Button>
+      ))}
+    </div>
+  );
+}
