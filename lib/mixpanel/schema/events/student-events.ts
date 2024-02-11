4adac7807e433d50c1aa71f3c85cf4b67f843eaa
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-02-11 19:42:56 +0530

    Implement task_viewed, task_submitted, result_viewed, task_submission_failed, tool_used, and chat_completion_failed events

--- lib/mixpanel/schema/events/student-events.ts
+++ lib/mixpanel/schema/events/student-events.ts
@@ -10,10 +10,11 @@ type BaseTaskProperties = {
 };
 
 type TaskEvents = {
-  task_viewed: BaseTaskProperties;
+  task_viewed: BaseTaskProperties; // Implemented
   task_started: BaseTaskProperties;
-  task_submitted: BaseTaskProperties;
-  result_viewed: BaseTaskProperties;
+  task_submitted: BaseTaskProperties; // Implemented
+  result_viewed: BaseTaskProperties; // Implemented
+  task_submission_failed: BaseTaskProperties; // Implemented
 };
 
 // --- Avatar Events ---
@@ -23,13 +24,25 @@ type AvatarEvents = {
 };
 
 // --- Chat Events ---
+
+type BaseChatProperties = {
+  taskType: TaskType;
+  attemptId: string;
+  model: string;
+  temperature: number;
+};
+
 type ChatEvents = {
   textToSpeech_used: {};
   speechToText_used: {};
-  message_sent: {
-    message_id: string;
-  };
+  message_sent: BaseChatProperties;
+  tool_used: BaseChatProperties & {
+    tool_name: string;
+  }; // Implemented
   chat_suggestion_used: {};
+  chat_completion_failed: BaseChatProperties & {
+    isError: boolean;
+  }; // Implemented
 };
 
 export type StudentEventProperties = CommonEvents &
