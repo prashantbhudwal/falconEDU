2fe7b93360e96bddbccad0e1dce039ee02def09a
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-02-16 16:44:12 +0530

    Refactor analytics widget component

--- app/dragon/teacher/class/[classId]/(home)/components/widgets/analytics-widget.tsx
+++ app/dragon/teacher/class/[classId]/(home)/components/widgets/analytics-widget.tsx
@@ -2,14 +2,14 @@ import { getTasksWithInteractions } from "@/lib/routers/studentRouter";
 import { getTaskUrlByType } from "@/lib/urls";
 import { TaskType } from "@/types";
 import Link from "next/link";
-import React from "react";
+import React, { Suspense } from "react";
 import { type TasksWithInteractions } from "@/lib/routers/studentRouter";
 import {
   ArrowTrendingDownIcon,
   ArrowTrendingUpIcon,
   FaceFrownIcon,
+  UserIcon,
 } from "@heroicons/react/24/solid";
-import { UserIcon } from "@heroicons/react/24/solid";
 
 const AnalyticsWidget = async ({ classId }: { classId: string }) => {
   const leastInteractedTasksList = await getTasksWithInteractions({ classId });
@@ -17,27 +17,29 @@ const AnalyticsWidget = async ({ classId }: { classId: string }) => {
   const threeMostInteractedTasks = leastInteractedTasksList?.slice(-3);
 
   return (
-    <div className="flex w-[250px] flex-col space-y-4 rounded-xl border border-base-200 p-3">
-      <h3 className="flex w-full items-center gap-2 rounded-xl text-sm text-slate-500">
-        <span>Student Activity</span>
-      </h3>
-      <div className="flex flex-col space-y-9 text-xs text-slate-400">
-        <section className="flex flex-col space-y-2">
-          <div className="flex items-center space-x-2 text-destructive/80">
-            <h4>Least Active Tasks</h4>
-            <ArrowTrendingDownIcon className="h-3 w-3" />
-          </div>
-          <TaskList taskList={threeLeastInteractedTasks} classId={classId} />
-        </section>
-        <section className="flex flex-col space-y-3">
-          <div className="flex items-center space-x-2 text-primary/80">
-            <h4>Most Active Tasks</h4>
-            <ArrowTrendingUpIcon className="h-3 w-3" />
-          </div>
-          <TaskList taskList={threeMostInteractedTasks} classId={classId} />
-        </section>
+    <Suspense fallback={<AnalyticsWidgetFallback />}>
+      <div className="flex w-[250px] flex-col space-y-4 rounded-xl border border-base-200 p-3">
+        <h3 className="flex w-full items-center gap-2 rounded-xl text-sm text-slate-500">
+          <span>Student Activity</span>
+        </h3>
+        <div className="flex flex-col space-y-9 text-xs text-slate-400">
+          <section className="flex flex-col space-y-2">
+            <div className="flex items-center space-x-2 text-destructive/80">
+              <h4>Least Active Tasks</h4>
+              <ArrowTrendingDownIcon className="h-3 w-3" />
+            </div>
+            <TaskList taskList={threeLeastInteractedTasks} classId={classId} />
+          </section>
+          <section className="flex flex-col space-y-3">
+            <div className="flex items-center space-x-2 text-primary/80">
+              <h4>Most Active Tasks</h4>
+              <ArrowTrendingUpIcon className="h-3 w-3" />
+            </div>
+            <TaskList taskList={threeMostInteractedTasks} classId={classId} />
+          </section>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
@@ -83,3 +85,13 @@ const TaskList = async ({
 };
 
 export default AnalyticsWidget;
+
+const AnalyticsWidgetFallback = () => {
+  return (
+    <div className="h-[200px] w-[250px] rounded-xl bg-base-200 p-3">
+      <div className="h-7 animate-pulse rounded-xl bg-base-100"></div>
+      <div className="mt-3 h-4 animate-pulse rounded-xl bg-base-100"></div>
+      <div className="mt-3 h-4 animate-pulse rounded-xl bg-base-100"></div>
+    </div>
+  );
+};
