4bf078c707bbf53065b644b8a899c608fc20813c
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-02-16 17:47:38 +0530

    Add upload button to resources section

--- app/dragon/teacher/class/[classId]/(home)/components/widgets/upload/upload-widget.tsx
+++ app/dragon/teacher/class/[classId]/(home)/components/widgets/upload/upload-widget.tsx
@@ -1,14 +1,22 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { AddIcon } from "@/components/icons";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export async function UploadWidget({ classId }: { classId: string }) {
   return (
     <Card className="custom-scrollbar h-[400px] overflow-y-clip hover:overflow-y-auto">
       <CardHeader className="sticky top-0 bg-base-200">
-        <CardTitle>Resources</CardTitle>
+        <div className="flex items-center justify-between">
+          <CardTitle>Resources</CardTitle>
+          <Link href={""}>
+            <Button size={"icon"} variant={"ghost"}>
+              <AddIcon size="sm" />
+            </Button>
+          </Link>
+        </div>
       </CardHeader>
-      <CardContent>
-     
-      </CardContent>
+      <CardContent></CardContent>
     </Card>
   );
 }
