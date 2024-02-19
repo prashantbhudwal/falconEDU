a7687424eee730e3256a9245bbd0db042baf6ea5
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-02-19 23:16:33 +0530

    Add ResourceCard component to display resources in the page

--- app/dragon/teacher/class/[classId]/(settings)/resources/page.tsx
+++ app/dragon/teacher/class/[classId]/(settings)/resources/page.tsx
@@ -2,6 +2,17 @@ import { Paper } from "@/components/ui/paper";
 import { db } from "@/lib/routers";
 
 import { NewResourceDialog, NewResourceForm } from "./new-resource-form";
+import { Source } from "@prisma/client";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { formatDate } from "@/lib/utils";
+import Link from "next/link";
+import { url } from "@/lib/urls";
 
 export default async function Resources({
   params,
@@ -14,13 +25,49 @@ export default async function Resources({
   const allResources = await db.source.queries.allByClassId({ classId });
 
   return (
-    <Paper className="items-center">
+    <Paper className="h-full max-w-3xl items-center space-y-8">
       <NewResourceDialog classId={classId} />
-      <div>
+      <div className="flex flex-col space-y-4">
         {allResources.map((resource) => (
-          <div key={resource.id}>{resource.title}</div>
+          <ResourceCard
+            resource={resource}
+            classId={classId}
+            key={resource.id}
+          />
         ))}
       </div>
     </Paper>
   );
 }
+
+const ResourceCard = ({
+  resource,
+  classId,
+}: {
+  resource: Source;
+  classId: string;
+}) => {
+  const { description, title, updatedAt } = resource;
+  const nonNullableDescription =
+    description && description.length > 0 ? description : "No description";
+  const formattedUpdatedAt = formatDate(updatedAt, { withTime: true });
+  const resourceRoute = url.teacher.editResource({
+    resourceId: resource.id,
+    classId: classId,
+  });
+  return (
+    <Link href={resourceRoute} key={resource.id}>
+      <Card className="cursor-pointer select-none ring-secondary transition-transform duration-200 ease-in-out hover:-translate-y-1 hover:ring-1">
+        <CardHeader>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>{nonNullableDescription}</CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <div className="flex flex-row space-x-4 text-xs text-slate-400">
+            <div>Last Updated: {formattedUpdatedAt}</div>
+          </div>
+        </CardFooter>
+      </Card>
+    </Link>
+  );
+};
