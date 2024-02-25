6cbe0a45a016b01dfec63ccf94f5a06606425d25
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-02-25 22:40:55 +0530

    Add magic content suggestions to task form fields

---
+++ app/dragon/teacher/class/[classId]/(tasks)/_components/task-form/fields/magic-content/suggestions.ts
@@ -0,0 +1,22 @@
+export const magicContentSuggestions = [
+  {
+    keyword: "shorter",
+    suggestion: "Make it shorter",
+    prompt: "Make this shorter",
+  },
+  {
+    keyword: "simpler",
+    suggestion: "Make it simpler",
+    prompt: "Make this simpler",
+  },
+  {
+    keyword: "longer",
+    suggestion: "Make it longer",
+    prompt: "Can you add more details and make it a bit longer?",
+  },
+  {
+    keyword: "funnier",
+    suggestion: "Make it funnier",
+    prompt: "Can you make this funnier by adding a joke or two?",
+  },
+] as const;
