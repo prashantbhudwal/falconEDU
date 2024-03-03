f67df6c393737e584974feae19cae916f9770b7d
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-03-04 04:52:18 +0530

    Refactor right action bar component

--- components/navbar/right-action-bar.tsx
+++ components/navbar/right-action-bar.tsx
@@ -1,41 +1,14 @@
 "use client";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-  TooltipProvider,
-} from "@ui/tooltip";
 import usePageTracking from "@/hooks/usePageTracking";
-import clsx from "clsx"; // for combining class names
 import { useRouter } from "next/navigation";
 import { ButtonConfig } from "@/components/navbar/use-navbar-buttons";
 import useNavbarButtons from "@/components/navbar/use-navbar-buttons";
-import { useEffect, useState } from "react";
-import {
-  getTeacherData,
-  typeGetTeacherPreferences,
-} from "@/app/dragon/teacher/(settings)/teacher-preferences/getTeacherData";
-import { removeOptionalFieldFormZodTypes } from "@/lib/utils";
-import { SparklesIcon } from "@heroicons/react/24/solid";
-import { teacherPreferencesSchema } from "@/lib/schema";
+import { cn } from "@/lib/utils";
 
 export default function RightActionBar() {
   const { currentPage } = usePageTracking();
   const router = useRouter();
   const buttonConfiguration = useNavbarButtons();
-  const [preferences, setPreferences] = useState({});
-
-  useEffect(() => {
-    const asyncFunction = async () => {
-      const { preferences } = (await getTeacherData()) as {
-        preferences: typeGetTeacherPreferences;
-      };
-      if (preferences) {
-        setPreferences(preferences);
-      }
-    };
-    asyncFunction();
-  }, []);
 
   const createButton = (
     key: string,
@@ -48,16 +21,6 @@ export default function RightActionBar() {
       onClick,
     }: ButtonConfig,
   ) => {
-    let isFormIncomplete = false;
-    if (name === "My Avatar") {
-      const updatedTeacherPreferencesSchema = removeOptionalFieldFormZodTypes(
-        teacherPreferencesSchema,
-      );
-      const { success } =
-        updatedTeacherPreferencesSchema.safeParse(preferences);
-      if (!success) isFormIncomplete = true;
-    }
-
     return (
       <button
         key={key}
@@ -65,31 +28,17 @@ export default function RightActionBar() {
           onClick();
           href !== "" && router.push(href);
         }}
-        className={clsx(
+        className={cn(
           `btn btn-xs relative flex place-content-center rounded-sm py-4 font-medium capitalize`,
           linkClass,
         )}
         disabled={!isEnabled}
       >
         <Icon
-          className={clsx(`text-sm font-bold text-slate-600`, additionalClass)}
+          className={cn(`text-sm font-bold text-slate-600`, additionalClass)}
         />
         {name}
         {/* ---------------------------------------------- */}
-        {name === "My Avatar" && isFormIncomplete && (
-          <TooltipProvider>
-            <Tooltip delayDuration={20}>
-              <TooltipTrigger asChild>
-                <span className="absolute -left-2 -top-2 h-[20px] w-[20px] text-destructive">
-                  <SparklesIcon />
-                </span>
-              </TooltipTrigger>
-              <TooltipContent className="bg-base-200 text-slate-300">
-                Your Avatar is Incomplete
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        )}
       </button>
     );
   };
