73e80d8fe64827aaba3e320684f7f5301204a0d9
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-03-07 07:27:18 +0530

    Update PrismaClient configuration

--- prisma/index.ts
+++ prisma/index.ts
@@ -2,7 +2,16 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    transactionOptions: {
+      timeout: 10000, // 10 seconds
+      maxWait: 5000, // 5 seconds
+    },
+    log: ["error", "info", "query", "warn"],
+    errorFormat: "pretty",
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
