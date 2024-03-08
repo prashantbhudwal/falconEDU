5bde045750184186a585d9c13269f0a40d258da2
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-03-08 13:04:04 +0530

    Add image optimization to file upload

--- app/dragon/teacher/class/[classId]/(tasks)/components/image-upload/upload.ts
+++ app/dragon/teacher/class/[classId]/(tasks)/components/image-upload/upload.ts
@@ -3,6 +3,7 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import { s3 } from "@/lib/aws";
 import { nanoid } from "nanoid";
 import { getServerSession } from "next-auth";
+import sharp from "sharp";
 
 export const uploadToDigitalOcean = async (formData: FormData) => {
   const session = await getServerSession(authOptions);
@@ -11,15 +12,23 @@ export const uploadToDigitalOcean = async (formData: FormData) => {
   }
   const file = formData.get("file") as File;
   const buffer = Buffer.from(await file.arrayBuffer());
-  const params = {
-    Bucket: process.env.DO_SPACES_BUCKET ?? "",
-    Key: `uploads/${file.name}-${nanoid()}`,
-    Body: buffer,
-    ACL: "public-read",
-  };
+  const OptimizedBuffer = await sharp(buffer)
+    .webp({ lossless: true })
+    .toBuffer();
+
+  const bucket = process.env.DO_SPACES_BUCKET ?? "";
+
   try {
-    const stored = await s3.upload(params).promise();
-    return { url: stored.Location, key: stored.Key, bucket: params.Bucket };
+    const stored = await s3
+      .upload({
+        Bucket: bucket,
+        Key: `uploads/${file.name}-${nanoid()}.webp`,
+        Body: OptimizedBuffer,
+        ACL: "public-read",
+        ContentType: "image/webp",
+      })
+      .promise();
+    return { url: stored.Location, key: stored.Key, bucket: bucket };
   } catch (error) {
     console.error("Error uploading file:", error);
     throw error;
