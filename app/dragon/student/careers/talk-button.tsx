d389887f1a65b9f24f54200930a0b70918006ed4
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-03-09 02:06:27 +0530

    Refactor talk-button component

--- app/dragon/student/careers/talk-button.tsx
+++ app/dragon/student/careers/talk-button.tsx
@@ -3,11 +3,10 @@ import { Button } from "@/components/ui/button";
 import { BotConfig, HostedImage } from "@prisma/client";
 import React from "react";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
 } from "@/components/ui/dialog";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Avvvatars from "avvvatars-react";
@@ -15,6 +14,7 @@ import { cn, constructDigitalOceanUrl } from "@/lib/utils";
 import { useSession } from "next-auth/react";
 import { createBot } from "./create-bot";
 import { useRouter } from "next/navigation";
+import { PulseLoader } from "react-spinners";
 type BotConfigWithHostedImage = BotConfig & { avatar: HostedImage | null };
 export function TalkButton({ config }: { config: BotConfigWithHostedImage }) {
   const bucket = config.avatar?.bucket ?? "";
@@ -26,30 +26,36 @@ export function TalkButton({ config }: { config: BotConfigWithHostedImage }) {
   const [open, setOpen] = React.useState(false);
   const handleClick = async () => {
     setOpen(true);
-    console.log("Creating bot");
     const { newBot, defaultChat } = await createBot(config, userId);
-    console.log("Bot created", newBot);
-    console.log("Default chat", defaultChat);
     const redirectUrl = `/dragon/student/careers/chat/${newBot.id}/${defaultChat.id}`;
-    console.log("Redirecting to", redirectUrl);
     router.push(redirectUrl);
-    setOpen(false);
   };
 
   return (
     <div className="flex w-full flex-col items-center">
       <Dialog open={open} onOpenChange={setOpen}>
-        <DialogContent className="flex h-2/5 w-4/5 flex-col items-center space-y-3">
+        <DialogContent className="flex h-2/5 w-4/5 flex-col items-center space-y-3 border-fuchsia-950">
           <DialogHeader className="pt-10">
-            <DialogTitle>Connecting ... </DialogTitle>
-            <DialogDescription> {config.name}</DialogDescription>
+            <DialogTitle>
+              <div className="flex animate-pulse flex-row items-baseline gap-1">
+                <div>Connecting</div>
+                <PulseLoader size={4} color={"#059669"} speedMultiplier={0.5} />
+              </div>
+            </DialogTitle>
           </DialogHeader>
-          <Avatar className={cn("h-16 w-16 group-hover:blur-sm")}>
-            <AvatarImage src={url} alt="User Avatar" />
-            <AvatarFallback className="bg-base-100">
-              <Avvvatars value={config.name} style="shape" />
-            </AvatarFallback>
-          </Avatar>
+          <div className="flex flex-col items-center space-y-5">
+            <Avatar
+              className={cn(
+                "h-20 w-20 animate-pulse ring ring-fuchsia-900 group-hover:blur-sm",
+              )}
+            >
+              <AvatarImage src={url} alt="User Avatar" />
+              <AvatarFallback className="bg-base-100">
+                <Avvvatars value={config.name} style="shape" />
+              </AvatarFallback>
+            </Avatar>
+            <DialogTitle>{config.name}</DialogTitle>
+          </div>
         </DialogContent>
       </Dialog>
       <Button onClick={handleClick} className="w-2/3">
