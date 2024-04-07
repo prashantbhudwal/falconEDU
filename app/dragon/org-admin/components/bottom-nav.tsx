59c95f7bcf2b90c7b6134cd25b0b0671ceefdd2f
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-04-08 02:13:26 +0530

    Refactor bottom navigation in org-admin components

--- app/dragon/org-admin/components/bottom-nav.tsx
+++ app/dragon/org-admin/components/bottom-nav.tsx
@@ -7,28 +7,66 @@ import {
   AIMagicIcon,
 } from "@/components/icons";
 import { url } from "@/lib/urls";
+import { useSelectedLayoutSegment } from "next/navigation";
+import { motion } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+type AdminHomeSegments = "explore" | "manage" | null;
+
+const bottomNavConfig = [
+  { href: url.orgAdmin.home, Icon: AIMagicIcon, text: "Act", segment: null },
+  {
+    href: url.orgAdmin.explore.home,
+    Icon: PieChartIcon,
+    text: "Explore",
+    segment: "explore",
+  },
+  {
+    href: url.orgAdmin.manage.home,
+    Icon: ToolIcon,
+    text: "Manage",
+    segment: "manage",
+  },
+] as const;
 
 export const BottomNav = () => {
+  const layoutSegment = useSelectedLayoutSegment() as AdminHomeSegments;
+
   return (
-    <div className="fixed bottom-0 z-50 w-full bg-base-300 shadow-md">
-      <div className="flex justify-around py-2">
-        <NavbarItem href={url.orgAdmin.home} Icon={AIMagicIcon} text="Act" />
-        <NavbarItem
-          href={url.orgAdmin.explore.home}
-          Icon={PieChartIcon}
-          text="Explore"
-        />
-        <NavbarItem
-          href={url.orgAdmin.manage.home}
-          Icon={ToolIcon}
-          text="Manage"
-        />
-      </div>
+    <div className="fixed bottom-0 z-40 w-full bg-base-300 shadow-md">
+      <motion.div layout className="flex justify-around py-2">
+        {bottomNavConfig.map(({ href, Icon, text, segment }) => {
+          const isSelected = layoutSegment === segment;
+          return (
+            <Link
+              key={href}
+              href={href}
+              className={cn(
+                "relative flex min-w-20 flex-col items-center px-4 py-1",
+                {
+                  "text-primary": isSelected,
+                  "text-slate-600": !isSelected,
+                },
+              )}
+            >
+              <Icon size="xs" />
+              <span className="text-xs">{text}</span>
+              {isSelected && (
+                <motion.div
+                  layoutId="activeNavItem"
+                  className="absolute inset-0 -z-10 rounded-badge bg-purple-800"
+                  transition={{ duration: 0.2 }}
+                ></motion.div>
+              )}
+            </Link>
+          );
+        })}
+      </motion.div>
     </div>
   );
 };
 
-const NavbarItem = ({
+const NavItem = ({
   href,
   Icon,
   text,
