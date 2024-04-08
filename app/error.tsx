e4a3b66d0179e79cbac342c57ac7bc6304c4ec20
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-04-09 00:23:01 +0530

    Refactor error component in app/error.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { toast } from "@/components/ui/use-toast";
 
 export default function Error({
   error,
@@ -22,34 +21,44 @@ export default function Error({
   }, [error]);
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-5">
-      <Image
-        src="/chubbi-sad.png"
-        alt="chubbi"
-        width={200}
-        height={200}
-        className="rounded-full"
-      />
+      <div className="flex flex-col items-center gap-5 text-center">
+        <Image
+          src="/chubbi-sad.png"
+          alt="chubbi"
+          width={200}
+          height={200}
+          className="rounded-full"
+        />
 
-      <div className="text-4xl font-bold">
-        {isOnline ? "Something went wrong!" : "Check your internet connection!"}
+        <div className="text-2xl font-bold sm:text-2xl md:text-3xl lg:text-4xl">
+          {isOnline ? "Something went wrong!" : "You are offline!"}
+        </div>
+        <div className="text-lg sm:text-lg md:text-xl lg:text-2xl">
+          {isOnline ? (
+            <div>
+              <span className="">
+                If the error persists please reach out to us at:{" "}
+              </span>
+              <Link href="mailto:hello@falconai.in" className="font-bold">
+                hello@falconai.in
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <span className="">Please check your internet connection.</span>
+            </div>
+          )}
+        </div>
+        <Button
+          variant={"secondary"}
+          onClick={
+            // Attempt to recover by trying to re-render the segment
+            () => reset()
+          }
+        >
+          Try again
+        </Button>
       </div>
-      <div>
-        <span className="">
-          If the error persists please reach out to us at:{" "}
-        </span>
-        <Link href="mailto:hello@falconai.in" className="font-bold">
-          hello@falconai.in
-        </Link>
-      </div>
-      <Button
-        variant={"secondary"}
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </Button>
     </div>
   );
 }
