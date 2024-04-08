d25584d193af630d6e34d8f3c44f2d36024fed57
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-04-09 03:34:59 +0530

    Add getClassesByTeacherId and getTeacherNameByTeacherId functions in teacher router

--- lib/routers/teacher/index.ts
+++ lib/routers/teacher/index.ts
@@ -35,6 +35,48 @@ export const getTeacherByTeacherId = cache(
   },
 );
 
+export const getClassesByTeacherId = cache(
+  async ({ teacherId }: { teacherId: string }) => {
+    try {
+      const classes = await prisma.class.findMany({
+        where: {
+          teacherId: teacherId,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+      return classes;
+    } catch (err) {
+      console.log(err);
+      return [];
+    }
+  },
+);
+
+export const getTeacherNameByTeacherId = cache(
+  async ({ teacherId }: { teacherId: string }) => {
+    try {
+      const teacherProfile = await prisma.teacherProfile.findUnique({
+        where: {
+          id: teacherId,
+        },
+        select: {
+          User: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      });
+      return teacherProfile?.User.name;
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
+  },
+);
+
 export const getTeacherId = cache(async function (userId: string) {
   const teacherProfile = await prisma.teacherProfile.findUnique({
     where: { userId },
