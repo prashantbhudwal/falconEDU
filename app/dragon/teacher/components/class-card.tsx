fbf2887d669ab827fae31610e209fb3cbca5f531
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2024-04-09 04:18:59 +0530

    Update ClassCard component styles for responsive design

--- app/dragon/teacher/components/class-card.tsx
+++ app/dragon/teacher/components/class-card.tsx
@@ -29,7 +29,7 @@ export default function ClassCard({
       transition={{ type: "spring", stiffness: 90, damping: 10 }}
       layout
       whileHover={{ translateY: -10 }}
-      className="group group card w-48 hover:-translate-y-1"
+      className="group group card w-36 hover:-translate-y-1 md:w-48"
     >
       <Card className="border border-slate-700">
         <figure className="bg-gradient-to-t from-slate-900 to-slate-950 py-6 text-fuchsia-800 group-hover:from-fuchsia-900 ">
@@ -37,11 +37,13 @@ export default function ClassCard({
             <Avvvatars value={id} style="shape" size={70} />
           </div>
         </figure>
-        <div className="card-body h-20 items-center border-t border-t-slate-800 py-2 ">
+        <div className="card-body h-20 items-center border-t border-t-slate-800 py-2 md:h-20 ">
           <h2 className="card-title group-hover:text-fuchsia-700">
             {getFormattedGrade({ grade })}
           </h2>
-          {section && <div className="truncate text-sm">{section}</div>}
+          {section && (
+            <div className="truncate text-xs md:text-sm">{section}</div>
+          )}
         </div>
       </Card>
     </motion.div>
